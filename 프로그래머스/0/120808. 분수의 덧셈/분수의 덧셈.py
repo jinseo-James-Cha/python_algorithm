421e59174d3ff4f48952326fd6c9b0a2e8e85a5b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-17 15:34:47 +0100

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,12 +1,19 @@
+import math # for version 2
+
 def solution(numer1, denom1, numer2, denom2):
     new_numer = (numer1 * denom2) + (numer2 * denom1)
     new_denom = denom1 * denom2
     
     # GCD = Greatest Common Divisor
-    answer = [new_numer , new_denom]
-    for i in range(min(new_numer, new_denom), 1, -1):
-        if new_numer % i == 0 and new_denom % i == 0:
-            answer = [new_numer / i, new_denom / i]
-            break
+    
+    # version 1
+    # answer = [new_numer , new_denom]
+    # for i in range(min(new_numer, new_denom), 1, -1):
+    #     if new_numer % i == 0 and new_denom % i == 0:
+    #         answer = [new_numer / i, new_denom / i]
+    #         break
+    
+    gcd = math.gcd(new_numer, new_denom)
+    answer = [new_numer/gcd, new_denom/gcd]
     
     return answer
