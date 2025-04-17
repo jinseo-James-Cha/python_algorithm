f031f045fe1efae7ab1f1ec26a218c31e9222a1e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-17 12:06:17 +0100

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,11 +1,22 @@
 def solution(ineq, eq, n, m):
-    if ineq == '<':
-        if eq == '=':
-            return int(n <= m)
-        else:
-            return int(n < m)
-    else:
-        if eq == '=':
-            return int(n >= m)
-        else:
-            return int(n > m)
+    # version 1
+    # if ineq == '<':
+    #     if eq == '=':
+    #         return int(n <= m)
+    #     else:
+    #         return int(n < m)
+    # else:
+    #     if eq == '=':
+    #         return int(n >= m)
+    #     else:
+    #         return int(n > m)
+    
+    # version 2
+    # if eq == '!':
+    #     return int(n > m) if ineq == '>' else int(n < m)
+    # else:
+    #     return int(n >= m) if ineq == '>' else int(n <= m)
+    
+    # version 3
+    eq = eq if eq == '=' else ''
+    return int(eval(f"{n} {ineq}{eq} {m}"))
