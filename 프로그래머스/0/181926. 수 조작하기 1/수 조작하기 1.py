a3d3e12a15f28f1b09e68599c679f0d534401a9f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-20 23:30:12 +0100

    [level 0] Title: 수 조작하기 1, Time: 8.25 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,14 @@
+# control을 리스트로 바꾸고 set을 사용하면 각 레터별 중복의 수를 알수있지않나?
+# 아 set으로 하니깐 {'s', 'w', 'a', 'd'} 이렇게 나오네 중복제거만 가능함 + 리스트 아니여도됨
+
+def solution(n, control):
+    for con in control:
+        if con == 'w':
+            n += 1
+        elif con == 's':
+            n -= 1
+        elif con == 'd':
+            n += 10
+        else:
+            n -= 10
+    return n
