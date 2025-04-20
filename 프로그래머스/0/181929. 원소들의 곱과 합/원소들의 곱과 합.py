040227fbb4e8efa9cd3bd1608c3d13da87eed7d4
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-20 22:51:41 +0100

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,11 +1,15 @@
 # for loop을 써야할까?
 # 다른방법은 없을까..
 
+# 곱하는건 for loop이 맞는듯하다
+# 하지만 정수의 리스트를 더하는건 sum(num_list)
+
 def solution(num_list):
-    sum = 0
+    # s = 0
+    s = sum(num_list)
     mul = 1
     for i in range(len(num_list)):
-        sum += num_list[i]
+        # s += num_list[i]
         mul *= num_list[i]
 
-    return int(mul < sum**2)
+    return int(mul < s**2)
