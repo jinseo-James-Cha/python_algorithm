e06b522948a181ce73fa6ff0d7314b2da443225e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-22 10:19:50 +0100

    [level 0] Title: 배열 만들기 2, Time: 235.13 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,19 +1,31 @@
+# set - set 할 수도 있구나?!
+# 5의 배수단위로만 interval setting
+
+# def solution(l, r):
+#     answer = []
+#     for num in range(l, r+1):
+#         num_str = str(num)
+#         if not set(num_str) - set(['0', '5']):
+#             answer.append(num)
+            
+#     return answer if answer else [-1]    
+
+# 다른방식으로도 적어보자
 def solution(l, r):
     answer = []
+    letters = ['0', '5']
     
-    number = ['0', '5']
-    
-    for i in range(l, r+1):
-        now = str(i)
-        now_in = True
-        for j in now:
-            if j not in number:
-                now_in = False
+    for target in range(l, r + 1):
+        flag = False
+        target_str = str(target)
+        for target_letter in target_str:
+            if target_letter in letters:
+                flag = True
+            else:
+                flag = False
                 break
-        if now_in == True:
-            answer.append(i)
-    
-    if answer:
-        return answer
-    else:
-        return [-1]
+        if flag:
+            answer.append(target)
+            
+    return answer if answer else [-1]
+ 
