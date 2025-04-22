7790e214f3d2319eb706ad40066082a47f35fa86
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-22 10:22:53 +0100

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -1,3 +1,6 @@
+# 방법 1 return list(range(start_num, end_num + 1))
+# 방법 2 return [num for num in range(start_num, end_num + 1)]
+
 def solution(start_num, end_num):
     answer = []
     for i in range(start_num, end_num +  1):
