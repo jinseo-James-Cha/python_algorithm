f2b93732a45103e91bafa82181d43f1c44c08b40
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-23 14:45:57 +0100

    [Silver IV] Title: 베스트셀러, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -1,15 +1,37 @@
-from collections import Counter
+# version 2
+# Counter없이 그냥 map을 써보자 pure하게!
+# map == dict
+books = dict()
+for _ in range(int(input())):
+    book = input()
+    if book in books:
+        books[book] += 1
+    else:
+        books[book] = 1
 
-books = [input() for _ in range(int(input()))]
-counter_bn = Counter(books)
+max_num = max(books.values())
+candi = []
+for key, value in books.items():
+    if max_num == value:
+        candi.append(key)
 
-answer = ""
-freq = 0
-for key, value in counter_bn.items():
-    if value > freq:
-        answer = key
-        freq = value
-    elif value == freq and key < answer:
-        answer = key
+candi.sort()
+print(candi[0])
 
-print(answer)
+# version 1 using Counter
+
+# from collections import Counter
+
+# books = [input() for _ in range(int(input()))]
+# counter_bn = Counter(books)
+
+# answer = ""
+# freq = 0
+# for key, value in counter_bn.items():
+#     if value > freq:
+#         answer = key
+#         freq = value
+#     elif value == freq and key < answer:
+#         answer = key
+
+# print(answer)
