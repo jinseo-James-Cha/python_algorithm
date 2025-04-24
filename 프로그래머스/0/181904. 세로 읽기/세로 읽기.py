6fac68102d4094be2353d7831e462ddb706506e2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-24 10:32:22 +0100

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,6 +1,2 @@
 def solution(my_string, m, c):
-    # answer = ''
-    # for st in my_string[c-1::m]:
-    #     answer += st
-    
-    return ''.join([st for st in my_string[c-1::m]])
+    return my_string[c-1::m]
