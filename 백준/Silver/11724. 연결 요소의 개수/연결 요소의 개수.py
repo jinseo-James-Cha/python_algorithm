a8cf2e104a91e40ebfd9f0b7c5ce0d7d818b305f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-24 15:09:07 +0100

    [Silver II] Title: 연결 요소의 개수, Time: 728 ms, Memory: 39308 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,27 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+adj = [[0] * N for _ in range(N)]
+for _ in range(M):
+    a, b = map(lambda x: x - 1, map(int, input().split()))
+    adj[a][b] = adj[b][a] = 1 # no direction
+
+ans = 0
+chk = [False] * N # to check all vertex, set all False
+
+def dfs(now):
+    for nxt in range(N):
+        if adj[now][nxt] and not chk[nxt]:
+            chk[nxt] = True # 더 효율적인 방법
+            dfs(nxt)
+
+for i in range(N):
+    if not chk[i]:
+        ans += 1
+        chk[i] = True
+        dfs(i)
+
+print(ans)
