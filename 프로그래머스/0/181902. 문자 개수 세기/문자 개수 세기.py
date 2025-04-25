b5c0a2c44de9f7e8323c5425b6d52ebc61ce66b2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-25 10:58:53 +0100

    [level 0] Title: 문자 개수 세기, Time: 0.06 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,16 +1,22 @@
-# ord(ch)는 문자의 아스키코드 숫자를 알려줘.
+# 난 이 방법이 더 좋은거같아
 def solution(my_string):
-    lower_case = [0] * 26
-    upper_case = [0] * 26
-    
-    a_num = ord('a')
-    z_num = ord('z')
-    A_num = ord('A')
-    for target in my_string:
-        target_num = ord(target)
-        if target_num >= a_num and target_num <= z_num:
-            lower_case[target_num - (a_num)] += 1
-        else:
-            upper_case[target_num - (A_num)] += 1
+    alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    return [my_string.count(a) for a in alphabets.upper() + alphabets]
 
-    return upper_case + lower_case
+
+# ord(ch)는 문자의 아스키코드 숫자
+# def solution(my_string):
+#     lower_case = [0] * 26
+#     upper_case = [0] * 26
+    
+#     a_num = ord('a')
+#     z_num = ord('z')
+#     A_num = ord('A')
+#     for target in my_string:
+#         target_num = ord(target)
+#         if target_num >= a_num and target_num <= z_num:
+#             lower_case[target_num - (a_num)] += 1
+#         else:
+#             upper_case[target_num - (A_num)] += 1
+
+#     return upper_case + lower_case
