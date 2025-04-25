b44411a82cf8a44f4d849a01015b9766df14cc65
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-25 11:31:59 +0100

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,3 +1,20 @@
+# 답지에 외워둘만한 두가지.
+# try:
+#   ...
+# except:
+#   ...
+# index(찾는값, 시작index)
+
+def solution(arr, idx):
+    answer = 0
+    try:
+        answer = arr.index(1, idx)
+    except:
+        answer = -1
+
+    return answer
+
+## 내가 푼 방식이고 
 def solution(arr, idx):
     answer = -1
     for idx in range(idx, len(arr)):
