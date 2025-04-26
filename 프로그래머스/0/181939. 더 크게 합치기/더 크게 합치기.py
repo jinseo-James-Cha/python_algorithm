6b6d318040001e2284aa482ba1b933a04c5ee74e
jinseo-James-Cha <jinseo_cha@hotmail.com>
2025-04-26 15:33:09 +0100

    solution2

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,4 +1,7 @@
 def solution(a, b):
+    return int(max(f"{a}{b}", f"{b}{a}"))
+
+# solution 2
 #     str_ab = str(a) + str(b)
 #     str_ba = str(b) + str(a)
     
@@ -6,4 +9,3 @@ def solution(a, b):
 #     int_ba = int(str_ba)
     
 #     return int_ab if int_ab >= int_ba else int_ba
-    return int(max(f"{a}{b}", f"{b}{a}"))
