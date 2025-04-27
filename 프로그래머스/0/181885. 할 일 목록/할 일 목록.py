efdb5d046d2d6b501bbda7ed3b1f20324e8ac79d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-27 16:20:06 +0100

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,2 +1,6 @@
+# enumerate를 하면 idx, value가 나오는구나!
+# return [work for idx, work in enumerate(todo_list) if not finished[idx]]
+
+
 def solution(todo_list, finished):
     return [todo_list[i] for i in range(len(todo_list)) if not finished[i]]
