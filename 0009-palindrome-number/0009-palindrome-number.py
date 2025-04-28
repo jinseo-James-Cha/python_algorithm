625ba4f9760a937bd7bee61a0f2cc523edf94174
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-28 13:43:57 +0100

    Time: 13 ms (22.9%), Space: 17.9 MB (39.4%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,13 +1,34 @@
+# Follow up: Could you solve it without converting the integer to a string?
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         # all negative cannot be True
         if x < 0:
             return False
-        
-        x_str = str(x)
-        x_str_rev = x_str[::-1]
 
-        return x_str == x_str_rev
+        # copy original
+        original = x
+
+        # for loop from the back
+        back_num = 0
+        while x > 0:
+            back_num *= 10
+            back_num += x % 10
+            x = x // 10
+        
+        return original == back_num
+            
+
+
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         # all negative cannot be True
+#         if x < 0:
+#             return False
+        
+#         x_str = str(x)
+#         x_str_rev = x_str[::-1]
+
+#         return x_str == x_str_rev
 
 
         
