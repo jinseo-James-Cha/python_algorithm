308854cb362666e0c264cc68b27067df30c45fd3
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-28 12:50:54 +0100

    Time: 4 ms (26.64%), Space: 17.9 MB (25.61%) - LeetHub

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -0,0 +1,19 @@
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        # return true for Good and false otherwise
+        # good means all chars has the same num of occurrences
+        dct = {}
+        for char in s:
+            if char in dct:
+                dct[char] += 1
+            else:
+                dct[char] = 1
+        
+        v_dct = list(dct.values())
+        for i in range(len(v_dct) - 1):
+            if not v_dct[i] == v_dct[i+1]:
+                return False
+        
+        return True
+
+        
