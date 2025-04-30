799b8988b1ddd7b4bb51fe9e653be66472a0e0b1
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-30 09:33:13 +0100

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 18.08 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,8 +1,17 @@
+# version 2 by heapq
 import heapq
 
 def solution(myString):
-#     answer= []
-#     for a in myString.split("x"):
-#         if a:
-            
-    return sorted([a for a in myString.split("x") if a])
+    answer = []
+    for a in myString.split("x"):
+        if a:
+            heapq.heappush(answer,a)
+    
+    # return answer
+    return [heapq.heappop(answer) for _ in range(len(answer))]
+    
+    
+
+
+# def solution(myString):
+#     return sorted([a for a in myString.split("x") if a])
