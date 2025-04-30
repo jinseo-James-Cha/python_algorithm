885eb1a00195524e69c5997858ab1b1720fd012a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-30 09:14:37 +0100

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 5.34 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -0,0 +1,8 @@
+import heapq
+
+def solution(myString):
+#     answer= []
+#     for a in myString.split("x"):
+#         if a:
+            
+    return sorted([a for a in myString.split("x") if a])
