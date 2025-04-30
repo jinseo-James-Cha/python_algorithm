081daaeb28efd8db1a59e1d2da6bcee804da9f47
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-04-30 13:35:44 +0100

    Time: 47 ms (7.5%), Space: 26.5 MB (99.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,5 +1,14 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set(nums)
-        return not len(s) == len(nums)
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        
+        return False
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         s = set(nums)
+#         return not len(s) == len(nums)
         
