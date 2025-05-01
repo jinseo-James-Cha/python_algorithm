1d9dc3133f3dec394b388f8ef864314892319c00
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-05-01 10:16:10 +0100

    [level 0] Title: 문자열 묶기, Time: 24.08 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,11 +1,13 @@
 def solution(strArr):
-    answer = 0
     a = {}
     for s in strArr:
         a[len(s)] = a.get(len(s),0) + 1
+        
+    # 가장 큰 값의 k를 구하려면...아래와같이?    
+    # m = 0
+    # answer = 0
+    # for k, v in a.items():
+    #     if v > m:
+    #         answer = k
+    return max(a.values())
     
-    m = 0
-    for k, v in a.items():
-        if v > m:
-            m = v
-    return m
