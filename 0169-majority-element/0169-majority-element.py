7ab158d1069bb079c69abf0af2abf106c240acd1
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-05-01 17:13:24 +0100

    Time: 0 ms (100%), Space: 19.3 MB (51.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -6,14 +6,26 @@
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        h = {}
-        m_value = m_key = 0
+        # follow up - space O(N)
+        m_e = c = 0
         for n in nums:
-            h[n] = h.get(n, 0) + 1
-            if h[n] > m_value:
-                m_value = h[n]
-                m_key = n
-        return m_key
+            if c == 0:
+                m_e = n
+            
+            c += 1 if n == m_e else -1
+
+        return m_e
+
+        # time O(N)
+        # space O(N)
+        # h = {}
+        # m_value = m_key = 0
+        # for n in nums:
+        #     h[n] = h.get(n, 0) + 1
+        #     if h[n] > m_value:
+        #         m_value = h[n]
+        #         m_key = n
+        # return m_key
 
         # memory limit exceeded
         # answer = [0] * (max(nums) + 1)
