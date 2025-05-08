8b8519ce8fc8491986b0a127e2216a5854a69691
jinseo-James-Cha <jinseo_cha@hotmail.com>
2025-05-08 13:57:46 +0100

    change name

---
+++ Self-PS/11726-binomial.py
@@ -0,0 +1,11 @@
+# bottom - up
+MOD = 10007
+N = int(input())
+
+cache = [0] * 1001
+cache[1] = 1
+cache[2] = 2
+for i in range(3, N + 1):
+    cache[i] = (cache[i-1] + cache[i-2]) % MOD
+
+print(cache[N])
