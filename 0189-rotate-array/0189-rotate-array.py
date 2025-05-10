f9ef24b8715ad874d1a487967e1267404f98376b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-05-10 23:12:45 +0100

    Time: 7 ms (45.84%), Space: 25.6 MB (75.02%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,13 +5,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Version 3
+        # (k + i) % len = the index
+        temp = [0] * len(nums)
+        for i in range(len(nums)):
+            temp[(k+i) % len(nums)] = nums[i]
+        
+        for j in range(len(temp)):
+            nums[j] = temp[j]
         
         # Version 2 deque
         # Using [:] change original value in-place
         # rotate() => pop() + appendleft
-        dq = deque(nums)
-        dq.rotate(k)
-        nums[:] = list(dq)
+        # dq = deque(nums)
+        # dq.rotate(k)
+        # nums[:] = list(dq)
         
         # Version 1 deque
         # Using [:] change original value in-place
@@ -20,3 +28,8 @@ class Solution:
             # temp = dq.pop()
             # dq.appendleft(temp)
         # nums[:] = list(dq)
+        
+        
+        
+        
+        
