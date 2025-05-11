f7fbeab107191434c35db77df9c8c03ce8e4fcbb
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-05-11 17:07:30 +0100

    Time: 0 ms (100%), Space: 19.5 MB (77.74%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,18 @@
+# Version 2 XOR operator ^
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        temp = []
+        result = 0
         for n in nums:
-            if n in temp:
-                temp.remove(n)
-            else:
-                temp.append(n)
-        return temp[0]        
+            result ^= n
+        return result
+
+
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         temp = []
+#         for n in nums:
+#             if n in temp:
+#                 temp.remove(n)
+#             else:
+#                 temp.append(n)
+#         return temp[0]        
