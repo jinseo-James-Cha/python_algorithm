1220c3d7b4e2c6732078012fc8b7a5ba7ca307f1
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-05-11 17:05:18 +0100

    Time: 623 ms (9.57%), Space: 19.6 MB (44.59%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,20 +1,9 @@
-# O(N) time
-# O(1) space
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xor = 0
-        for num in nums:
-            xor ^= num
-        
-        return xor        
-        # if len(nums) <= 2:
-        #     return nums[0]
-
-        # for n in nums:
-        #     index_n = nums.index(n)
-        #     if not n in nums[index_n+1:]:
-        #         return n
-
-        # return nums[-1]
-            
-        
+        temp = []
+        for n in nums:
+            if n in temp:
+                temp.remove(n)
+            else:
+                temp.append(n)
+        return temp[0]        
