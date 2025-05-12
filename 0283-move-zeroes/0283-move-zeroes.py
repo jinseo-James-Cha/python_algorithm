075ff6e5c6095e856679793933b352467cdcead5
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-05-12 11:08:15 +0100

    Time: 35 ms (13.27%), Space: 18.6 MB (94.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,11 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # two pointers solution 2
+#         left = 0
+#         for right in range(1, len(nums)):
+#             if nums[left] == 0 and nums[right] != 0:
+#                 nums[left], nums[right] = nums[right], nums[left]
+#                 left += 1
+#             elif nums[left] != 0:
+#                 left += 1
+        
         # Two pointer solution
         left = 0
-
         for right in range(len(nums)):
             if nums[right] != 0:
+                print(left)
+                print(right)
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
 
