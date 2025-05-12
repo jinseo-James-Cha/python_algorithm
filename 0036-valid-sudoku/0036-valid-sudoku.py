231f701ca1f674ea99cfd115f381a30ef3375672
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-05-12 13:18:59 +0100

    Time: 6 ms (45.39%), Space: 17.7 MB (92.12%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,6 +1,6 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:        
-         # 3x3 박스 체크용 3차원 배열 (3x3x10)
+         # 3x3x10 -> 10False in a box and 3 * 3 box
         box_check = [[[False] * 10 for _ in range(3)] for _ in range(3)]
 
         for r in range(9):
@@ -9,21 +9,20 @@ class Solution:
 
             for c in range(9):
                 # -----------------------------
-                # 행 검사
+                # row
                 if board[r][c] != ".":
                     num = int(board[r][c])
                     if row_check[num]:
                         return False
                     row_check[num] = True
 
-                    # 3x3 박스 검사
+                    # 3x3 box
                     box_r, box_c = r // 3, c // 3
                     if box_check[box_r][box_c][num]:
                         return False
                     box_check[box_r][box_c][num] = True
 
-                # -----------------------------
-                # 열 검사
+                # col
                 if board[c][r] != ".":
                     num = int(board[c][r])
                     if col_check[num]:
