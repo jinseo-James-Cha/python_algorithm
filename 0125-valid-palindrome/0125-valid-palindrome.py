4d2ea83bf43745fea5fe5f23423e67c387982766
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-05-16 15:37:03 +0100

    Time: 15 ms (13.8%), Space: 18.2 MB (47.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,21 +1,42 @@
-# converting upper into lowercase
-# removing all non-alpha
-
+# New Solution with two pointer
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         new_s = ""
-        for letter in s:
-            if letter >= "a" and letter <= "z":
-                new_s += letter
-            elif letter >= "A" and letter <= "Z":
-                new_s += letter.lower()
-            elif letter >= "0" and letter <= "9":
-                new_s += letter
-            
-        r_new_s = new_s[::-1]
+        for a in s:
+            if (a >= "a" and a <= "z") or (a >= "A" and a <= "Z") or (a >= "0" and a <= "9"):
+                new_s += a.lower()
 
-        if r_new_s == new_s:
-            return True
+            # if not a.isalpha(): above or this
+                # continue
+        # two pointer
+        left = 0
+        for right in range(len(new_s)-1, -1, -1):
+            if left > right:
+                break
+            if new_s[left] != new_s[right]:
+                return False
+            left += 1
+        return True
+
+
+# converting upper into lowercase
+# removing all non-alpha
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         new_s = ""
+#         for letter in s:
+#             if letter >= "a" and letter <= "z":
+#                 new_s += letter
+#             elif letter >= "A" and letter <= "Z":
+#                 new_s += letter.lower()
+#             elif letter >= "0" and letter <= "9":
+#                 new_s += letter
+            
+#         r_new_s = new_s[::-1]
+
+#         if r_new_s == new_s:
+#             return True
         
-        return False
+#         return False
         
