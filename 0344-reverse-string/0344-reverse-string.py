d7398b4b0b15f1b9fa92a447e28bc01ed0bb38b2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-05-16 10:43:18 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,17 +1,32 @@
-# my solition 1 - recursion
-# slicing is creating new list, so it won't change the original
+# my solution 2 - two pointer
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        # space O(1)
-        return self.reverse(s, 0, len(s) - 1)
+        # left ->
+        # right <-
+        left = 0
+        right = len(s) - 1 # index
+        while right - left > 0:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            left -= 1
+        return s
+
+
+
+# my solition 1 - recursion
+# slicing is creating new list, so it won't change the original
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         # space O(1)
+#         return self.reverse(s, 0, len(s) - 1)
     
-    def reverse(self, s: List[str], start, end) -> List[str]:
-        if end - start < 1:
-            return s
-        s[start], s[end] = s[end], s[start]
-        start += 1
-        end -= 1
-        return self.reverse(s, start, end)
+#     def reverse(self, s: List[str], start, end) -> List[str]:
+#         if end - start < 1:
+#             return s
+#         s[start], s[end] = s[end], s[start]
+#         start += 1
+#         end -= 1
+#         return self.reverse(s, start, end)
