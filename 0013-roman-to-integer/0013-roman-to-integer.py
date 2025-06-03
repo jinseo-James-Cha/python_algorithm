e9c72ef13fcf392962ef123fbc492a70f2c793a0
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-03 15:19:07 +0100

    Time: 10 ms (18.03%), Space: 17.7 MB (64.3%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,7 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman = {'I':1 ,'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        # d_roman = {'IV': 4, 'IX' : 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
         d_roman = {'IV': -2, 'IX' : -2, 'XL': -20, 'XC': -20, 'CD': -200, 'CM': -200}
 
         answer = 0
