7b1f4e20d56fe4a13b3562735de6d07a4290a9d8
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-06 16:23:53 +0100

    Time: 0 ms (100%), Space: 17.8 MB (80.82%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,36 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        open_stack = []
-        open_brackets = ['(', '{', '[']
-        close_brackets = [')', '}', ']']
-        if len(s) % 2 != 0:
-            return False
-        
-        for b in s:
-            if b in open_brackets:
-                open_stack.append(b)
+        stack = []
+        hash = {')': '(', ']': '[', '}': '{'}
+
+        for char in s:
+            if char in hash:
+                if stack and stack[-1] == hash[char]:
+                    stack.pop()
+                else:
+                    return False
             else:
-                if not open_stack:
-                    return False
-                    
-                bracket = open_stack.pop()
-                if bracket != open_brackets[close_brackets.index(b)]:
-                    return False
-        return not open_stack
+              stack.append(char)
+
+        return not stack
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         open_stack = []
+#         open_brackets = ['(', '{', '[']
+#         close_brackets = [')', '}', ']']
+#         if len(s) % 2 != 0:
+#             return False
+        
+#         for b in s:
+#             if b in open_brackets:
+#                 open_stack.append(b)
+#             else:
+#                 if not open_stack:
+#                     return False
+
+#                 bracket = open_stack.pop()
+#                 if bracket != open_brackets[close_brackets.index(b)]:
+#                     return False
+#         return not open_stack
 
