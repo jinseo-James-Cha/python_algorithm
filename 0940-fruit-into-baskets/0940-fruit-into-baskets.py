0f307a4e26ff6ed9eee799fa6a6d3178db33b0e2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-09 18:48:28 +0100

    Time: 187 ms (51.05%), Space: 23.5 MB (77.91%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -0,0 +1,80 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        if len(fruits) == 1:
+            return 1
+        
+        # save two fruit types 
+        # using dict? or what?
+        baskets = {} 
+        left = 0
+        m = 0
+        for right in range(len(fruits)):
+            baskets[fruits[right]] = baskets.get(fruits[right], 0) + 1
+            
+            while len(baskets) > 2:
+                baskets[fruits[left]] -= 1
+                if baskets[fruits[left]] == 0:
+                    del baskets[fruits[left]]
+                left += 1
+
+            m = max(right - left + 1, m)
+        return m
+
+
+# class Solution:
+#     def totalFruit(self, fruits: List[int]) -> int:
+#         # count # of two types
+#         # 1 <= fruits.length <= 10^5 -> O(N^2) is not available
+
+#         # my idea
+#         # add +1 into Dictionary and descending order and sum first 2 value
+#         # but we dont need key, so let me try with array
+#         d = [0] * len(fruits)
+#         for f in fruits:
+#             d[f] += 1
+        
+#         d.sort(reverse = True)
+#         return d[0] + d[1]
+
+
+# class Solution:
+#     def totalFruit(self, fruits: List[int]) -> int:
+        # problem understanding failed
+        # it wants the maximum num of 2 types in a row
+        # I think I have done something like this and it uses two pointers.
+        # if len(fruits) == 1:
+        #     return 1
+        
+        # baskets = {}
+        # left = 0
+        
+        # baskets[fruits[left]] = left
+        # m = 1
+        # for right in range(1, len(fruits)):
+        #     print("right", right)
+        #     if len(baskets) < 2:
+        #         if fruits[right] not in baskets:
+        #             # register the first index of the type
+        #             baskets[fruits[right]] = right
+        #         else:
+        #             # already registered 
+        #             continue
+        #     elif fruits[right] in baskets:
+        #         print("got same continue")
+        #     else:
+        #         # update baskets with new type
+        #         del baskets[fruits[left]]
+
+        #         # update left with remained basket
+        #         for t, i in baskets.items():
+        #             left = i
+        #         baskets[fruits[right]] = right
+
+        #     print("left", left)
+        #     print("baskets", baskets)
+
+        #     if len(baskets) == 2:
+        #         print("answer", right- left + 1)
+        #         m = max(right - left + 1, m)
+
+        # return m
