a5e267c174685d223a6f7c99e039cfe124d237a7
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-09 22:05:19 +0100

    Time: 16 ms (68.06%), Space: 17.7 MB (82.07%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,35 @@
 # sliding window feeling !
-
-# 0 <= s.length <= 5 * 10^4 -> O(N)
-from collections import deque
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        m = 0
-        dq = deque()
-
-        left = 0
+        left = max_length = 0
+        char_set = set()
+        
         for right in range(len(s)):
-            temp = 1
-            while s[right] in dq:
-                dq.popleft()
+            while s[right] in char_set:
+                char_set.remove(s[left])
                 left += 1
+
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
+
+
+
+# 0 <= s.length <= 5 * 10^4 -> O(N)
+# from collections import deque
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         m = 0
+#         dq = deque()
+
+#         left = 0
+#         for right in range(len(s)):
+#             temp = 1
+#             while s[right] in dq:
+#                 dq.popleft()
+#                 left += 1
             
-            dq.append(s[right])
-            m = max(len(dq), m)
-        return m
+#             dq.append(s[right])
+#             m = max(len(dq), m)
+#         return m
