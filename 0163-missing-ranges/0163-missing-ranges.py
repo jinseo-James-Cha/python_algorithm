34b4392bf34f70098736af487e06daee655785f7
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-10 16:13:25 +0100

    Time: 0 ms (100%), Space: 18 MB (14.73%) - LeetHub

---
+++ 0163-missing-ranges/0163-missing-ranges.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+        # nums = sorted unique integer
+        # x is missing if lower < x < upper and not in nums
+        # return shortest sorted list of ranges that exactly covers all the missing numbers
+
+        # All the values of nums are unique.
+        # why is this given.. something better with unique nums..?
+
+        res = []
+
+        if len(nums) == 0:
+            return [[lower, upper]]
+
+        for i, n in enumerate(nums):
+            # first i needs to compare with lower
+            if i == 0:
+                if lower + 1 <= nums[i]:
+                    res.append([lower, nums[0]-1])
+            # last i needs to compare with upper
+            
+            if i == len(nums) - 1:
+                if nums[i] + 1 <= upper:
+                    res.append([nums[i] + 1, upper])
+            # compare i - i+1
+            else:
+                if n + 1 < nums[i+1]:
+                    res.append([n+1, nums[i+1] - 1])
+
+        return res
