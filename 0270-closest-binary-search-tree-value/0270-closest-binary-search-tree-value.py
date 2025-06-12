e32a6ce73b1cfb55cf6b5d19f0a3ec6f9a119222
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-12 10:59:21 +0100

    Time: 3 ms (14.67%), Space: 19.5 MB (82.49%) - LeetHub

---
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+import sys
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        # BST!!!!
+        # uh !? return the value in the BST that is closest to the target?! print the smallest.
+        # this is not about BFS ?!!! closest and smallest?!
+
+        # keep tracking minumum gap beween val and target
+        res = []
+        minimum = sys.maxsize
+        q = deque([root])
+
+        while q:
+            cur = q.popleft() # root
+            if abs(cur.val - target) < minimum:
+                # new closest came
+                # reset minimum and candidate
+                res = [cur.val]
+                minimum = abs(cur.val - target)
+            elif abs(cur.val - target) == minimum:
+                # add another candidate
+                res.append(cur.val)
+
+            # next level
+            if cur.left:
+                q.append(cur.left)
+            if cur.right:
+                q.append(cur.right)
+        
+        return min(res)
