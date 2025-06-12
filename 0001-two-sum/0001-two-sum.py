de83e04bd09fe851e4d7804f5f3d4d66237a20c2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-12 15:46:03 +0100

    Time: 3 ms (50.64%), Space: 18.8 MB (51.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,25 @@
+# uhm.. I can use inward traversal two pointers if its sorted list.
+# You may assume that each input would have exactly one solution,
+# not use the same element twice.
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+ nums[j] == target:
-                    return [i, j]
-        return []            
-              
+        d = {}
+        for i, n in enumerate(nums):
+            d[n] = i
+        # no dup
+        for i,n in enumerate(nums):
+            t = target - n
+            if t in d and i != d[t]:
+                return [i, d[t]]
+
+
+
+# brute force O(N^2)
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i]+ nums[j] == target:
+#                     return [i, j]
+#         return []            
             
