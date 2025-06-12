d8a774c67f878ac249068a2b477b1040253a3b4a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-12 11:57:39 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -1,14 +1,39 @@
+# v2: sliding window
+from collections import deque
 class MovingAverage:
 
     def __init__(self, size: int):
-        self.arr = []
+        self.window = deque()
+        self.window_sum = 0
+        self.window_size = 0
         self.size = size
 
     def next(self, val: int) -> float:
-        if len(self.arr) >= self.size:
-            self.arr = self.arr[1:]
-        self.arr.append(val)
-        return sum(self.arr) / len(self.arr)
+        poped = 0
+        if self.window_size >= self.size:
+            poped = self.window.popleft()
+            self.window_size -= 1
+
+        self.window.append(val)
+        self.window_size += 1
+        self.window_sum += val - poped
+        print(self.window)
+        print(self.window_sum)
+        return self.window_sum / self.window_size
+
+
+# just for anwer submit.. lets try another version with sliding window
+# class MovingAverage:
+
+#     def __init__(self, size: int):
+#         self.arr = []
+#         self.size = size
+
+#     def next(self, val: int) -> float:
+#         if len(self.arr) >= self.size:
+#             self.arr = self.arr[1:]
+#         self.arr.append(val)
+#         return sum(self.arr) / len(self.arr)
 
         
 
