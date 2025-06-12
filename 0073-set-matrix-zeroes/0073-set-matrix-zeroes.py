e5b0973c88be57aa9baf95861eee38d7518ee30d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-12 18:44:44 +0100

    Time: 4 ms (38.55%), Space: 18.5 MB (35.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,27 +1,79 @@
+# optimize to make 
+# time :
+# space :
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # first I need r,c for all 0?
-        rows = set()
-        cols = set()
+        # using markers into first row and column
+        # check there is already first row and column has zero
+
+        m, n = len(matrix), len(matrix[0])
+
+        # check first row has zero
+        first_row_has_zero = False
+        for c in range(n):
+            if matrix[0][c] == 0:
+                first_row_has_zero = True
+                break
         
-        m = len(matrix) # r
-        n = len(matrix[0]) # c
+        # check first col has zero
+        first_col_has_zero = False
         for r in range(m):
-            for c in range(n):
+            if matrix[r][0] == 0:
+                first_col_has_zero = True
+                break
+        
+        # mark first row or col if there is 0 in it
+        for r in range(1, m):
+            for c in range(1, n):
                 if matrix[r][c] == 0:
-                    rows.add(r)
-                    cols.add(c)
-        
-        for row in rows:
-            matrix[row] = [0] * n
-        
-        for col in cols:
-            for i in range(m): 
-                matrix[i][col] = 0
+                    matrix[0][c] = 0
+                    matrix[r][0] = 0
+
+        # update other row and col if there is marker
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        # if first row has zero already without marker, update too
+        if first_row_has_zero:
+            for c in range(n):
+                matrix[0][c] = 0
+
+        # col too
+        if first_col_has_zero:
+            for r in range(m):
+                matrix[r][0] = 0
 
 
+
+# time O(M * N)
+# space O(M + N)
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+#         # first I need r,c for all 0?
+#         rows = set()
+#         cols = set()
         
+#         m = len(matrix) # r
+#         n = len(matrix[0]) # c
+#         for r in range(m):
+#             for c in range(n):
+#                 if matrix[r][c] == 0:
+#                     rows.add(r)
+#                     cols.add(c)
         
+#         # reset whole rows into 0
+#         for row in rows:
+#             matrix[row] = [0] * n
+        
+#         # reset one by one in cols
+#         for col in cols:
+#             for i in range(m): 
+#                 matrix[i][col] = 0
