0a8767102648299a5d2c69fb850ab644d0972a14
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-12 10:16:56 +0100

    Time: 0 ms (100%), Space: 18 MB (18.84%) - LeetHub

---
+++ 0266-palindrome-permutation/0266-palindrome-permutation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        # palindrome has to have even nums
+        # add a letter into set and if there is the letter already, remove it
+        # check set size -> len s is odd -> size 1 true or false
+        s_set = set()
+        for a in s:
+            if not a in s_set:
+                s_set.add(a)
+            else:
+                s_set.remove(a)
+        
+        s_set_len = len(s_set)
+        s_len = len(s)
+        if s_len % 2 == 0 and s_set_len == 0:
+            return True
+        elif s_len % 2 != 0 and s_set_len == 1:
+            return True
+        
+        return False
+
+
+
+        
