277dc9678fc5d347b84400b6a823ed41aed23855
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-12 17:52:36 +0100

    Time: 7 ms (38.17%), Space: 18 MB (24.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,32 +1,96 @@
+# [".",".","4"   ,".",".","."   ,"6","3","."],
+# [".",".","."   ,".",".","."   ,".",".","."]
+# ["5",".","."   ,".",".","."   ,".","9","."]
+
+# [".",".","."   ,"5","6","."   ,".",".","."]
+# ["4",".","3"   ,".",".","."   ,".",".","1"]
+# [".",".","."   ,"7",".","."   ,".",".","."]
+
+
+# [".",".","."   ,"5",".","."   ,".",".","."]
+# [".",".","."   ,".",".","."   ,".",".","."]
+# [".",".","."   ,".",".","."   ,".",".","."]]
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:        
-         # 3x3x10 -> 10False in a box and 3 * 3 box
-        box_check = [[[False] * 10 for _ in range(3)] for _ in range(3)]
-
+        # should I break down by row, column and 3x3?
+        sub_boxes = [[set() for _ in range(3)] for _ in range(3)]
         for r in range(9):
-            row_check = [False] * 10
-            col_check = [False] * 10
-
+            row = set()
+            column = set()
             for c in range(9):
-                # -----------------------------
-                # row
-                if board[r][c] != ".":
-                    num = int(board[r][c])
-                    if row_check[num]:
+                # row check
+                r_num = board[r][c]
+                if r_num != ".":
+                    if r_num in row:
                         return False
-                    row_check[num] = True
-
-                    # 3x3 box
-                    box_r, box_c = r // 3, c // 3
-                    if box_check[box_r][box_c][num]:
+                    else:
+                        row.add(r_num)
+                # column check
+                c_num = board[c][r]
+                if c_num != ".":
+                    if c_num in column:
                         return False
-                    box_check[box_r][box_c][num] = True
+                    else:
+                        column.add(c_num)
 
-                # col
-                if board[c][r] != ".":
-                    num = int(board[c][r])
-                    if col_check[num]:
+                # how can I check sub box
+                # which data structure?
+                # there are 9 subboxes
+                # [r // 3][c // 3] -> one box!
+                if r_num != ".":
+                    if r_num in sub_boxes[r // 3][c // 3]:
                         return False
-                    col_check[num] = True
-
+                    else:
+                        sub_boxes[r // 3][c // 3].add(r_num)
+                # if c_num != "." and r != c:
+                #     if c_num in sub_boxes[c // 3][r // 3]:
+                #         return False
+                #     else:
+                #         sub_boxes[c // 3][r // 3].add(c_num)
+            # print(c)
+            # print(r)
         return True
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def isValidSudoku(self, board: List[List[str]]) -> bool:        
+#          # 3x3x10 -> 10False in a box and 3 * 3 box
+#         box_check = [[[False] * 10 for _ in range(3)] for _ in range(3)]
+
+#         for r in range(9):
+#             row_check = [False] * 10
+#             col_check = [False] * 10
+
+#             for c in range(9):
+#                 # -----------------------------
+#                 # row
+#                 if board[r][c] != ".":
+#                     num = int(board[r][c])
+#                     if row_check[num]:
+#                         return False
+#                     row_check[num] = True
+
+#                     # 3x3 box
+#                     box_r, box_c = r // 3, c // 3
+#                     if box_check[box_r][box_c][num]:
+#                         return False
+#                     box_check[box_r][box_c][num] = True
+
+#                 # col
+#                 if board[c][r] != ".":
+#                     num = int(board[c][r])
+#                     if col_check[num]:
+#                         return False
+#                     col_check[num] = True
+
+#         return True
