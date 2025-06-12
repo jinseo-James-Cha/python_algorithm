cfba53b025f37fcbf5c6b2f9fac84cbf0dd34ada
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-12 18:02:14 +0100

    Time: 3 ms (82.26%), Space: 17.8 MB (43.85%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,3 +1,27 @@
+# v2: optimize
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:   
+        rows = [set() for _ in range(9)]
+        columns = [set() for _ in range(9)]
+        subBoxes = [[set() for _ in range(3)] for _ in range(3)] # subBoxes[][] = set()
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+                if num == ".":
+                    continue
+                
+                if num in rows[r]:
+                    return False
+                if num in columns[c]:
+                    return False
+                if num in subBoxes[r // 3][c // 3]:
+                    return False
+                
+                rows[r].add(num)
+                columns[c].add(num)
+                subBoxes[r // 3][c // 3].add(num)
+        return True
+
 # [".",".","4"   ,".",".","."   ,"6","3","."],
 # [".",".","."   ,".",".","."   ,".",".","."]
 # ["5",".","."   ,".",".","."   ,".","9","."]
@@ -11,46 +35,46 @@
 # [".",".","."   ,".",".","."   ,".",".","."]
 # [".",".","."   ,".",".","."   ,".",".","."]]
 
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:        
-        # should I break down by row, column and 3x3?
-        sub_boxes = [[set() for _ in range(3)] for _ in range(3)]
-        for r in range(9):
-            row = set()
-            column = set()
-            for c in range(9):
-                # row check
-                r_num = board[r][c]
-                if r_num != ".":
-                    if r_num in row:
-                        return False
-                    else:
-                        row.add(r_num)
-                # column check
-                c_num = board[c][r]
-                if c_num != ".":
-                    if c_num in column:
-                        return False
-                    else:
-                        column.add(c_num)
+# class Solution:
+#     def isValidSudoku(self, board: List[List[str]]) -> bool:        
+#         # should I break down by row, column and 3x3?
+#         sub_boxes = [[set() for _ in range(3)] for _ in range(3)]
+#         for r in range(9):
+#             row = set()
+#             column = set()
+#             for c in range(9):
+#                 # row check
+#                 r_num = board[r][c]
+#                 if r_num != ".":
+#                     if r_num in row:
+#                         return False
+#                     else:
+#                         row.add(r_num)
+#                 # column check
+#                 c_num = board[c][r]
+#                 if c_num != ".":
+#                     if c_num in column:
+#                         return False
+#                     else:
+#                         column.add(c_num)
 
-                # how can I check sub box
-                # which data structure?
-                # there are 9 subboxes
-                # [r // 3][c // 3] -> one box!
-                if r_num != ".":
-                    if r_num in sub_boxes[r // 3][c // 3]:
-                        return False
-                    else:
-                        sub_boxes[r // 3][c // 3].add(r_num)
-                # if c_num != "." and r != c:
-                #     if c_num in sub_boxes[c // 3][r // 3]:
-                #         return False
-                #     else:
-                #         sub_boxes[c // 3][r // 3].add(c_num)
-            # print(c)
-            # print(r)
-        return True
+#                 # how can I check sub box
+#                 # which data structure?
+#                 # there are 9 subboxes
+#                 # [r // 3][c // 3] -> one box!
+#                 if r_num != ".":
+#                     if r_num in sub_boxes[r // 3][c // 3]:
+#                         return False
+#                     else:
+#                         sub_boxes[r // 3][c // 3].add(r_num)
+#                 # if c_num != "." and r != c:
+#                 #     if c_num in sub_boxes[c // 3][r // 3]:
+#                 #         return False
+#                 #     else:
+#                 #         sub_boxes[c // 3][r // 3].add(c_num)
+#             # print(c)
+#             # print(r)
+#         return True
 
 
 
