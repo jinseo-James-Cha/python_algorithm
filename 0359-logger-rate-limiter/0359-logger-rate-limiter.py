7c289f06833c6a99a264e4b1ef41de2b40afee8f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-13 10:12:39 +0100

    Time: 7 ms (81.71%), Space: 24.9 MB (36.73%) - LeetHub

---
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -0,0 +1,24 @@
+# at most every 10 seconds
+# t + 10 < next message
+# Several messages may arrive at the same timestamp.
+class Logger:
+
+    def __init__(self):
+        self.message_timestamp = {}
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if not message in self.message_timestamp:
+            self.message_timestamp[message] = timestamp + 10
+        
+        elif self.message_timestamp[message] > timestamp:
+            return False
+
+        else:
+            self.message_timestamp[message] = timestamp + 10
+
+        return True
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
