39fa1943c2e5f17a6ffbdf2ab88010ac99f1e38d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-15 00:52:33 +0100

    Time: 0 ms (100%), Space: 17.9 MB (31.73%) - LeetHub

--- 0734-sentence-similarity/0734-sentence-similarity.py
+++ 0734-sentence-similarity/0734-sentence-similarity.py
@@ -1,25 +1,35 @@
-
-# sentence1 = ["great","acting","skills"] 5 6 6
-# sentence2 = ["fine","drama","talent"] 4 5 5
-# similarPairs = [["great","fine"],["drama","acting"],["skills","talent"]]
-# True
 class Solution:
     def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
-        s1_len = len(sentence1)
-        s2_len = len(sentence2)
-        if s1_len != s2_len:
+        if len(sentence1) != len(sentence2):
             return False
-        
-        for i in range(s1_len):
-            # same word -> similar
-            if sentence1[i] == sentence2[i]:
+        wordToSimilarWords = defaultdict(set)
+        for x, y in similarPairs:
+            wordToSimilarWords[x].add(y)
+            wordToSimilarWords[y].add(x)
+        for i in range(len(sentence1)):
+            if sentence1[i] == sentence2[i] or sentence2[i] in wordToSimilarWords[sentence1[i]]:
                 continue
-            else: # if its different
-                flag = False
-                for x, y in similarPairs:
-                    if (x == sentence1[i] and y == sentence2[i]) or (x == sentence2[i] and y == sentence1[i]):
-                        flag = True
-                if not flag:
-                    return False
+            return False
         return True
+
+# My answer time : O(N^2) needs to optimize by choosing different data structure
+# class Solution:
+#     def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+#         s1_len = len(sentence1)
+#         s2_len = len(sentence2)
+#         if s1_len != s2_len:
+#             return False
+        
+#         for i in range(s1_len):
+#             # same word -> similar
+#             if sentence1[i] == sentence2[i]:
+#                 continue
+#             else: # if its different
+#                 flag = False
+#                 for x, y in similarPairs:
+#                     if (x == sentence1[i] and y == sentence2[i]) or (x == sentence2[i] and y == sentence1[i]):
+#                         flag = True
+#                 if not flag:
+#                     return False
+#         return True
         
