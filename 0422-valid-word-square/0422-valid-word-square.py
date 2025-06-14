827724091b640ec6993e205f126b3dd7a1b8130f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-14 23:26:37 +0100

    Time: 12 ms (88.29%), Space: 18.3 MB (59.8%) - LeetHub

---
+++ 0422-valid-word-square/0422-valid-word-square.py
@@ -0,0 +1,83 @@
+# let me do another idea
+# v3 : fill up with 0 and then compare them
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+        # standarize with row len
+        row_len = len(words)
+        col_len = len(words[0])
+        for w in words:
+            col_len = max(col_len, len(w))
+        max_len = max(row_len, col_len)
+
+        # make same rows if col is greater than row
+        # if col_len > row_len:
+        for i in range(max_len - row_len):
+            words.append("0" * col_len)
+        row_len = max_len
+        ## print(words)
+        ## return False
+
+        # make same cols if row is greater than col
+        for i in range(row_len):
+            if len(words[i]) != row_len:
+                words[i] += "0" * (row_len - len(words[i]))
+
+        # print(words)
+        # return False
+        for r in range(row_len):
+            for c in range(row_len):
+                if words[r][c] != words[c][r]:
+                    return False
+        return True
+
+# 1 <= words.length <= 500
+# 1 <= words[i].length <= 500
+
+# v2: let me compare one by one 
+# a,n,m,e is the same letter for row and column
+# 00 11 22 33 skip
+# 
+# class Solution:
+#     def validWordSquare(self, words: List[str]) -> bool:
+#         for r in range(len(words)):
+#             print(len(words[r]))
+#             for c in range(len(words[r])):
+#                 break
+#                 if r == c:
+#                     print(r,c)
+#                 # print(r,c)
+#                 # if words[r][c]:
+#                 #     print(words[r][c])
+#                 # if words[c][r]:
+#                 #     print(words[c][r])
+#                 #     # return False
+#         return True
+
+
+
+
+# I think its can be O(N^2) !
+# I can have rows to be compared, so I need colums!
+
+
+# 0 <= k < max(numRows, numColumns) this is the key
+# class Solution:
+#     def validWordSquare(self, words: List[str]) -> bool:
+#         columns = []
+#         for i in range(len(words[0])):
+#             j = 0
+#             temp_col = ""
+#             while j < len(words[i]):
+#                 # if not words[j][i]: is out of range
+#                 # need to check by len
+#                     # return False
+#                 temp_col += words[j][i]
+#                 j += 1
+#             columns.append(temp_col)
+#             print(columns)
+        
+#         # compare rows and colums
+#         for i in range(len(words)):
+#             if words[i] != columns[i]:
+#                 return False
+#         return True
