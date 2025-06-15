7209529ceb8b8f73318ec6f27c44cd769bcadb28
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-15 15:52:09 +0100

    Time: 0 ms (100%), Space: 17.6 MB (90.14%) - LeetHub

--- 0762-find-anagram-mappings/0762-find-anagram-mappings.py
+++ 0762-find-anagram-mappings/0762-find-anagram-mappings.py
@@ -1,9 +1,25 @@
+# using hashmap -> O(N)
+
+from collections import defaultdict
+
 class Solution:
     def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        mapping = []
-        for n1 in nums1:
-            for i,n2 in enumerate(nums2):
-                if n1 == n2:
-                    mapping.append(i)
-                    break
-        return mapping
+        hm = defaultdict(int)
+        for i, n2 in enumerate(nums2):
+            hm[n2] = i
+
+        for i, n1 in enumerate(nums1):
+            nums1[i] = hm[n1]
+
+        return nums1
+
+# brute force - > O(N^2)
+# class Solution:
+#     def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         mapping = []
+#         for n1 in nums1:
+#             for i,n2 in enumerate(nums2):
+#                 if n1 == n2:
+#                     mapping.append(i)
+#                     break
+#         return mapping
