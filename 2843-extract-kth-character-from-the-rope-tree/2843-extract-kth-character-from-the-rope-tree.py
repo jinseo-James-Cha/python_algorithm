d3d8ac872a27fb1b9c9f3012d41a3f7037a71a91
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-16 11:18:17 +0100

    Time: 65 ms (27.69%), Space: 28.9 MB (61.54%) - LeetHub

---
+++ 2843-extract-kth-character-from-the-rope-tree/2843-extract-kth-character-from-the-rope-tree.py
@@ -0,0 +1,42 @@
+# Definition for a rope tree node.
+# class RopeTreeNode(object):
+#     def __init__(self, len=0, val="", left=None, right=None):
+#         self.len = len
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getKthCharacter(self, root: Optional[object], k: int) -> str:
+        """
+        :type root: Optional[RopeTreeNode]
+        """
+        # yes ! I like Tree question
+        # node.val -> lowercase English letters (possibly empty)
+        # node.len -> non-negative integer
+
+        # types
+        # leaf : no children, len = 0, val is non-empty string
+        # internal: 1 or 2 children, len > 0, val is empty
+
+        # S[node]
+        # if leaf, S[node] = node.val
+        # it not, S[node] = concat(S[node.left], S[node.right]) and S[node].length = node.len
+        
+        # return k-th character of string S[root]
+
+        # implement all logic and then return value
+
+        # DFS will be useful to get leaves
+        # S = ""
+        def dfs(root: Optional[object]):
+            if not root:
+                return ""
+
+            if root.len == 0 and root.val:
+                return root.val
+            
+            return concat(dfs(root.left), dfs(root.right))
+            
+        S = dfs(root)
+        return S[k-1]
+
