c21075a850bf7dde08d889cdae57a8bc8716ce30
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-16 16:30:41 +0100

    Time: 0 ms (100%), Space: 17.8 MB (34.58%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -3,28 +3,50 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Time O(N)
+# class Solution:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#         # I think I can use two pointer and right - left = n
+#         # once right arrives the end
+#         # left arrives where we need to remove..
+#         # but we need to reach one node bofore the target node we will remove
+#         # head.next = head.next.next -> remove the next pointer.
+#         answer = ListNode(-1)
+#         answer.next = head
+#         left = right = answer
+#         # move right first
+#         for _ in range(n):
+#             right = right.next
+#             if not right:
+#                 return head
+
+#         while right.next:
+#             right = right.next
+#             left = left.next
+
+#         # remve the tarket node
+#         left.next = left.next.next
+
+#         return answer.next
+
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # I think I can use two pointer and right - left = n
-        # once right arrives the end
-        # left arrives where we need to remove..
-        # but we need to reach one node bofore the target node we will remove
-        # head.next = head.next.next -> remove the next pointer.
-        answer = ListNode(-1)
-        answer.next = head
-        left = right = answer
-        # move right first
-        for _ in range(n):
-            right = right.next
-            if not right:
-                return head
-
-        while right.next:
-            right = right.next
-            left = left.next
-
-        # remve the tarket node
-        left.next = left.next.next
-
-        return answer.next
-
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        first = dummy
+        second = dummy
+        # Advances first pointer so that the gap between first and second is n nodes apart
+        for i in range(n + 1):
+            first = first.next
+        # Move first to the end, maintaining the gap
+        while first is not None:
+            first = first.next
+            second = second.next
+        second.next = second.next.next
+        return dummy.next
