1820806e4a3ba6663eb90dd4f8fc58517b74caf5
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-16 18:17:21 +0100

    Time: 161 ms (32.18%), Space: 78.2 MB (90.21%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,59 @@
+# I like this problem, will solve again later !
+
+# I wanna use this deque...why not?!
+# ah no!
+# The functions get and put must each run in O(1) average time complexity.
+# insertions and deletions 0(1) -> linked list
+# we keep moving node in cache to the end if its used the most recent
+# finally, we use doubly linked list
+class DoublyLinkedListNode:
+    def __init__(self, key: int, val: int):
+        self.key = key
+        self.val = val
+        self.next = self.prev = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap = {}
+
+        self.head = DoublyLinkedListNode(-1, -1)
+        self.tail = DoublyLinkedListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def add_to_tail(self, node: DoublyLinkedListNode) -> None:
+        prev_node = self.tail.prev
+        node.prev = prev_node
+        node.next = self.tail
+        prev_node.next = node
+        self.tail.prev = node
+    
+    def remove_node(self, node: DoublyLinkedListNode) -> None:
+        node.prev.next = node.next
+        node.next.prev = node.prev    
+
+    def get(self, key: int) -> int:
+        if key not in self.hashmap:
+            return -1
+        
+        self.remove_node(self.hashmap[key])
+        self.add_to_tail(self.hashmap[key])
+        return self.hashmap[key].val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:
+            self.remove_node(self.hashmap[key])
+        node = DoublyLinkedListNode(key, value)
+        self.hashmap[key] = node
+
+        if len(self.hashmap) > self.capacity:
+            del self.hashmap[self.head.next.key]
+            self.remove_node(self.head.next)
+        self.add_to_tail(node)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
