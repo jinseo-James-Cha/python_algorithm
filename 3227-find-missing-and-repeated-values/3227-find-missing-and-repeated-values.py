6cf1b99c0e486c10bd019972059c0f7aeeb31fc4
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-19 16:31:21 +0100

    Time: 4 ms (81.9%), Space: 18.1 MB (63.17%) - LeetHub

---
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        # size n = len(grid)
+        # 1 ~ n**2
+        # ans[0] == a appears twice 
+        # ans[1] == b is missing
+        n = len(grid)
+        ans = [0] * 2
+        checked = [0] * ((n**2) + 1)
+        for i in range(n):
+            for num in grid[i]:
+                checked[num] += 1
+        
+        for i in range(len(checked)):
+            if checked[i] == 2:
+                ans[0] = i
+            if i != 0 and not checked[i]:
+                ans[1] = i
+        return ans
+                
