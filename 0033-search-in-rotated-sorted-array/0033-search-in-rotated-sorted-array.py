3fd185631c5680339420d7c1464e3f6948b11505
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-19 14:08:17 +0100

    Time: 0 ms (100%), Space: 18.2 MB (41.7%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,29 @@
+# There is an integer array nums sorted in ascending order (with distinct values).
+# and O(log n)
+# -> Binary Search ?!
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # using Binary search, but its partially not sorted !?
+        # like 12345 -> 45123
+        # one index has bigger than next index
+        # two parts of array 
+        
+        # need to check non-ascending order
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[left] <= nums[mid]:
+                # if nums[left:mid] is proper asceding order list
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return left if nums and nums[left] == target else -1
