b552750d877c8e83335d18c85df8b721c7db5c6d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-22 16:07:51 +0100

    Time: 198 ms (5.01%), Space: 48.2 MB (5.05%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # 12 13 11 23 21 31
+        # i != j -> nums[i] == nums[j] and abs(0 - 3) <= 3
+        # loop find index pairs of nums[i] == nums[j]
+        # loop for abs(i - j) <= k ?
+        # 1 <= nums.length <= 10**5 -> no O(N**2)
+
+        # try with hashmap-defaultdict(list) because it can be multiple indexs
+        index_pairs = defaultdict(list)
+        for index, num in enumerate(nums):
+            index_pairs[num].append(index)
+
+        print(index_pairs)
+
+        for key, value in index_pairs.items():
+            if len(value) < 2:
+                continue
+
+            for i in range(len(value) - 1):
+                for j in range(i + 1, len(value)):
+                    if abs(value[i] - value[j]) <= k:
+                        return True
+        
+        return False
+                
+
