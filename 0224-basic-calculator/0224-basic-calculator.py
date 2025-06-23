c11795fdf3b3418fdac552dbd4e17deb70f8d121
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-23 16:28:05 +0100

    Time: 24 ms (63.11%), Space: 18.3 MB (63.38%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,23 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        curr_num, sign, res = 0, 1, 0
+
+        for c in s:
+            if c.isdigit():
+                curr_num = curr_num * 10 + int(c)
+            elif c == '+' or c == '-':
+                res += curr_num * sign
+                sign = -1 if c == '-' else 1
+                curr_num = 0
+            elif c == '(':
+                stack.append(res)
+                stack.append(sign)
+                res, sign = 0, 1
+            elif c == ')':
+                res += sign * curr_num
+                
+                res *= stack.pop()
+                res += stack.pop()
+                curr_num = 0
+        return res + curr_num * sign
