05d85e48ab5b57645afd395d386951f76de26b07
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-23 20:56:57 +0100

    Time: 0 ms (100%), Space: 17.9 MB (26.76%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -10,24 +10,22 @@ class Solution:
         max_length = gcd(len(str1), len(str2))
         return str1[:max_length]
 
-class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        res = ""
+# class Solution:
+#     def gcdOfStrings(self, str1: str, str2: str) -> str:
+#         len1 = len(str1)
+#         len2 = len(str2)
 
-        len1 = len(str1)
-        len2 = len(str2)
-
-        def valid(k):
-            if len1 % k or len2 % k:
-                return False
-            n1, n2 = len1 // k, len2 // k
-            base = str1[:k]
-            return str1 == n1 * base and str2 == n2 * base
+#         def valid(k):
+#             if len1 % k or len2 % k:
+#                 return False
+#             n1, n2 = len1 // k, len2 // k
+#             base = str1[:k]
+#             return str1 == n1 * base and str2 == n2 * base
         
-        for i in range(min(len1, len2), 0, -1):
-            if valid(i):
-                return str1[:i]
-        return ""
+#         for i in range(min(len1, len2), 0, -1):
+#             if valid(i):
+#                 return str1[:i]
+#         return ""
 
 
 # class Solution:
