5099857845b6180527b6ed9118d18fbd43986462
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-24 17:02:28 +0100

    Time: 3 ms (52.01%), Space: 17.9 MB (63.06%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -4,13 +4,11 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         freqs = Counter(words)
         
+        # using tuple (1priority to compare, 2priority if 1st is the same value)
+        # using -freq to take "minimum" first but actually maximum
         max_heap = [(-freq, word) for word, freq in freqs.items()]
-        
-        print(max_heap)
 
+        # transform list -> heapq by tuple elements
         heapq.heapify(max_heap)
-
-        print(max_heap)
-
         return [heapq.heappop(max_heap)[1] for _ in range(k)]
     
