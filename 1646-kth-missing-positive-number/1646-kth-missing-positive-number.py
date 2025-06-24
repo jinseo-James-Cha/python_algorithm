cdeffe27751ded45ae32bac929714cecd8a6a2d2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-24 15:55:37 +0100

    Time: 0 ms (100%), Space: 18 MB (14.91%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
@@ -1,9 +1,25 @@
+# v2 : O(logN)
+# the sorted input should ring the bell -> Binary Search
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        # create res and fill up with missing num
+        left, right = 0, len(arr) - 1
+        while left <= right:
+            mid = (left + right) // 2
 
-        res = []
-        for i in range(1, 2001):
-            if i not in arr:
-                res.append(i)
-        return res[k-1]
+            if arr[mid] - mid - 1 < k:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left + k
+
+# O(N)
+# class Solution:
+#     def findKthPositive(self, arr: List[int], k: int) -> int:
+#         # create res and fill up with missing num
+
+#         res = []
+#         for i in range(1, 2001):
+#             if i not in arr:
+#                 res.append(i)
+#         return res[k-1]
