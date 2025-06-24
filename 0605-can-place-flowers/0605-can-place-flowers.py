8fe00a94a620622be7a9da1a6d4850ddbcf57607
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-24 14:03:36 +0100

    Time: 10 ms (45.14%), Space: 18.4 MB (14.45%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,3 +1,25 @@
+# v2
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+        for i in range(len(flowerbed)):
+            # Check if the current plot is empty.
+            if flowerbed[i] == 0:
+                # first two
+                empty_left_plot = (i == 0) or (flowerbed[i - 1] == 0)
+
+                # last two
+                empty_right_lot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+                
+                # If both plots are empty, we can plant a flower here.
+                if empty_left_plot and empty_right_lot:
+                    flowerbed[i] = 1
+                    count += 1
+                    if count >= n:
+                        return True
+                    
+        return count >= n
+
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         # flowers cannot be planted in adjacent plots.
