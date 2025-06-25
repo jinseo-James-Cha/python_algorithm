9a80983afaf27f24f4ef9a91183e3c7fe94e7f4d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-25 11:16:30 +0100

    Time: 0 ms (100%), Space: 17.8 MB (72.63%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -14,19 +14,19 @@ class Solution:
 
 
 # brute force O(n^2)
-class Solution:
-    def minimumOperations(self, nums: List[int]) -> int:
-        # ensure all in array is distinct
-        # operate - remove 3 elements from the beginning
-        # [1,2,3,4,2,3,3,5,7]
-        # [4,2,3,3,5,7]
-        # [3,5,7]
-        # len(set) == len(array) -> return?
-        count = 0
-        for i in range(0, len(nums),3):
-            arr = nums[i:]
-            s = set(arr)
-            if len(s) == len(arr):
-                return count
-            count += 1
-        return count
+# class Solution:
+#     def minimumOperations(self, nums: List[int]) -> int:
+#         # ensure all in array is distinct
+#         # operate - remove 3 elements from the beginning
+#         # [1,2,3,4,2,3,3,5,7]
+#         # [4,2,3,3,5,7]
+#         # [3,5,7]
+#         # len(set) == len(array) -> return?
+#         count = 0
+#         for i in range(0, len(nums),3):
+#             arr = nums[i:]
+#             s = set(arr)
+#             if len(s) == len(arr):
+#                 return count
+#             count += 1
+#         return count
