6b6915829ee1d80e52caa940a3a2b5373b44804a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-26 16:37:04 +0100

    Time: 27 ms (85.21%), Space: 20.5 MB (39.36%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -11,8 +11,7 @@ class Solution:
             curr_prefix_sum += num #
             if curr_prefix_sum - k in prefix_sum_map:
                 count += prefix_sum_map[curr_prefix_sum - k]
-            freq = prefix_sum_map.get(curr_prefix_sum, 0)
-            prefix_sum_map[curr_prefix_sum] = freq + 1
+            prefix_sum_map[curr_prefix_sum] = prefix_sum_map.get(curr_prefix_sum, 0) + 1
         return count
 
 
