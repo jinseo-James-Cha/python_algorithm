2e5167381ca7c74ea6950a91d1fcef27d388f6b1
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-26 16:36:55 +0100

    Time: 30 ms (68.41%), Space: 20.5 MB (39.36%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,34 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+
+        prefix_sum_map = {0: 1}
+        curr_prefix_sum = 0
+        # 1 1 1
+        # curr = 1 + 0
+        # 
+        for num in nums:
+            curr_prefix_sum += num #
+            if curr_prefix_sum - k in prefix_sum_map:
+                count += prefix_sum_map[curr_prefix_sum - k]
+            freq = prefix_sum_map.get(curr_prefix_sum, 0)
+            prefix_sum_map[curr_prefix_sum] = freq + 1
+        return count
+
+
+# using prefix_sum algorithm 
+# O(n^2) 
+# 1 <= nums.length <= 2 * 10^4 -> TLE
+# class Solution:
+#     def subarraySum(self, nums: List[int], k: int) -> int:
+#         res = 0
+        
+#         prefix_sum = [0] # for just prefix index itself
+#         for i in range(len(nums)):
+#             prefix_sum.append(prefix_sum[-1] + nums[i])
+        
+#         for j in range(1, len(nums) + 1):
+#             for i in range(1, j + 1):
+#                 if prefix_sum[j] - prefix_sum[i - 1] == k:
+#                     res += 1
+#         return res            
