750328af8f107609fb750fa03f70e369d4a67949
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-30 15:14:40 +0100

    Time: 311 ms (40.55%), Space: 18.2 MB (8.54%) - LeetHub

---
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.py
@@ -0,0 +1,37 @@
+from collections import Counter
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        # may contain duplicates
+        
+        # find all unique 
+        # 1. concatenation of three elements 
+        # 2. no leading zero
+        # 3. even number only
+        # return sorted array
+        
+        # 1, "2", 3 -> 2 is only even, so 132 312 len == 2
+        # "2" 1 3 "0" -> [102,120,130,132,210,230,302,310,312,320] len == 10
+        # 3 7 5 -> [] 0
+        
+        c = Counter(digits)   
+        res = []
+        for i in range(100, 999, 2):
+            s_i = str(i)
+            matching_count = 0
+            c = Counter(digits)
+            for a in s_i:
+                print(s_i)
+                print(c)
+                if int(a) not in c:
+                    break
+                else:
+                    if c[int(a)] == 0:
+                        break
+
+                    matching_count += 1
+                    c[int(a)] -= 1
+            if matching_count == 3:
+                res.append(int(s_i))
+        return res
+                
+
