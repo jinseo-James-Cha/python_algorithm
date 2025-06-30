21d7891b8b62f6d705896a702804e84272ab2f80
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-06-30 18:04:20 +0100

    Time: 293 ms (44.42%), Space: 17.6 MB (97.9%) - LeetHub

---
+++ 1656-count-good-triplets/1656-count-good-triplets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        ### Problem
+        # i < j < j, arr[i], arr[j], arr[k]
+        # abs(arr[i] - arr[j]) <= a
+        # ...
+
+        ### Solving
+        # 3 nested loops..? 
+        # no...uhm I wannt to do with 3 pointers.. -> no it cannot be j not moving..
+        # especially, k <= next element -> res += 1
+        res = 0
+        n = len(arr)
+        for i in range(n - 2):
+            for j in range(i+1, n - 1):
+                for k in range(j + 1, n):
+                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        res += 1
+        return res
+                    
+
