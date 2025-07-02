9c8103f93dcfd7218a96e172c6a0bc99a7dd08f2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-02 13:25:30 +0100

    Time: 2 ms (20.05%), Space: 17.8 MB (89.97%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,15 +1,21 @@
 from collections import defaultdict
 class Solution:
+    # v2
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        # count and then check number is taken or not?
-        occ = defaultdict(int)
-        for a in arr:
-            occ[a] += 1
+        freq = Counter(arr).values()
+        return len(freq) == len(set(freq))
+
+    # v1
+    # def uniqueOccurrences(self, arr: List[int]) -> bool:
+    #     # count and then check number is taken or not?
+    #     occ = defaultdict(int)
+    #     for a in arr:
+    #         occ[a] += 1
         
-        uniq = set()
-        for v in occ.values():
-            if v in uniq:
-                return False
-            uniq.add(v)
+    #     uniq = set()
+    #     for v in occ.values():
+    #         if v in uniq:
+    #             return False
+    #         uniq.add(v)
         
-        return True
+    #     return True
