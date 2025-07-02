6a2c748b86f1160559ef81c712e460d7de58f207
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-02 11:29:11 +0100

    Time: 27 ms (6.32%), Space: 20 MB (6.09%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+   
+    def transformStr(self, s: str) -> str:
+        isomorphic_pairs = {}
+        new_s = []
+        for i, c in enumerate(s):
+            if c not in isomorphic_pairs:
+                isomorphic_pairs[c] = i
+            new_s.append(str(isomorphic_pairs[c]))
+        return " ".join(new_s)
+            
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        print(self.transformStr(s))
+        print(self.transformStr(t))
+        return self.transformStr(s) == self.transformStr(t)
