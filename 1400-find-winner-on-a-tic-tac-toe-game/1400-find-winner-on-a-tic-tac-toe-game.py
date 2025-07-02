2a856aacc92067955727cd9f2de77f6507fc7b8a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-02 14:38:36 +0100

    Time: 3 ms (8.92%), Space: 17.6 MB (93.8%) - LeetHub

--- 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.py
+++ 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.py
@@ -3,23 +3,16 @@ class Solution:
 
         def rowCheck(row: int, board: List[List[int]]) -> bool:
             a,b,c = board[row]
-            if a == b == c:
-                return True
-            return False
+            return a != ' ' and a == b == c
 
         def colCheck(col: int, board: List[List[int]]) -> bool:
             a,b,c = [board[i][col] for i in range(3)]
-            if a == b == c:
-                return True
-            return False
+            return a != ' ' and a == b == c
         
         def diaCheck(board: List[List[int]]) -> bool:
-            a1,b1,c1 = [board[i][i] for i in range(3)]
-            if a1 != ' ' and a1 == b1 == c1:
+            if board[0][0] != ' ' and board[0][0] == board[1][1] == board[2][2]:
                 return True
-            
-            a2,b2,c2 = [board[0][2], board[1][1], board[2][0]]
-            if a2 != ' ' and a2 == b2 == c2:
+            if board[0][2] != ' ' and board[0][2] == board[1][1] == board[2][0]:
                 return True
             return False
         # 3 * 3
@@ -27,24 +20,12 @@ class Solution:
         # A -> 'X' , B-> 'O' into ' ' empty paces
         # end 1) 3 the same row, column, diagonal(cross)
         # end 2) there is non empty
-        board = [ [' '] * 3 for _ in range(3)]
-        count = 0
-        turn = True # True -> A and start from A
+        board = [[' '] * 3 for _ in range(3)]
+        turn = True  # True: A ("X"), False: B ("O")
+
         for x, y in moves:
-            if board[x][y] == ' ':
-                if turn:
-                    board[x][y] = "X"
-                else:
-                    board[x][y] = "O"
-            if rowCheck(x, board):
-                return "A" if turn else "B"
-            if colCheck(y, board):
-                return "A" if turn else "B"
-            if diaCheck(board):
+            board[x][y] = "X" if turn else "O"
+            if rowCheck(x, board) or colCheck(y, board) or diaCheck(board):
                 return "A" if turn else "B"
             turn = not turn
-            count += 1
-        
-        if count != 9:
-            return "Pending"
-        return "Draw"
+        return "Draw" if len(moves) == 9 else "Pending"
