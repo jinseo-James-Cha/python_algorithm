5c8a1b8614c4e7abb65b8cf452726a35672137df
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-02 17:21:00 +0100

    Time: 0 ms (100%), Space: 18.9 MB (69.49%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -15,3 +15,19 @@ class Solution:
             return targetSum == 0
         
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+
+        # DFS Stack
+        if not root:
+            return False
+        
+        de = [(root, targetSum - root.val)]
+        while de:
+            node, curr_sum = de.pop()
+            if not node.left and not node.right and curr_sum == 0:
+                return True
+            if node.right:
+                de.append((node.right, curr_sum - node.right.val))
+            if node.left:
+                de.append((node.left, curr_sum - node.left.val))
+
+        return False
