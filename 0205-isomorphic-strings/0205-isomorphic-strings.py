a83925a468a36dc8d9e828ccbdef6a29b245482d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-02 11:34:49 +0100

    Time: 3 ms (95.37%), Space: 18.2 MB (18.09%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,13 +1,27 @@
 class Solution:
-   
-    def transformStr(self, s: str) -> str:
-        isomorphic_pairs = {}
-        new_s = []
-        for i, c in enumerate(s):
-            if c not in isomorphic_pairs:
-                isomorphic_pairs[c] = i
-            new_s.append(str(isomorphic_pairs[c]))
-        return " ".join(new_s)
-            
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return self.transformStr(s) == self.transformStr(t)
+        char_map = {}
+
+        for sc, tc in zip(s, t):
+            if sc in char_map:
+                if char_map[sc] != tc:
+                    return False
+            elif tc in char_map.values():
+                return False
+
+            char_map[sc] = tc
+
+        return True
+
+# class Solution:
+#     def transformStr(self, s: str) -> str:
+#         isomorphic_pairs = {}
+#         new_s = []
+#         for i, c in enumerate(s):
+#             if c not in isomorphic_pairs:
+#                 isomorphic_pairs[c] = i
+#             new_s.append(str(isomorphic_pairs[c]))
+#         return " ".join(new_s)
+            
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         return self.transformStr(s) == self.transformStr(t)
