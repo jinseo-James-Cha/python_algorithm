12f86b54f7ace3014cecf764e727d5a78ed5ab96
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-07 12:23:30 +0100

    Time: 175 ms (60.03%), Space: 23.3 MB (13.58%) - LeetHub

--- 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.py
+++ 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.py
@@ -1,30 +1,51 @@
 class TwoSum:
-    # v2
-
+    # v2 : hash map
     def __init__(self):
-        self.arr = []
-        self.isSorted = False
+        self.nums = {}
 
     def add(self, number: int) -> None:
-        self.arr.append(number)
-        self.isSorted = False
+        if number in self.nums:
+            self.nums[number] += 1
+        else:
+            self.nums[number] = 1
 
     def find(self, value: int) -> bool:
-        if not self.isSorted:
-            self.arr.sort()
-            self.isSorted = True
-
-        left = 0
-        right = len(self.arr) - 1
-
-        while left < right:
-            if value > self.arr[left] + self.arr[right]:
-                left += 1
-            elif value < self.arr[left] + self.arr[right]:
-                right -= 1
-            else:
+        for k in self.nums.keys():
+            x = value - k
+            # not the same number
+            # like 14 and 7 and 7 
+            if k != x:
+                if x in self.nums:
+                    return True
+            elif self.nums[k] > 1:
                 return True
         return False
+
+    # v1 : sorted list
+    # def __init__(self):
+    #     self.arr = []
+    #     self.isSorted = False
+
+    # def add(self, number: int) -> None:
+    #     self.arr.append(number)
+    #     self.isSorted = False
+
+    # def find(self, value: int) -> bool:
+    #     if not self.isSorted:
+    #         self.arr.sort()
+    #         self.isSorted = True
+
+    #     left = 0
+    #     right = len(self.arr) - 1
+
+    #     while left < right:
+    #         if value > self.arr[left] + self.arr[right]:
+    #             left += 1
+    #         elif value < self.arr[left] + self.arr[right]:
+    #             right -= 1
+    #         else:
+    #             return True
+    #     return False
 
 # add 1 3 5
 # find 4 -> 1, 3 -> True
