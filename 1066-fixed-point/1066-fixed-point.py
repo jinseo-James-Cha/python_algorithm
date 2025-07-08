729845d187f1f9370a24d1c06e257a57617b7b64
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-08 18:38:11 +0100

    Time: 3 ms (2.42%), Space: 18.7 MB (32.73%) - LeetHub

--- 1066-fixed-point/1066-fixed-point.py
+++ 1066-fixed-point/1066-fixed-point.py
@@ -3,15 +3,25 @@ class Solution:
         # distinct intergers in asceding order
         # arr[i] == i return i or -1
 
-        # arr[i]
+        # v2 Binary search
+        left = 0
+        right = len(arr) - 1
+        res = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == mid:
+                res = mid
+                right = mid - 1
+            elif arr[mid] < mid:
+                left = mid + 1
+            else:
+                right = mid - 1
 
-
-        for i, n in enumerate(arr):
-            if n < 0:
-                continue
-
-            if i == n:
-                return i
-
-
-        return -1
+        return res
+        # v1
+        # for i, n in enumerate(arr):
+        #     if n < 0:
+        #         continue
+        #     if i == n:
+        #         return i
+        # return -1
