d979f82049853c6ff31b20ed30b3f5cee6d6c232
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-08 13:46:11 +0100

    Time: 0 ms (100%), Space: 18 MB (15.17%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,27 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        # s follows the same pattern
+        
+        # follow -> full match
+        # Each letter in pattern maps to exactly one unique word in s.
+        # Each unique word in s maps to exactly one letter in pattern.
+
+        # hashmap ? hashmap[pattern[i]] = s[i]
+
+        hashmap = {}
+        registered = set()
+        s_array = s.split(" ")
+        if len(pattern) != len(s_array):
+            return False
+        
+        for i, p in enumerate(pattern):
+            if p not in hashmap:
+                if s_array[i] not in registered:
+                    registered.add(s_array[i])
+                    hashmap[p] = s_array[i]
+                else:
+                    return False
+            # already registered? compare with previous
+            elif hashmap[p] != s_array[i]:
+                return False
+        return True
