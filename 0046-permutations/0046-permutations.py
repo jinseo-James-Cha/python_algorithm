6331ae7d96939ede76326e74a4b8b70edf3faeaf
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-09 17:22:39 +0100

    Time: 3 ms (32.01%), Space: 18.1 MB (33.05%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # backtracking
+        res = []
+        self.backtrack(nums, [], set(), res)
+        return res
+
+    def backtrack(self, nums: List[int], candidate: List[int], used: Set[int], res: List[List[int]]) -> None:
+        # base case
+        if len(candidate) == len(nums):
+            res.append(candidate[:])
+            return
+
+        # dfs feeling
+        for num in nums:
+            if num not in used:
+                candidate.append(num)
+                used.add(num)
+
+                self.backtrack(nums, candidate, used, res)
+
+                candidate.pop()
+                used.remove(num)
+
+    
