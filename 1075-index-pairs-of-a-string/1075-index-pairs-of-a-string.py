db878b173dc76b6aca6c6f68525e883d6dd1f98f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-09 12:02:05 +0100

    Time: 3 ms (70.78%), Space: 18.2 MB (22.08%) - LeetHub

--- 1075-index-pairs-of-a-string/1075-index-pairs-of-a-string.py
+++ 1075-index-pairs-of-a-string/1075-index-pairs-of-a-string.py
@@ -1,5 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for w in word:
+            if w not in node.children:
+                node.children[w] = TrieNode()
+            node = node.children[w]
+        node.is_word = True
+
 class Solution:
     def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
+        # v2 Trie - prefix tree
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        
+        res = []
+        for i in range(len(text)):
+            node = trie.root
+            for j in range(i, len(text)):
+                if text[j] not in node.children:
+                    break
+                node = node.children[text[j]]
+                if node.is_word:
+                    res.append([i, j])
+        return res
+
+        # V1
         # get index i and j to match with each word in words
         
         # need to think duplicate i 
@@ -8,16 +42,16 @@ class Solution:
         
         # constraints : All the strings of words are unique
 
-        res = set()
+        # res = set()
 
-        for word in words:
-            first = word[0]
-            for i, t in enumerate(text):
-                if first == t:
-                    j =  i + len(word)
-                    if word == text[i:j]:
-                        res.add((i,j - 1))
+        # for word in words:
+        #     first = word[0]
+        #     for i, t in enumerate(text):
+        #         if first == t:
+        #             j =  i + len(word)
+        #             if word == text[i:j]:
+        #                 res.add((i,j - 1))
         
-        res = list(res)
-        res.sort(key= lambda x: (x[0], x[1]))
-        return res
+        # res = list(res)
+        # res.sort(key= lambda x: (x[0], x[1]))
+        # return res
