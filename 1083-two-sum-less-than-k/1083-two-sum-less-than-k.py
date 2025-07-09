a556bc0e79a1c83367b143e9843bb949be5542cb
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-09 13:03:27 +0100

    Time: 0 ms (100%), Space: 17.9 MB (19.18%) - LeetHub

--- 1083-two-sum-less-than-k/1083-two-sum-less-than-k.py
+++ 1083-two-sum-less-than-k/1083-two-sum-less-than-k.py
@@ -1,13 +1,29 @@
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
-        # i < j? matters?
+        # v2 two pointers
+        nums.sort()
+        res = -1
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            s = nums[left] + nums[right]
+            if s < k:
+                res = max(res, s)
+                left += 1
+            else:
+                right -= 1
+        return res
+
+
+
+        # i < j? matters? no
         # nums[i] + nums[j] = sum < k
         # I can do with O(n^2)..
         # let me start with brute force
         #
-        res = -1 # max(res, X) if X < k
-        for i in range(len(nums) - 1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] < k:
-                    res = max(res, nums[i] + nums[j])
-        return res
+        # res = -1 # max(res, X) if X < k
+        # for i in range(len(nums) - 1):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] < k:
+        #             res = max(res, nums[i] + nums[j])
+        # return res
