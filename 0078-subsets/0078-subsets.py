f20f140839bafb647fa660ad8d8ed118a48a9899
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-09 18:28:15 +0100

    Time: 153 ms (1.8%), Space: 18.6 MB (21.34%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.backtrack(0, [], nums, res)
+        return res
+    
+    def backtrack(self, i: int, curr_subset: List[int], nums: List[int], res: List[List[int]]) -> None:
+        # base case: if all elements have been considered, add the current subset to the output
+        print(i)
+        if i == len(nums):
+            res.append(curr_subset[:])
+            print(res)
+            return
+
+        # include the current element 
+        # and recursively explore all paths that branch from this subset
+        curr_subset.append(nums[i])
+        self.backtrack(i + 1, curr_subset, nums, res)
+
+        # exclude the current element
+        # and recursively explore all paths that branch from this subset
+        curr_subset.pop()
+        self.backtrack(i + 1, curr_subset, nums, res)
+
