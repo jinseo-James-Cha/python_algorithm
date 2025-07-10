7c2bf954e2ed681138e8295e3f3b1d32ab6bcf3b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-10 13:04:40 +0100

    Time: 0 ms (100%), Space: 18 MB (33.04%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -6,19 +6,24 @@ class Solution:
         return res
 
     def backtrack(self, nums: List[int], candidate: List[int], used: Set[int], res: List[List[int]]) -> None:
-        # base case
+        # base case / termination condition
         if len(candidate) == len(nums):
+            # process solution
             res.append(candidate[:])
             return
 
-        # dfs feeling
-        for num in nums:
+        for num in nums: 
             if num not in used:
-                candidate.append(num)
+                # make a decision
+                # add num
+                candidate.append(num) 
                 used.add(num)
 
+                # move further 
                 self.backtrack(nums, candidate, used, res)
 
+                # undo decition 
+                # backtrack
                 candidate.pop()
                 used.remove(num)
 
