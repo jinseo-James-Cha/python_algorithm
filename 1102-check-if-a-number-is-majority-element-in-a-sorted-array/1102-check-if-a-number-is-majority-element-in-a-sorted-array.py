64c9f87a6d40b79854f242b431041db1979b95c9
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-11 12:50:01 +0100

    Time: 0 ms (100%), Space: 18 MB (27.04%) - LeetHub

--- 1102-check-if-a-number-is-majority-element-in-a-sorted-array/1102-check-if-a-number-is-majority-element-in-a-sorted-array.py
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/1102-check-if-a-number-is-majority-element-in-a-sorted-array.py
@@ -21,12 +21,40 @@ class Solution:
         # 4. data structure
         # int count, mid, left, right
 
-        # count = 0
-        # left = 0
-        # right = len(nums) # to get bound using len instead of len - 1 
-        # while left < right: # using < for bound question instead of left <= right
-        #     # how to get index of lower and upper bound? 
-        #     # let me use a module
+        # v2: implement lower bound and upper bound function myself
+        def lower_bound(nums: List[int], target: int) -> int: # 포함하는 index구하기
+            left = 0
+            right = len(nums)
+            res = 0
+            while left < right:
+                mid = (left + right) // 2
+                if nums[mid] < target: # target이 클때만 옮긴다는 같을때는 오른쪽만 이동시키는거야~ right가 움직이니깐 자연스럽게 계속 왼쪽을 체크하겠지
+                    left = mid + 1
+                else: # 타켓이 작거나 같으면 우리는 계속해서 밑으로 내려가면서 체크해야겠지 mid 포함해서 
+                    right = mid
+            return left
 
-        count =  bisect_right(nums, target) - bisect_left(nums, target)
-        return len(nums) / 2 < count
+        def upper_bound(nums: List[int], target: int) -> int: # 초과하는 index구하기
+            left = 0
+            right = len(nums)
+            while left < right:
+                mid = (left + right) // 2
+                if nums[mid] <= target: # 타켓하고 같을때마저 left를 옮겨버려 그래서 초과 index가 나오겠지
+                    left = mid + 1
+                else:
+                    right = mid
+
+            return left
+        
+        lower_index = lower_bound(nums, target)
+        upper_index = upper_bound(nums, target)
+
+        return len(nums) / 2 < upper_index - lower_index
+
+
+
+
+        # v1 : I remember the binary search module, so it is easy
+        # what if I cannot use the module?
+        # count =  bisect_right(nums, target) - bisect_left(nums, target)
+        # return len(nums) / 2 < count
