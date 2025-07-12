cda9783899d733ad116fececd7fd700cec2d4e8e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-12 23:30:37 +0100

    Time: 131 ms (77.84%), Space: 59.6 MB (86.79%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,31 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        # 1. intuition
+        # I don't know the loop's depths -> backtracking?
+        # no duplications -> [1,2] == [2, 1]
+        # using for loop in range(1, n+1)
+        # 1 -> 2 and pop and call backtrack 1 -> 3 ~~~
+        # len == k -> put res and pop
+
+        # 2. complexity
+        # Backtrack: no duplication -> N^N
+        # 20^20 -> is it possible to use backtrack?
+
+        # 3. data structure
+        # res: [][] no duplication
+        # cur: []
+
+        def backtrack(start: int, cur: List[int]):
+            if len(cur) == k:
+                res.append(cur[:])
+                return
+            for i in range(start, n + 1):
+                cur.append(i)
+                backtrack(i + 1, cur)
+                cur.pop()
+        
+        res = []
+        backtrack(1, [])
+        return res
+    
+    
