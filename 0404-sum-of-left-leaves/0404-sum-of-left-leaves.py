30c53b0727d57d58d46dcafaaca35fbcb99058b2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-13 17:04:00 +0100

    Time: 0 ms (100%), Space: 18.2 MB (28.93%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+    
+
+        res = 0
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+                if not node.left.left and not node.left.right:
+                    res += node.left.val
+            if node.right:
+                queue.append(node.right)
+        return res
