cd52491d60027b2903ba8e8b5f2a665b49afae99
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-14 12:17:01 +0100

    Time: 0 ms (100%), Space: 19.4 MB (65.34%) - LeetHub

--- 1618-delete-n-nodes-after-m-nodes-of-a-linked-list/1618-delete-n-nodes-after-m-nodes-of-a-linked-list.py
+++ 1618-delete-n-nodes-after-m-nodes-of-a-linked-list/1618-delete-n-nodes-after-m-nodes-of-a-linked-list.py
@@ -5,20 +5,20 @@
 #         self.next = next
 class Solution:
     def deleteNodes(self, head: Optional[ListNode], m: int, n: int) -> Optional[ListNode]:
-        dummy = ListNode()
-        dummy.next = head
-        i = 0
-        while head:
-            if i < m-1: # 0 < 2-1
-                i += 1 # 1
-            else:
-                j = 0
-                while j < n and head.next: # 0 < 3 
-                    head.next = head.next.next
-                    j += 1
-                i = 0
-            head = head.next
-        return dummy.next
+        # dummy = ListNode()
+        # dummy.next = head
+        # i = 0
+        # while head:
+        #     if i < m-1: # 0 < 2-1
+        #         i += 1 # 1
+        #     else:
+        #         j = 0
+        #         while j < n and head.next: # 0 < 3 
+        #             head.next = head.next.next
+        #             j += 1
+        #         i = 0
+        #     head = head.next
+        # return dummy.next
 
         # left moves m
         # copy left into right
@@ -27,25 +27,31 @@ class Solution:
 
         # back to first again
         # til right reach end before n -> left.next and end
-        # dummy = ListNode()
-        # dummy.next = head
+        dummy = ListNode()
+        dummy.next = head
 
-        # left = dummy
-        # while left.next:
-        #     i = 0
-        #     while m > i and left.next:        
-        #         left = left.next
-        #         i +=1 
+        left = dummy
+        while left.next:
+            # move left as many as m
+            i = 0
+            while m > i and left.next:        
+                left = left.next
+                i +=1 
 
-        #     right = left
-        #     j = 0
-        #     while n > j and right.next:
-        #         right = right.next
-        #         j += 1
+            # copy left to right
+            right = left
+
+            # move right as many as n
+            j = 0
+            while n > j and right.next:
+                right = right.next
+                j += 1
             
-        #     if n == j:
-        #         left.next = right.next
-        #     else:
-        #         left.next = None
+            # if j moved as many as n, remove in between
+            # if not, it is the end
+            if n == j:
+                left.next = right.next
+            else:
+                left.next = None
         
-        # return dummy.next
+        return dummy.next
