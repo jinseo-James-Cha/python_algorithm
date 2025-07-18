b91de5545ed47c60a5e39b008fdcdb119902b3d2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-18 22:31:19 +0100

    Time: 579 ms (49.28%), Space: 140.6 MB (16.66%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -1,15 +1,15 @@
 from collections import defaultdict, deque
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        # DFS
-
-
-
-
-        # making adjacent list and check
-        # using hashmap and num, list
-        # if source == destination:
-        #     return True
+        def dfs(node: int) -> bool:
+            if node == destination:
+                return True
+            for neighbor in adjacent_list[node]:
+                if not seen[neighbor]:
+                    seen[neighbor] = True
+                    if dfs(neighbor):
+                        return True
+            return False
 
         adjacent_list = defaultdict(list)
         
@@ -20,16 +20,31 @@ class Solution:
         # # BFS
         seen = [False] * n
         seen[source] = True
-        queue = deque([source])
+        return dfs(source)
 
-        while queue:
-            curr_node = queue.popleft()
-            if curr_node == destination:
-                return True
+
+
+        # making adjacent list and check
+        # using hashmap and num, list
+        # adjacent_list = defaultdict(list)
+        
+        # for u, v in edges:
+        #     adjacent_list[u].append(v)
+        #     adjacent_list[v].append(u)
+
+        # # # BFS
+        # seen = [False] * n
+        # seen[source] = True
+        # queue = deque([source])
+
+        # while queue:
+        #     curr_node = queue.popleft()
+        #     if curr_node == destination:
+        #         return True
             
-            for next_node in adjacent_list[curr_node]:
-                if not seen[next_node]:
-                    seen[next_node] = True
-                    queue.append(next_node)
+        #     for next_node in adjacent_list[curr_node]:
+        #         if not seen[next_node]:
+        #             seen[next_node] = True
+        #             queue.append(next_node)
 
-        return False
+        # return False
