7c8acccc0d1e9f0f9d6716bce231fe10acfbda98
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-22 12:15:09 +0100

    Time: 0 ms (100%), Space: 17.8 MB (34.31%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -6,13 +6,44 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        def dfs(node: TreeNode):
-            if not node:
-                return
-            res.append(node.val)
-            dfs(node.left)
-            dfs(node.right)
-        
+        # v2 : Morris -> space(1)
         res = []
-        dfs(root)
+        current = root
+        while current:
+            if current.left is None:
+                res.append(current.val)
+                current = current.right
+            else:
+                predecessor = current.left
+                # move to the left subtree's rightmost
+                while predecessor.right and predecessor.right != current:
+                    predecessor = predecessor.right
+
+                # first visit -> create link
+                if predecessor.right is None:
+                    predecessor.right = current
+                    res.append(current.val)
+                    current = current.left
+                else:
+                    # second bisit -> remove link and move to right
+                    predecessor.right = None
+                    current = current.right
         return res
+
+                
+
+
+
+
+
+        # v1: dfs
+        # def dfs(node: TreeNode):
+        #     if not node:
+        #         return
+        #     res.append(node.val)
+        #     dfs(node.left)
+        #     dfs(node.right)
+        
+        # res = []
+        # dfs(root)
+        # return res
