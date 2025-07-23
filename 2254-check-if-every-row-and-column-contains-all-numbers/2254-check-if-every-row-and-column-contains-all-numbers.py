8950cf1f61ee97030a7f9b74c01406d8ee318485
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-23 11:43:16 +0100

    Time: 11 ms (96.14%), Space: 18.5 MB (55.57%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.py
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.py
@@ -1,17 +1,23 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
-        # n * n
-        # is it \ the same / all different?
-
-        # brute force 
         n = len(matrix)
-        for r in range(n):
-            r_set = set()
-            c_set = set()
-            for c in range(n):
-                r_set.add(matrix[r][c])
-                c_set.add(matrix[c][r])
-            if len(r_set) != n or len(c_set) != n:
+        for row, col in zip(matrix, zip(*matrix)):
+            if len(set(row)) != n or len(set(col)) != n:
                 return False
         return True
+
+
+        # n * n
+        # is it \ the same / all different?
+        # brute force O(n^2)
+        # n = len(matrix)
+        # for r in range(n):
+        #     r_set = set()
+        #     c_set = set()
+        #     for c in range(n):
+        #         r_set.add(matrix[r][c])
+        #         c_set.add(matrix[c][r])
+        #     if len(r_set) != n or len(c_set) != n:
+        #         return False
+        # return True
             
