5db5648dd91ad8400ed2eb7c93a704fcef445da7
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-23 16:57:47 +0100

    Time: 3 ms (25.49%), Space: 17.8 MB (66.26%) - LeetHub

--- 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.py
+++ 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.py
@@ -4,21 +4,14 @@ class Solution:
         def convertToBit(x: int) -> List[int]:
             res = []
             while x > 0:
-                remainder = x % 2
-                res.append(remainder)
+                res.append(x % 2)
                 x //= 2
-            return res
+            return res 
 
         bit_n = convertToBit(n)
-        print(bit_n)
         res = [0, 0]
-        for i, n in enumerate(bit_n):
-            if n == 0:
-                continue
-            
-            if i % 2 == 0:
-                res[0] += 1
-            else:
-                res[1] += 1
+        for i, bit in enumerate(bit_n):
+            if bit == 1:
+                res[i % 2] += 1
         return res
             
