c4f48e2d4b49bb6a559682e340350c141ecb5a3a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-28 23:29:05 +0100

    Time: 14 ms (84.33%), Space: 17.7 MB (81.92%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -19,22 +19,22 @@ class Solution:
 
 # v2 dynamic sliding window
 # hash set
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        longest = 0
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         longest = 0
 
-        unique_str = set()
-        left = 0
-        for right in range(len(s)):
-            while s[right] in unique_str:
-                unique_str.remove(s[left])
-                left += 1
-            unique_str.add(s[right])
-            longest = max(longest, right - left + 1)
-        return longest
+#         unique_str = set()
+#         left = 0
+#         for right in range(len(s)):
+#             while s[right] in unique_str:
+#                 unique_str.remove(s[left])
+#                 left += 1
+#             unique_str.add(s[right])
+#             longest = max(longest, right - left + 1)
+#         return longest
 
 
-        return longest
+#         return longest
 
 # sliding window feeling !
 # class Solution:
