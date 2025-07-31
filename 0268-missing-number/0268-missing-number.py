c47c07733e9392404861b8f13030131af741339c
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-07-31 15:22:57 +0100

    Time: 6 ms (39.79%), Space: 18.8 MB (51.2%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,13 +2,24 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         # Follow up: 
         # Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+        checked = [False] * (len(nums) + 1)
+        for n in nums:
+            checked[n] = True
+        
+        res = len(nums)
+        for i, c in enumerate(checked):
+            if c == False:
+                res = i
+        return res
 
 
         # v1
-        nums.sort()
-        res = len(nums)
-        for i, num in enumerate(nums):
-            if i != num:
-                res = i
-                break
-        return res
+        # time: O(n log n)
+        # space: O(n)
+        # nums.sort()
+        # res = len(nums)
+        # for i, num in enumerate(nums):
+        #     if i != num:
+        #         res = i
+        #         break
+        # return res
