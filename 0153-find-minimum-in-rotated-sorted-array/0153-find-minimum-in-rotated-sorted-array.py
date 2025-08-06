86feab852af24ead64d0dd5c867e69c76a80b6fc
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-06 12:12:33 +0100

    Time: 0 ms (100%), Space: 18.3 MB (9.32%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -8,13 +8,35 @@ class Solution:
         # 4 5 6 7 0 1 2 3
         # unique elements
         # return minimum element in this array
+        if len(nums) == 1:
+            return nums[0]
+        
+        if nums[-1] > nums[0]:
+            return nums[0]
+        
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            
+            if nums[mid] < nums[mid - 1]:
+                return nums[mid]
+
+            if nums[mid] > nums[0]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
 
 
         # brute force
         # O(n)
-        res = nums[0]
-        for i in range(1, len(nums)):
-            if nums[i] < nums[i-1]:
-                res = nums[i]
-                break
-        return res
+        # res = nums[0]
+        # for i in range(1, len(nums)):
+        #     if nums[i] < nums[i-1]:
+        #         res = nums[i]
+        #         break
+        # return res
