6286a8cf6dcd0f11bab3a4c9eff5d555708ba734
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-09 16:01:19 +0100

    Time: 51 ms (39.83%), Space: 18.6 MB (68.89%) - LeetHub

---
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        # the main point : how can I have distinct ...?
+        # using set to save the shape?
+        # r c - new r c
+        n, m = len(grid), len(grid[0])
+
+        def is_within_bounds(r, c):
+            return 0 <= r < len(grid) and 0 <= c < len(grid[0])
+
+        DIR = [(-1,0), (1,0), (0,-1), (0,1)]
+        def dfs(r, c, start_r, start_c, island):
+            grid[r][c] = -1
+
+            for dy, dx in DIR:
+                next_r, next_c = r + dy, c + dx
+                if is_within_bounds(next_r, next_c) and grid[next_r][next_c] == 1:
+                    island.append((next_r - start_r, next_c - start_c))
+                    dfs(next_r, next_c, start_r, start_c, island)
+            
+            return island
+
+        res = []
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] == 1:
+                    current = dfs(r, c, r, c, [(0,0)])
+                    if current not in res:
+                        res.append(current)
+        return len(res)
