bb6165caf974de9867ff25e7978b26195f713d94
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-09 16:06:37 +0100

    Time: 39 ms (65.68%), Space: 18.6 MB (68.89%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -9,21 +9,20 @@ class Solution:
             return 0 <= r < len(grid) and 0 <= c < len(grid[0])
 
         DIR = [(-1,0), (1,0), (0,-1), (0,1)]
-        def dfs(r, c, start_r, start_c, island):
+        def dfs(r, c, start_r, start_c, shape):
             grid[r][c] = -1
 
             for dy, dx in DIR:
                 next_r, next_c = r + dy, c + dx
                 if is_within_bounds(next_r, next_c) and grid[next_r][next_c] == 1:
-                    island.append((next_r - start_r, next_c - start_c))
-                    dfs(next_r, next_c, start_r, start_c, island)
+                    shape.append((next_r - start_r, next_c - start_c))
+                    dfs(next_r, next_c, start_r, start_c, shape)
             
-            return island
-
         res = set()
         for r in range(n):
             for c in range(m):
                 if grid[r][c] == 1:
-                    current = dfs(r, c, r, c, [(0,0)])
-                    res.add(tuple(current))
+                    shape = [(0,0)]
+                    dfs(r, c, r, c, shape)
+                    res.add(tuple(shape))
         return len(res)
