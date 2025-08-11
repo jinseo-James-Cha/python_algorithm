53d7341237b1842a3c6ebff47516df00e3a53f97
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-11 17:08:01 +0100

    Time: 0 ms (100%), Space: 18.1 MB (33.66%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,30 +1,57 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # backtracking
         res = []
-        self.backtrack(nums, [], set(), res)
+        seen = set()
+
+        def backtrack(current, nums):
+            if len(current) == len(nums):
+                res.append(current[:])
+            
+            for num in nums:
+                if num not in seen:
+                    current.append(num)
+                    seen.add(num)
+                    backtrack(current, nums)
+                    current.pop()
+                    seen.remove(num)
+        
+        backtrack([], nums)
         return res
 
-    def backtrack(self, nums: List[int], candidate: List[int], used: Set[int], res: List[List[int]]) -> None:
-        # base case / termination condition
-        if len(candidate) == len(nums):
-            # process solution
-            res.append(candidate[:])
-            return
 
-        for num in nums: 
-            if num not in used:
-                # make a decision
-                # add num
-                candidate.append(num) 
-                used.add(num)
 
-                # move further 
-                self.backtrack(nums, candidate, used, res)
 
-                # undo decition 
-                # backtrack
-                candidate.pop()
-                used.remove(num)
+
+
+
+
+
+
+    #     # backtracking
+    #     res = []
+    #     self.backtrack(nums, [], set(), res)
+    #     return res
+
+    # def backtrack(self, nums: List[int], candidate: List[int], used: Set[int], res: List[List[int]]) -> None:
+    #     # base case / termination condition
+    #     if len(candidate) == len(nums):
+    #         # process solution
+    #         res.append(candidate[:])
+    #         return
+
+    #     for num in nums: 
+    #         if num not in used:
+    #             # make a decision
+    #             # add num
+    #             candidate.append(num) 
+    #             used.add(num)
+
+    #             # move further 
+    #             self.backtrack(nums, candidate, used, res)
+
+    #             # undo decition 
+    #             # backtrack
+    #             candidate.pop()
+    #             used.remove(num)
 
     
