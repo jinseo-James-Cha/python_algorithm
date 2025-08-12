37170492564b1b0aeed7b83ee0ddf142d4fb534e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-12 15:51:13 +0100

    Time: 13 ms (74.2%), Space: 18 MB (55.33%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,5 +1,18 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        subs = [nums[0]]
+        for num in nums:
+            if num > subs[-1]:
+                subs.append(num)
+            else:
+                i = 0
+                while num > subs[i]:
+                    i += 1
+                subs[i] = num
+        return len(subs)
+
+
+
         # maximum ?! or minimum ?! -> is it DP ?
 
         # 1. dp[i]
@@ -9,9 +22,9 @@ class Solution:
 
         # 3. base case
         
-        dp = [1] * len(nums)
-        for i in range(1, len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp)
+        # dp = [1] * len(nums)
+        # for i in range(1, len(nums)):
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             dp[i] = max(dp[i], dp[j] + 1)
+        # return max(dp)
