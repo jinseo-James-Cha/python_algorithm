38dfb014a810eceed920a9c2cf3985cdb8a93919
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-14 14:57:31 +0100

    Time: 6 ms (23.15%), Space: 17.5 MB (99.97%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,32 +1,12 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # top-down, recursion + memoization
-        def minimum_cost(i: int):
-            if i <= 1:
-                return 0
-            
-            if i in memo:
-                return memo[i]
-            
-            down_one = cost[i - 1] + minimum_cost(i - 1)
-            down_two = cost[i - 2] + minimum_cost(i - 2)
-            memo[i] = min(down_one, down_two)
-            return memo[i]
+        # 1step or 2step -> minimize total cost
+        # dp[len(cost)] -> top of the floor -> the answer
 
-
-        memo = {}
-        return minimum_cost(len(cost))
-
-
-        # bottom-up, tabulation, iterative
-        # minimum_cost = [0] * (len(cost) + 1)
-
-        # for i in range(2, len(cost) + 1):
-        #     take_one_step = minimum_cost[i-1] + cost[i - 1]
-        #     take_two_step = minimum_cost[i-2] + cost[i - 2]
-        #     minimum_cost[i] = min(take_one_step, take_two_step)
-
-        # return minimum_cost[-1]
-
-
+        # 1. bottom-up / tabulation
+        dp = [0] * (len(cost) + 1) # for the arrival 
+        # index 0, 1 are available to start, so its 0
+        for i in range(2, len(cost) + 1):
+            dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
         
+        return dp[-1]
