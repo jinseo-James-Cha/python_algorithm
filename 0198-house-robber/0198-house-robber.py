0220e20cdeebdc9afee21245d5059d55cc5c0786
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-14 15:16:14 +0100

    Time: 0 ms (100%), Space: 18 MB (12.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,30 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # optimize from dp
-        # i just need i - 1 and i - 2 to have answer
-        # dp[-1] is the maximum profit..
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-
-        prev_prev_profit = nums[0] # i - 2
-        prev_profit = max(nums[0], nums[1]) # i - 1
-        for i in range(2, len(nums)):
-            current = max(prev_profit, prev_prev_profit + nums[i])
-
-            prev_prev_profit = prev_profit
-            prev_profit = current
-        return prev_profit
+        # maximum amount I can rob ?! -> DP question?!
+        # top-down / memoization
+        def calculate_rob(i):
+            if i == 0:
+                return nums[0]
+            elif i == 1:
+                return max(nums[0], nums[1])
             
+            if i in memo:
+                return memo[i]
+            
+            memo[i] = max(calculate_rob(i-2) + nums[i], calculate_rob(i-1))
+            return memo[i]
+        memo = {}
+        return calculate_rob(len(nums)-1)
+        
 
-        # dp
-        # n = len(nums)
-        # if n == 1:
-        #     return nums[0]
-
-        # dp = [0] * n
-        # dp[0] = nums[0]
-        # dp[1] = max(nums[0], nums[1])
-        # for i in range(2, n):
-        #     dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-        # return dp[-1]
+        # bottom-up / tabulation
