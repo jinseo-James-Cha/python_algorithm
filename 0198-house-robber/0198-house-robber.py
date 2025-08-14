2b262573f4e02ab8873be0dc12ab2741698afbfe
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-14 15:25:02 +0100

    Time: 0 ms (100%), Space: 18 MB (12.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,31 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         # maximum amount I can rob ?! -> DP question?!
-        # bottom-up / tabulation
+        # opitimized bottom-up
+        # I just need the prev, and the prev prev until the last !
         if len(nums) == 1:
             return nums[0]
-
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
         
-        return dp[-1]
+        prev_prev = nums[0]
+        prev = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            current = max(prev, prev_prev + nums[i])
+            prev_prev = prev
+            prev = current
+        return prev
+
+
+        # bottom-up / tabulation
+        # if len(nums) == 1:
+        #     return nums[0]
+
+        # dp = [0] * len(nums)
+        # dp[0] = nums[0]
+        # dp[1] = max(nums[0], nums[1])
+        # for i in range(2, len(nums)):
+        #     dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        
+        # return dp[-1]
 
 
 
