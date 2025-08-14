0a7468618dc5256c07d42185ad672217f3c0a2a0
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-14 11:19:10 +0100

    Time: 0 ms (100%), Space: 17.9 MB (8.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,21 +1,47 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # optimize bottom-up
+        # I just need to have prev and prevprev values
+        # track and renew for those
+        if n <= 2:
+            return n
+        
+        prev_prev_val = 1
+        prev_val = 2
+        for i in range(3, n + 1):
+            current = prev_prev_val + prev_val
+            prev_prev_val = prev_val
+            prev_val = current
+        return prev_val
+
+
+        # bottom-up
+        # if n <= 2:
+        #     return n
+
+        # dp = [0] * (n + 1)
+        # dp[1], dp[2] = 1, 2
+        # for i in range(3, n+1):
+        #     dp[i] = dp[i-1] + dp[i-2]
+        
+        # return dp[n]
+
         # top-down
         # 1 -> 1
         # 2 -> 1 + 1
         # n에 도착하는 방법은 n-1에서 오거나 n-2에서 오거나
         # 그러므로 n = n-1의 경우의 수 + n-2의 경우의 수
 
-        def cal_stairs(n):
-            if n <= 2:
-                return n
-            if n in memo:
-                return memo[n]
-            memo[n] = cal_stairs(n-1) + cal_stairs(n-2)
-            return memo[n]
+        # def cal_stairs(n):
+        #     if n <= 2:
+        #         return n
+        #     if n in memo:
+        #         return memo[n]
+        #     memo[n] = cal_stairs(n-1) + cal_stairs(n-2)
+        #     return memo[n]
         
-        memo = {}
-        return cal_stairs(n)
+        # memo = {}
+        # return cal_stairs(n)
         
 
 
