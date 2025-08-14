a1a329b7861be582980c3709319a55e579ae39b5
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-14 15:04:31 +0100

    Time: 12 ms (5.03%), Space: 19.9 MB (13.14%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -4,9 +4,24 @@ class Solution:
         # dp[len(cost)] -> top of the floor -> the answer
 
         # 1. bottom-up / tabulation
-        dp = [0] * (len(cost) + 1) # for the arrival 
-        # index 0, 1 are available to start, so its 0
-        for i in range(2, len(cost) + 1):
-            dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
+        # dp = [0] * (len(cost) + 1) # for the arrival 
+        # # index 0, 1 are available to start, so its 0
+        # for i in range(2, len(cost) + 1):
+        #     dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
         
-        return dp[-1]
+        # return dp[-1]
+
+        # 2. top-down / memoization
+        def cal_cost(i):
+            # first and second can be starting point, so value 0
+            if i <= 1:
+                return 0
+            
+            # if already calculated, using memoization
+            if i in memo:
+                return memo[i]
+            
+            memo[i] = min(cal_cost(i-2) + cost[i-2], cal_cost(i-1) + cost[i-1])
+            return memo[i]
+        memo = {}
+        return cal_cost(len(cost))
