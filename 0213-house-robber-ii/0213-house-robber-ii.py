c7e59bc4701a4310773b3c2e0f471329c3b81153
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-14 15:56:29 +0100

    Time: 0 ms (100%), Space: 17.7 MB (86.39%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,21 +1,46 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return nums[0]
+        # 일단 서클이라는 조건을 생각하지말고 쭉 가보는거지..
+        # 그렇다면 n = max(n-2 + nums[n], n-1)이라는 가정인데
+        # 맨 마지막을 선택할건지 말건지...
+        # nums[1:] or nums[:-1]로 판단하는건디... 어떻게 이렇게 생각해낼수 있을까...
 
-        # 첫집을 안터는 경우와
-        # 마지막집을 안터는 경우
-        # 로 쪼개서 풀면되지!
-        def rob_houses(arr):
-            if len(arr) == 1:
-                return arr[0]
-            
-            dp = [0] * len(arr)
-            dp[0] = arr[0]
-            dp[1] = max(arr[0], arr[1])
-            for i in range(2, len(arr)):
-                dp[i] = max(dp[i-1], dp[i-2] + arr[i])
+        # bottom-up / tabulation
+        if len(nums) == 1:
+            return nums[0]
+        
+        def bottom_up_dp(houses):
+            n = len(houses)
+            if n == 1:
+                return houses[0]
+
+            dp = [0] * n
+            dp[0] = houses[0]
+            dp[1] = max(houses[0], houses[1])
+            for i in range(2, n):
+                dp[i] = max(dp[i-2]+houses[i], dp[i-1])
             
             return dp[-1]
-        return max(rob_houses(nums[:-1]), rob_houses(nums[1:]))
+        
+        return max(bottom_up_dp(nums[1:]), bottom_up_dp(nums[:-1]))
+
+
+
+
+        # top-down / memoization -> TLE
+        # if len(nums) == 1:
+        #     return nums[0]
+
+        # def top_down_dp(n, houses ,memo):
+        #     if n == 0:
+        #         return houses[0]
+        #     elif n == 1:
+        #         return max(houses[0], houses[1])
+        #     memo[n] = max(top_down_dp(n-2, houses, memo) + houses[n], top_down_dp(n-1, houses, memo))
+        #     return memo[n]
+        
+        return max(top_down_dp(len(nums)-2, nums[1:], {}), top_down_dp(len(nums)-2, nums[:-1], {}))
+
+
+        # bottom-up / tabulation
+        
