0260e8f888c7674201cc2c33fc599e9efac6e4ae
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-14 15:21:44 +0100

    Time: 0 ms (100%), Space: 17.7 MB (54.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,19 +2,30 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         # maximum amount I can rob ?! -> DP question?!
         # bottom-up / tabulation
+        if len(nums) == 1:
+            return nums[0]
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        
+        return dp[-1]
+
 
 
         # top-down / memoization
-        def calculate_rob(i):
-            if i == 0:
-                return nums[0]
-            elif i == 1:
-                return max(nums[0], nums[1])
+        # def calculate_rob(i):
+        #     if i == 0:
+        #         return nums[0]
+        #     elif i == 1:
+        #         return max(nums[0], nums[1])
             
-            if i in memo:
-                return memo[i]
+        #     if i in memo:
+        #         return memo[i]
             
-            memo[i] = max(calculate_rob(i-2) + nums[i], calculate_rob(i-1))
-            return memo[i]
-        memo = {}
-        return calculate_rob(len(nums)-1)
+        #     memo[i] = max(calculate_rob(i-2) + nums[i], calculate_rob(i-1))
+        #     return memo[i]
+        # memo = {}
+        # return calculate_rob(len(nums)-1)
