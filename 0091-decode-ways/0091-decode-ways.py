77a5390bab25e7f4e375cea49a3590c464dbc849
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-14 16:56:42 +0100

    Time: 0 ms (100%), Space: 17.9 MB (30.43%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,24 +1,42 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            
-            if i == len(s):
-                return 1
-            
-            if s[i] == "0":
-                return 0
-            
-            # 한 글자 해석
-            res = dfs(i + 1)
-
-            # 두 글자 해석 가능하면 더하기
-            if i + 1 < len(s) and int(s[i]) * 10 + int(s[i+1]) <= 26:
-                res += dfs(i + 2)                    
-            
-            memo[i] = res
-            return memo[i]
+        # bottom-up
+        if len(s) == 1:
+            return 1 if s[0] != "0" else 0
         
-        memo = {}
-        return dfs(0)
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == "0" else 1
+
+        for i in range(2, len(dp)):
+            if s[i - 1] != "0":
+                dp[i] = dp[i - 1]
+
+            two_digit = int(s[i - 2:i])
+            if 10 <= two_digit <= 26:
+                dp[i] += dp[i - 2]
+        return dp[-1]
+
+        # Top-down / memoization
+        # def dfs(i):
+        #     if i in memo:
+        #         return memo[i]
+            
+        #     if i == len(s):
+        #         return 1
+            
+        #     if s[i] == "0":
+        #         return 0
+            
+        #     # 한 글자 해석
+        #     res = dfs(i + 1)
+
+        #     # 두 글자 해석 가능하면 더하기
+        #     if i + 1 < len(s) and int(s[i]) * 10 + int(s[i+1]) <= 26:
+        #         res += dfs(i + 2)                    
+            
+        #     memo[i] = res
+        #     return memo[i]
+        
+        # memo = {}
+        # return dfs(0)
