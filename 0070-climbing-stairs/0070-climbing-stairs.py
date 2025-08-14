8ae0e1b84b8e03aca29d96c91b768dda4955a575
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-14 11:11:23 +0100

    Time: 0 ms (100%), Space: 17.7 MB (48.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,31 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # top-down
+        # 1 -> 1
+        # 2 -> 1 + 1
+        # n에 도착하는 방법은 n-1에서 오거나 n-2에서 오거나
+        # 그러므로 n = n-1의 경우의 수 + n-2의 경우의 수
+
+        def cal_stairs(n):
+            if n <= 2:
+                return n
+            if n in memo:
+                return memo[n]
+            memo[n] = cal_stairs(n-1) + cal_stairs(n-2)
+            return memo[n]
+        
+        memo = {}
+        return cal_stairs(n)
+        
+
+
+
+
+
+
+
+
+
         # v2: bottom-up
         if n <= 2:
             return n
