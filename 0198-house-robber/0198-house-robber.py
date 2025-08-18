077c5d060deb4ac8a6a0af55112d76b72776f83d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-18 22:03:37 +0100

    Time: 3 ms (7.27%), Space: 17.7 MB (53.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,45 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # maximum amount I can rob ?! -> DP question?!
-        # opitimized bottom-up
-        # I just need the prev, and the prev prev until the last !
-        if len(nums) == 1:
-            return nums[0]
-        
-        prev_prev = nums[0]
-        prev = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            current = max(prev, prev_prev + nums[i])
-            prev_prev = prev
-            prev = current
-        return prev
-
-
-        # bottom-up / tabulation
-        # if len(nums) == 1:
-        #     return nums[0]
-
-        # dp = [0] * len(nums)
-        # dp[0] = nums[0]
-        # dp[1] = max(nums[0], nums[1])
-        # for i in range(2, len(nums)):
-        #     dp[i] = max(dp[i-2] + nums[i], dp[i-1])
-        
-        # return dp[-1]
-
-
-
-        # top-down / memoization
-        # def calculate_rob(i):
-        #     if i == 0:
-        #         return nums[0]
-        #     elif i == 1:
-        #         return max(nums[0], nums[1])
+        # top down
+        def dp(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])
             
-        #     if i in memo:
-        #         return memo[i]
-            
-        #     memo[i] = max(calculate_rob(i-2) + nums[i], calculate_rob(i-1))
-        #     return memo[i]
-        # memo = {}
-        # return calculate_rob(len(nums)-1)
+            if i in memo:
+                return memo[i]
+
+            memo[i] = max(dp(i-2) + nums[i], dp(i-1))
+            return memo[i]
+        
+        memo = {}
+        return dp(len(nums)-1)
