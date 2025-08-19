a57673bf0e715e2f0ded6dfe6433132cef576338
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-19 10:28:00 +0100

    Time: 0 ms (100%), Space: 18.1 MB (28.87%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,47 +1,31 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # bottom up
-        dp = [0] * (len(cost)+1) # [0,0,0,    0]
-        for i in range(2, len(cost) + 1):
-            dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
-        return dp[-1]
-
-        # memo = {}
-
-        # def dp(i):
-        #     # if i < 0:
-        #     #     return 0
-        #     if i == 0 or i == 1:
-        #         return cost[i]
-        #     if i not in memo:
-        #         memo[i] = cost[i] + min(dp(i-1), dp(i-2))
-        #     return memo[i]
-
-        # # 마지막 계단(len(cost)-1) 또는 그 전 계단(len(cost)-2)에서 한 번에 올라갈 수 있으므로
-        # return min(dp(len(cost)-1), dp(len(cost)-2))
-
-        # 1step or 2step -> minimize total cost
-        # dp[len(cost)] -> top of the floor -> the answer
-
-        # 1. bottom-up / tabulation
-        # dp = [0] * (len(cost) + 1) # for the arrival 
-        # # index 0, 1 are available to start, so its 0
-        # for i in range(2, len(cost) + 1):
-        #     dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
+        # optimized bottom up
+        prev = 0
+        prev_prev = 0
         
-        # return dp[-1]
-
-        # 2. top-down / memoization
-        # def cal_cost(i):
-        #     # first and second can be starting point, so value 0
-        #     if i <= 1:
-        #         return 0
+        for i in range(2, len(cost) + 1):
+            current = min(prev + cost[i-1], prev_prev + cost[i-2])
+            prev_prev = prev
+            prev = current
+        return prev
+        
+#         # bottom up
+#         dp = [0] * (len(cost) + 1)
+        
+#         for i in range(2, len(cost) + 1):
+#             dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
+#         return dp[len(cost)]
+        
+        # top down
+#         def dp(i):
+#             if i <= 1:
+#                 return 0
             
-        #     # if already calculated, using memoization
-        #     if i in memo:
-        #         return memo[i]
+#             if i not in memo:
+#                 memo[i] = min(dp(i-2) + cost[i-2], dp(i-1) + cost[i-1])
             
-        #     memo[i] = min(cal_cost(i-2) + cost[i-2], cal_cost(i-1) + cost[i-1])
-        #     return memo[i]
-        # memo = {}
-        # return cal_cost(len(cost))
+#             return memo[i]
+            
+#         memo = {}
+#         return dp(len(cost))
