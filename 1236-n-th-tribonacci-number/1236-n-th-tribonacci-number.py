3fa9a25c64a8d3e65bd7a4a8c50b4ee891ed50d3
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-19 10:33:17 +0100

    Time: 0 ms (100%), Space: 17.9 MB (29.88%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,13 +1,36 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
+        # top down memoization
         if n == 0:
             return 0
+        
+        def dp(i):
+            if i <= 0 :
+                return 0
+            if i <= 2:
+                return 1
+            if i not in memo:
+                memo[i] = dp(i-3) + dp(i-2) + dp(i-1)
+            return memo[i]
+        
+        memo = {}
+        return dp(n)
 
-        memo = [0] * (n + 2)
-        memo[1] = memo[2] = 1
-        if n <= 2:
-            return memo[n]
 
-        for i in range(3, n+1):
-            memo[i] = memo[i-3] + memo[i-2] + memo[i-1]
-        return memo[n]
+
+
+
+
+
+
+#         if n == 0:
+#             return 0
+
+#         memo = [0] * (n + 2)
+#         memo[1] = memo[2] = 1
+#         if n <= 2:
+#             return memo[n]
+
+#         for i in range(3, n+1):
+#             memo[i] = memo[i-3] + memo[i-2] + memo[i-1]
+#         return memo[n]
