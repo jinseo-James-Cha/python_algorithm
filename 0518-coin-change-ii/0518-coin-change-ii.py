dbb91addba359f2e229a801a22d49778fa8aec17
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-21 23:25:33 +0100

    Time: 271 ms (49.38%), Space: 48.4 MB (41.1%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,8 +1,31 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         # state
+        # ith coins
         # amount
 
+        # no duplication combinations !
+
+        # bottom up
+        n = len(coins)
+        # dp[coins][amount]
+        dp = [[0] * (amount + 1) for _ in range(n+1)] 
+        for i in range(n):
+            dp[i][0] = 1 
+
+        for i in range(n-1, -1, -1):
+            for j in range(1, amount + 1):
+                if coins[i] > j:
+                    dp[i][j] = dp[i + 1][j]
+                else:
+                    dp[i][j] = dp[i + 1][j] + dp[i][j - coins[i]]
+                    
+        return dp[0][amount]
+
+
+
+
+        # top down
         def dp(i, target_amount):
             if target_amount == 0:
                 return 1
