792190f250f605cbb4346aab8f202b9bd4546547
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-21 15:10:27 +0100

    Time: 4 ms (56.06%), Space: 18.7 MB (49.63%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,6 +4,26 @@ class Solution:
         # days -> i
         # action -> 0 buying, 1 selling
 
+        # bottom up
+        dp = [[0] * 2 for _ in range(len(prices) + 2)]
+
+        for i in range(len(prices)-1, -1, -1):
+            for holding in range(2):
+                doNothing = dp[i+1][holding]
+                doSomething = 0
+                if holding:
+                    doSomething = prices[i] + dp[i+2][0]
+                else:
+                    doSomething = -prices[i] + dp[i+1][1]
+                dp[i][holding] = max(doNothing, doSomething)
+        
+        return dp[0][0]
+
+
+
+
+
+        # top down
         def dp(i, holding):
             if i >= len(prices):
                 return 0
