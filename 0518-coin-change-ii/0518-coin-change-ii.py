f9c6321a2ab07718145e00d899b18888229459a0
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-21 18:02:42 +0100

    Time: 1312 ms (5.03%), Space: 273.1 MB (10.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,10 +1,58 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount + 1)
-        dp[0] = 1
+        # state
+        # amount
 
-        for c in coins:
-            for a in range(1, amount + 1):
-                if a - c >= 0:
-                    dp[a] += dp[a-c]
-        return dp[-1]
+        def dp(i, target_amount):
+            if target_amount == 0:
+                return 1
+            if i == len(coins) or target_amount < 0:
+                return 0
+            
+            if (i, target_amount) not in memo:
+                used = dp(i, target_amount - coins[i])
+                not_used = dp(i+1, target_amount)
+
+                memo[(i,target_amount)] = used + not_used
+            
+            return memo[(i,target_amount)]
+        
+        memo = {}
+        return dp(0, amount)
+
+
+
+
+
+
+
+
+        # top down
+        def dfs(i, amount):
+            if amount == 0:
+                return 1
+            if i == len(coins):
+                return 0
+            if memo[i][amount] != -1:
+                return memo[i][amount]
+            
+            if coins[i] > amount: # coin is greater than amount, skip to the next
+                memo[i][amount] = dfs(i+1, amount)
+            else: # calculatable coin !
+                memo[i][amount] = dfs(i, amount-coins[i]) + dfs(i+1, amount)
+
+            return memo[i][amount]
+        
+        memo = [[-1] * (amount + 1) for _ in range(len(coins))]
+        dfs(0, amount)
+        print(memo)
+        return 0
+
+        # dp = [0] * (amount + 1)
+        # dp[0] = 1
+
+        # for c in coins:
+        #     for a in range(1, amount + 1):
+        #         if a - c >= 0:
+        #             dp[a] += dp[a-c]
+        # return dp[-1]
