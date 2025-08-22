f73b05e289455d3cacaf080e41216da5009e69d3
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-22 16:11:31 +0100

    Time: 5 ms (3.41%), Space: 17.9 MB (30.6%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -8,7 +8,27 @@ class Solution:
         # one letter should be 1 ~ 26
 
         # action choose 1letter or 2letters
+
+        # bottom up
+        if len(s) == 1:
+            return 0 if s[0] == "0" else 1
+
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == "0" else 1
+        for i in range(2, len(s) + 1):
+            if s[i-1] != "0":
+                dp[i] = dp[i-1]
+            
+            two_digits = int(s[i-2: i])
+            if 10 <= two_digits <= 26:
+                dp[i] += dp[i-2]
+            
+        return dp[-1]
         
+
+        
+        # top down
         def dp(i):
             if i == len(s): # I think even tho its over the len, it is still true..
                 return 1
@@ -26,21 +46,6 @@ class Solution:
         
         memo = {}
         return dp(0)
-
-        # dp(2-1)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         # bottom-up
         if len(s) == 1:
