1ed3f07829f21eef1981569f3db2ada6ecddd1f7
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-22 17:00:18 +0100

    Time: 142 ms (11.47%), Space: 26.9 MB (32.22%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy = prices[0]
-        profit = 0
+        # state
+        # days -> i
+        # action -> holding? yes -> sell or skip / no -> buying or skip
+
+        if len(prices) == 1:
+            return 0
+
+        buying_price = prices[0]
+        max_profit = 0
+
         for i in range(1, len(prices)):
-            if prices[i] < buy:
-                buy = prices[i]
-            elif prices[i] - buy > profit:
-                profit = prices[i] - buy
-        return profit
+            max_profit = max(max_profit, prices[i] - buying_price)
+            buying_price = min(buying_price, prices[i])
+        return max_profit
+
