72531985c57a987cd3ea332270be6f5d8816c0a5
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-25 13:59:23 +0100

    Time: 69 ms (12.89%), Space: 27.7 MB (5.5%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -1,14 +1,14 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         def dp(i, j):
-            if i == 0 and j < n:
-                return matrix[i][j]
-            
-            if i < 0 or j < 0 or j >= n:
+            if j < 0 or j >= n:
                 return float(inf)
 
+            if i == m - 1:
+                return matrix[i][j]
+
             if (i,j) not in memo:
-                min_val = min(dp(i-1, j-1), dp(i-1, j), dp(i-1, j+1))
+                min_val = min(dp(i+1, j-1), dp(i+1, j), dp(i+1, j+1))
                 memo[(i,j)] = min_val + matrix[i][j]
 
             return memo[(i,j)]
@@ -17,5 +17,5 @@ class Solution:
         m,n = len(matrix), len(matrix[0])
         res = float(inf)
         for col in range(n):
-            res = min(res, dp(n-1, col))
+            res = min(res, dp(0, col))
         return res
