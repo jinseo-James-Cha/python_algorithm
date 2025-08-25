0826b0b417b638a51ea9618343ae98ed90d0274c
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-25 11:09:08 +0100

    Time: 0 ms (100%), Space: 18.2 MB (5.44%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,35 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # state
+        # i for rows
+        # j for cols
+        def dp(i, j):
+            if i == 0 or j == 0:
+                return 1
+            
+            if (i,j) not in memo:
+                memo[(i,j)] = dp(i-1, j) + dp(i, j-1)
+
+            return memo[(i,j)]
+        
+        memo = {}
+        return dp(m-1, n-1)
+
+
+
+
+
+
+
+
+
+
+        # grid[m - 1][n - 1] = grid[m-2][n-1] + grid[m-1][n-2]
+
+        # bottom up
+        dp = [[1 for _ in range(n)] for _ in range(m)] 
+        for r in range(1, m): 
+            for c in range(1, n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+        
+        return dp[m-1][n-1]
