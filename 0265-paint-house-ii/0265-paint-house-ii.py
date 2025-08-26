d2ba04304ff508f964d35938ff1a5ebe0d9de6bc
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-26 11:48:22 +0100

    Time: 62 ms (35.62%), Space: 18.1 MB (43.58%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.py
+++ 0265-paint-house-ii/0265-paint-house-ii.py
@@ -1,5 +1,28 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
+        # bottom up
+        n = len(costs) # houses
+        k = len(costs[0]) # colors
+        dp = [[0] * k for _ in range(n)]
+
+        # base case
+        for j in range(k):
+            dp[0][j] = costs[0][j]
+        
+        for i in range(1, n):
+            for j in range(k):
+                min_cost = float(inf)
+                for z in range(k):
+                    if j != z:
+                        min_cost = min(min_cost, dp[i-1][z])
+                dp[i][j] = min_cost + costs[i][j]
+        
+        return min(dp[n-1])
+
+
+
+
+
         # top down
         def dp(row, col):
             if row == n-1:
