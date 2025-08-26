c4a2167c153c965866aa3f2b5714cc7b84df965b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-08-26 15:37:05 +0100

    Time: 343 ms (40.27%), Space: 24.5 MB (11.55%) - LeetHub

---
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        # n : num of dice
+        # k : num of face -> 1~k
+        # 10**9 + 7 < answer -> 10**9 + 7
+
+
+        # dp
+        # possible ways
+
+        # state
+        # n, current_target
+        
+        # top down
+        def dp(dices, current_target):
+            if current_target == 0 and dices == 0:
+                return 1
+            
+            if current_target < 0:
+                return 0
+            
+            if dices == 0:
+                return 0
+            
+            if (dices, current_target) not in memo:
+                ways = 0
+                for f in range(1, k+1):
+                    ways = (ways + dp(dices-1, current_target-f)) % (10**9 + 7)
+                memo[(dices, current_target)] = ways 
+            return memo[(dices, current_target)]
+
+        memo = {}
+        return dp(n, target)
