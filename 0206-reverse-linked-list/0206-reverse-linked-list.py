b81007e30823199d7377e8b16903c5ae4fe0d248
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-01 12:36:13 +0100

    Time: 0 ms (100%), Space: 18.6 MB (53.29%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,25 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node = None
+        prev = None
+        current = head
+        while current:
+            temp = current.next
+            current.next = prev
+            prev = current
 
-        while head:
-            temp = head.next # keeping moving by connection, so save this connection in temp
-            
-            head.next = node # attach node into next
-            node = head # attach head's val and node into node
-
-            head = temp
-        return node
-
-
-# Recursive version
-# class Solution:
-#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         if not head or not head.next:
-#             return head
-
-#         new_head = self.reverseList(head.next)
-#         head.next.next = head
-#         head.next = None
-#         return new_head
+            current = temp
+        return prev
