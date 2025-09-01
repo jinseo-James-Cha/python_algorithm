7566ca42af4d9362688918dd0bd476f1ae4f2dde
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-01 12:44:35 +0100

    Time: 0 ms (100%), Space: 19.4 MB (44.4%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
@@ -7,6 +7,20 @@
 from collections import deque
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return []
+        
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        
+        return self.searchBST(root.right, val)
+
+
+
+
+
         # BST!!
         # val == me -> [me, left, right]
 
