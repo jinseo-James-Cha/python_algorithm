80a0e799b9c18ea3641cf9913e25eba21304407d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-03 12:24:57 +0100

    Time: 0 ms (100%), Space: 18.1 MB (72.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -2,41 +2,61 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         # matrix[i][j] increasing order
         # matrix[i][0] increasing ordr
-        m, n = len(matrix), len(matrix[0])
 
-        # binary search - > sorted ok
-        # first binary search for lower bound for matrix[i][0]
-        # second find the exact target in matrix[i]
-        def get_lower_bound(nums, target):
-            left = 0
-            right = len(nums)
-            while left < right:
-                mid = (left + right) // 2
-                if nums[mid] <= target:
-                    left = mid + 1
-                else:
-                    right = mid
-            
-            return left-1
+        # 2d -> 1d and then binary search?
+        arr = []
+        for i in range(len(matrix)):
+            arr.extend(matrix[i])
         
-        def check_target(nums, target):
-            left = 0
-            right = len(nums) - 1
+        def binary_search(nums, target):
+            left, right = 0, len(nums)-1
             while left <= right:
                 mid = (left + right) // 2
                 if nums[mid] == target:
                     return True
-                elif nums[mid] > target:
-                    right = mid - 1
-                else:
+                elif nums[mid] < target:
                     left = mid + 1
+                else:
+                    right = mid - 1
             return False
+        
+        return binary_search(arr, target)
 
-        found_row_num = get_lower_bound([matrix[i][0] for i in range(m)], target)
-        if found_row_num < 0:
-            return False
 
-        return check_target(matrix[found_row_num], target)
+        # binary search - > sorted ok
+        # first binary search for lower bound for matrix[i][0]
+        # second find the exact target in matrix[i]
+        # m, n = len(matrix), len(matrix[0])
+        # def get_lower_bound(nums, target):
+        #     left = 0
+        #     right = len(nums)
+        #     while left < right:
+        #         mid = (left + right) // 2
+        #         if nums[mid] <= target:
+        #             left = mid + 1
+        #         else:
+        #             right = mid
+            
+        #     return left-1
+        
+        # def check_target(nums, target):
+        #     left = 0
+        #     right = len(nums) - 1
+        #     while left <= right:
+        #         mid = (left + right) // 2
+        #         if nums[mid] == target:
+        #             return True
+        #         elif nums[mid] > target:
+        #             right = mid - 1
+        #         else:
+        #             left = mid + 1
+        #     return False
+
+        # found_row_num = get_lower_bound([matrix[i][0] for i in range(m)], target)
+        # if found_row_num < 0:
+        #     return False
+
+        # return check_target(matrix[found_row_num], target)
 
 
             
