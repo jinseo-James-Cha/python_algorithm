075b7976f6aee42d67b1f6a8ada42fa3ecc4b0f7
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-03 13:39:07 +0100

    Time: 152 ms (17.82%), Space: 24 MB (46.81%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -5,6 +5,9 @@ class Solution:
         row = height - 1
         col = 0
         
+        # target보다 클때는 row를 줄이고
+        # target보다 작을때는 col을 줄이는 전략이라서
+        # 왼쪽 맨 밑에서 시작하는구나.
         while col < width and row >= 0:
             if matrix[row][col] > target:
                 row -= 1
