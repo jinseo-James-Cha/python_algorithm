b86912714e07751f3cdc9f5d3f2b5ddb73630180
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-05 17:38:00 +0100

    Time: 0 ms (100%), Space: 18 MB (43.07%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,9 +1,8 @@
-from collections import deque
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         # Backtrack
         def backtrack(curr, left_count, right_count):
-            if len(curr) == 2*n:
+            if len(curr) == n*2:
                 res.append("".join(curr))
                 return
             
@@ -16,35 +15,7 @@ class Solution:
                 curr.append(")")
                 backtrack(curr, left_count, right_count+1)
                 curr.pop()
-
+        
         res = []
         backtrack([], 0, 0)
-        return res
-
-
-        # BFS
-        def isValid(s):
-            left_count = 0
-            for letter in s:
-                if letter == "(":
-                    left_count += 1
-                else:
-                    left_count -= 1
-                
-                if left_count < 0:
-                    return False
-            return left_count == 0
-
-        res = []
-        queue = deque([""])
-        while queue:
-            cur_string = queue.popleft()
-
-            if len(cur_string) == 2 * n:
-                if isValid(cur_string):
-                    res.append(cur_string)
-                continue
-            
-            queue.append(cur_string + "(")
-            queue.append(cur_string + ")")
         return res
