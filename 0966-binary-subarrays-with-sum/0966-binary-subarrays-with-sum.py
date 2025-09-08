fb332853edb061d5c9fbc854ad2e7caf6e7e6e88
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-08 10:20:41 +0100

    Time: 22 ms (94.78%), Space: 21.6 MB (23.66%) - LeetHub

---
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -0,0 +1,49 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        # prefix sum
+        freq = {0: 1}
+        res,curr = 0, 0
+
+        for num in nums:
+            curr += num
+            res += freq.get(curr - goal, 0)
+            freq[curr] = freq.get(curr, 0) + 1
+        return res
+
+
+
+
+
+        
+        
+        
+        # sliding window by size
+        # TLE
+        res = 0
+        for size in range(1, len(nums)+1):
+            # initial sliding window
+            curr = sum(nums[:size]) 
+            if curr == goal:
+                res += 1
+            for i in range(size, len(nums)): 
+                curr += nums[i] - nums[i-size]
+                if curr == goal:
+                    res += 1
+        return res
+
+        
+        # backtrack? no -> cuz of subarray, which means it needs to be contiguous
+
+        # def backtrack(start, curr):
+        #     print(curr)
+        #     if sum(curr) == goal:
+        #         return 1
+        #     res = 0
+        #     for i in range(start, len(nums)):
+        #         curr.append(nums[i])
+        #         res += backtrack(i+1, curr)
+        #         curr.pop()
+
+        #     return res
+
+        # return backtrack(0, [])
