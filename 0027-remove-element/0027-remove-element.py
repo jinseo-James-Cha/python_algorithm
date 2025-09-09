bee0b365e611aa29f7acdff8a5892d0fa28ffda9
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-09 22:54:04 +0100

    Time: 0 ms (100%), Space: 17.7 MB (52.28%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,20 +1,13 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] == val:
-                # val can be only 0 ~ 100
+    def removeElement(self, nums: List[int], val: int):
+        for i, num in enumerate(nums):
+            if num == val:
                 nums[i] = -1
 
-        # two pointer -> move all -1 into back
+        
         left = 0
         for right in range(len(nums)):
             if nums[right] != -1:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
-
         return left
-
-
-
-
-        
