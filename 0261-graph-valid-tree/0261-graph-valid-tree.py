a3e38af803c20d4084b75efb7a1b607d348d9385
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-10 16:28:09 +0100

    Time: 0 ms (100%), Space: 18.9 MB (66.05%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,6 +1,48 @@
 from collections import defaultdict
+
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [0] * size
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union_set(self, x, y):
+        xset, yset = self.find(x), self.find(y)
+
+        if xset == yset:
+            return False
+        
+        if self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+        elif self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = yset
+        else:
+            self.parent[xset] = yset
+            yset += 1
+        
+        return True
+
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # union find
+        if len(edges) != n-1:
+            return False
+
+        uf = UnionFind(n)
+        res = n
+        for a,b in edges:
+            if not uf.union_set(a, b):
+                return False
+        return True
+
+
+
+
+
         if len(edges) != n-1:
             return False
             
