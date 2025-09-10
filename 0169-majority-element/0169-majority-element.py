50a4e563660833f5bc41595fff29c3b842ce2245
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-10 10:12:38 +0100

    Time: 10 ms (30.48%), Space: 19.4 MB (50.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,37 +1,15 @@
-# n = len(nums)
-# n / 2 > majority num
-# len 7 includes more than 4
-
-# O(1) in space
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # follow up - space O(N)
-        m_e = c = 0
-        for n in nums:
-            if c == 0:
-                m_e = n
-            
-            c += 1 if n == m_e else -1
+        # space O(1)?
+        count_map = {}
+        max_count = 0
+        res = 0
 
-        return m_e
-
-        # time O(N)
-        # space O(N)
-        # h = {}
-        # m_value = m_key = 0
-        # for n in nums:
-        #     h[n] = h.get(n, 0) + 1
-        #     if h[n] > m_value:
-        #         m_value = h[n]
-        #         m_key = n
-        # return m_key
-
-        # memory limit exceeded
-        # answer = [0] * (max(nums) + 1)
+        for num in nums:
+            count_map[num] = count_map.get(num, 0) + 1
+            if max_count < count_map[num]:
+                max_count = count_map[num]
+                res = num
         
-        # for n in nums:
-        #     answer[n] += 1
-
-        # return answer.index(max(answer))
+        return res
 
