78382a441ea44179d4c93bfad6867711314404f5
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-14 20:46:41 +0100

    Time: 0 ms (100%), Space: 19.4 MB (38.87%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,35 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        if n == 1:
+            return 0
+        
+
+        # left half and right half by the most highest bar ?
+        highest_bar_num = height[0]
+        highest_bar_index = 0
+        for i, num in enumerate(height):
+            if num > highest_bar_num:
+                highest_bar_num = num
+                highest_bar_index = i
+
+
+        res = 0
+        
+        # left to right till the maximum
+        cur_standard = height[0]
+        for i in range(1, highest_bar_index): # 1, 7
+            if cur_standard > height[i]:
+                res += cur_standard - height[i]
+            else:
+                cur_standard = height[i]
+        
+        # right to the left till the maximum
+        cur_standard = height[n-1]
+        for j in range(n-1, highest_bar_index, -1):
+            if cur_standard > height[j]:
+                res += cur_standard - height[j]
+            else:
+                cur_standard = height[j]
+        
+        return res
