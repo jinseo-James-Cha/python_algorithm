fe2b46df6ac0e8c591acc1e9a48d99c8ff2d4b35
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-23 00:34:26 +0100

    Time: 0 ms (100%), Space: 17.8 MB (58.97%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,5 +1,29 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        
+        hashmap = {}
+        registered = set()
+        s_arr = s.split()
+        if len(pattern) != len(s_arr):
+            return False
+            
+        for p, word in zip(pattern, s_arr):
+            if p in hashmap:
+                if hashmap[p] != word:
+                    return False
+            elif word in registered:
+                return False
+
+            registered.add(word)
+            hashmap[p] = word        
+        return True
+
+
+
+
+
+
+
         # s follows the same pattern
         
         # follow -> full match
@@ -8,20 +32,20 @@ class Solution:
 
         # hashmap ? hashmap[pattern[i]] = s[i]
 
-        hashmap = {}
-        registered = set()
-        s_array = s.split(" ")
-        if len(pattern) != len(s_array):
-            return False
+        # hashmap = {}
+        # registered = set()
+        # s_array = s.split(" ")
+        # if len(pattern) != len(s_array):
+        #     return False
         
-        for i, p in enumerate(pattern):
-            if p not in hashmap:
-                if s_array[i] not in registered:
-                    registered.add(s_array[i])
-                    hashmap[p] = s_array[i]
-                else:
-                    return False
-            # already registered? compare with previous
-            elif hashmap[p] != s_array[i]:
-                return False
-        return True
+        # for i, p in enumerate(pattern):
+        #     if p not in hashmap:
+        #         if s_array[i] not in registered:
+        #             registered.add(s_array[i])
+        #             hashmap[p] = s_array[i]
+        #         else:
+        #             return False
+        #     # already registered? compare with previous
+        #     elif hashmap[p] != s_array[i]:
+        #         return False
+        # return True
