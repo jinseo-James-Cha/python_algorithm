837aec439fa401a1d3bd2bd76a9634616e519999
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-23 00:39:49 +0100

    Time: 15 ms (42.21%), Space: 18 MB (50.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,43 +1,18 @@
-# first
-# return Counter(s) == Counter(t) will work without algorithm
-
-# s converts to Hashmap by letter = key, freq = value
-# deduct freq by t by letter and if it meets 0, it returns False
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        hashmap = {}
+        for letter in s:
+            hashmap[letter] = hashmap.get(letter, 0) + 1
         
-        counter = {} # hashmap
-        for l in s:
-            counter[l] = counter.get(l, 0) + 1
-        
-        for l in t:
-            if l not in counter or counter[l] == 0:
+        for char in t:
+            if char not in hashmap:
                 return False
-            else:
-                counter[l] -= 1
-        
-        return True
+            
+            hashmap[char] -= 1
+            if hashmap[char] < 0:
+                return False
+            
+            if hashmap[char] == 0:
+                del hashmap[char]
 
-
-
-# my poor answer
-# from collections import Counter
-
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         if not len(s) == len(t):
-#             return False
-
-#         s_c = Counter(s)
-#         t_c = Counter(t)
-
-#         for k, v in s_c.items():
-#             if not k in t_c:
-#                 return False
-#             elif not s_c[k] == t_c[k]:
-#                 return False
-        
-#         return True
+        return True if not hashmap else False
