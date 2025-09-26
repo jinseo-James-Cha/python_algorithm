9a281d6812375ab4604d5e0d46d5f2bff3ab0a36
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-26 22:06:35 +0100

    Time: 55 ms (34.18%), Space: 19.3 MB (45.51%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -3,6 +3,21 @@ import heapq
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         
+        # bellman-ford
+        dist = [float(inf)] * n
+        dist[src] = 0
+
+        for _ in range(k+1):
+            temp = dist[:]
+            for u, v, w in flights:
+                if dist[u] != float(inf) and dist[u] + w < temp[v]:
+                    temp[v] = dist[u] + w
+            dist = temp
+        
+        return -1 if dist[dst] == float(inf) else dist[dst]
+
+
+
         # dijkstra
         graph = defaultdict(list)
         for u, v, p in flights:
