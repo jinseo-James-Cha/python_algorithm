0944507c14055c773722c7ffa3e11260f9b54a87
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-27 22:20:05 +0100

    Time: 374 ms (43.28%), Space: 19.4 MB (60.3%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
@@ -1,29 +1,30 @@
 import heapq
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        # get minimum among a maximum in a path
+
         # dijkstra
-        # heapq, while, relax
-        def is_within_bound(x, y):
-            return 0 <= x < len(heights) and 0 <= y < len(heights[0])
+        def is_within_bound(r, c):
+            return 0 <= r < len(heights) and 0 <= c < len(heights[0])
 
-        row, col = len(heights), len(heights[0])
-        dist_matrix = [[float('inf')] * col for _ in range(row)]
-        dist_matrix[0][0] = 0
-        
-        visited = [[False] * col for _ in range(row)]
-        pq = [(0, 0, 0)] # diff, x, y
+        rows, cols = len(heights), len(heights[0])
+        diff_matrix = [[float('inf')] * cols for _ in range(rows)]
+        diff_matrix[0][0] = 0
+        visited = [[False] * cols for _ in range(rows)]
 
-        DIR = [(-1,0), (1,0), (0,-1), (0,1)]
+        DIR = [(1,0), (-1,0), (0,1), (0,-1)]
+        pq = [(0,0,0)]
         while pq:
-            diff, x, y = heapq.heappop(pq)
-            visited[x][y] = True
-            for dx, dy in DIR:
-                next_x, next_y = x+dx, y+dy
+            max_diff, r, c = heapq.heappop(pq)
+            visited[r][c] = True
+            
+            for dy, dx in DIR:
+                next_x, next_y = r+dy, c+dx
                 if is_within_bound(next_x, next_y) and not visited[next_x][next_y]:
-                    curr_diff = abs(heights[next_x][next_y] - heights[x][y])
-                    max_diff = max(curr_diff, dist_matrix[x][y])
+                    curr_diff = abs(heights[r][c] - heights[next_x][next_y])
+                    new_diff = max(curr_diff, diff_matrix[r][c])
                     
-                    if dist_matrix[next_x][next_y] > max_diff:
-                        dist_matrix[next_x][next_y] = max_diff
-                        heapq.heappush(pq, (max_diff, next_x, next_y))
-        return dist_matrix[-1][-1]
+                    if diff_matrix[next_x][next_y] > new_diff:
+                        diff_matrix[next_x][next_y] = new_diff
+                        heapq.heappush(pq, (new_diff, next_x, next_y))
+        return diff_matrix[-1][-1]
