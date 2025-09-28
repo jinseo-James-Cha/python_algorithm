e43dbff56140ea85e47605167184e609e895fa6a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-28 22:29:57 +0100

    Time: 4 ms (48.17%), Space: 19.2 MB (59.99%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,27 +1,29 @@
 from collections import defaultdict, deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # [a,b] =>     b -> a
+
+        # topology algorithm
         adj = defaultdict(list)
-        indegree = {}
+        indegrees = {}
         for dest, src in prerequisites:
             adj[src].append(dest)
-            indegree[dest] = indegree.get(dest,0) + 1
+            indegrees[dest] = indegrees.get(dest, 0) + 1
         
-        zero_indegree_queue = deque([k for k in range(numCourses) if k not in indegree])
-
-        topological_sorted_order = []
-        while zero_indegree_queue:
-
-            vertex = zero_indegree_queue.popleft()
-            topological_sorted_order.append(vertex)
-
-            if vertex in adj:
-                for neighbor in adj[vertex]:
-                    indegree[neighbor] -= 1
-
-                    if indegree[neighbor] == 0:
-                        zero_indegree_queue.append(neighbor)
+        # start with zero indegree
+        zero_indegrees = deque()
+        for i in range(numCourses):
+            if i not in indegrees:
+                zero_indegrees.append(i)
         
-        return topological_sorted_order if len(topological_sorted_order) == numCourses else []
+        answer = []
+        while zero_indegrees:
+            current = zero_indegrees.popleft()
+            answer.append(current)
 
-         
+            for neighbor in adj[current]:
+                indegrees[neighbor] -= 1
+                if indegrees[neighbor] == 0:
+                    zero_indegrees.append(neighbor)
+        
+        return answer if len(answer) == numCourses else []
