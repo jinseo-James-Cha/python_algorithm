92ca962265ee47b4ba38252249204bae1bc3fd88
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-09-29 18:12:42 +0100

    Time: 34 ms (97.57%), Space: 29.7 MB (42.49%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,48 +1,44 @@
+from collections import defaultdict, deque
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        # MHT: Minimum Height Tree
+        # MHT -> remove leaves until > 2
+        # undirected
 
-        # find minimum height
-        # minimum height == current height: add the root
-        # if minimum height > current height: reset answer with [current root]
-
-        # who can be root?
-
-        # a -> b
-        #  [[1,0],[1,2],[1,3]]
-        # 1->0,2,3 
-
-        # b -> a
-        #  [[1,0],[1,2],[1,3]]
-        # 0->1 
-        # 2-> 1
-        # 3 -> 1
-
-        # kahn algorithm - topological sort
         if n <= 2:
             return [i for i in range(n)]
         
-        neighbors = [set() for i in range(n)]
+        adj_list = [set() for i in range(n)]
         for a, b in edges:
-            neighbors[a].add(b)
-            neighbors[b].add(a)
-
-        leaves = [] # 0, 2 , 3
+            # undirected
+            adj_list[a].add(b)
+            adj_list[b].add(a)
+        
+        # initial node -> in_degree ==1
+        leaves = []
         for i in range(n):
-            if len(neighbors[i]) == 1:
+            if len(adj_list[i]) == 1:
                 leaves.append(i)
         
-        remaining_nodes = n # 4
-        while remaining_nodes > 2:
-            remaining_nodes -= len(leaves) # 4-3 = 1
+        remaining_leaves = n
+        while remaining_leaves > 2:
+            remaining_leaves -= len(leaves)
+
             new_leaves = []
 
-            while leaves: # 0,2,3
-                leaf = leaves.pop() # 3
-                neighbor = neighbors[leaf].pop() # 1
-                neighbors[neighbor].remove(leaf) # 1에 있는 3을 지워
-                if len(neighbors[neighbor]) == 1:
+            # all leaves take out
+            while leaves:
+                leaf = leaves.pop()
+                neighbor = adj_list[leaf].pop()
+                
+                adj_list[neighbor].remove(leaf)
+                if len(adj_list[neighbor]) == 1:
                     new_leaves.append(neighbor)
+            
             leaves = new_leaves
         
         return leaves
+
+
+
+
+
