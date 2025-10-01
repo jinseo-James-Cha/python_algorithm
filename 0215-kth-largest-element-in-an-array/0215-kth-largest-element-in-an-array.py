50a41a95daf2bdc6e444b67020901ee2386bdb08
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-01 10:49:02 +0100

    Time: 1034 ms (5.05%), Space: 36.2 MB (5.65%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,29 +1,39 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # heap sort
-        def max_heapify(heap_size, index):
+
+
+
+
+
+
+
+
+
+
+        # min heap
+        def min_heapify(heap_size, index):
             left = index * 2 + 1
             right = index * 2 + 2
             largest = index
 
-            if left < heap_size and nums[left] > nums[largest]:
+            if left < heap_size and nums[left] < nums[largest]:
                 largest = left
             
-            if right < heap_size and nums[right] > nums[largest]:
+            if right < heap_size and nums[right] < nums[largest]:
                 largest = right
             
             if largest != index:
                 nums[largest], nums[index] = nums[index], nums[largest]
-                max_heapify(heap_size, largest)
+                min_heapify(heap_size, largest)
             
         # initial heapify
         for i in range(len(nums)//2 -1, -1, -1):
-            max_heapify(len(nums), i)
+            min_heapify(len(nums), i)
         
         # swap first and last and heapify
         for i in range(len(nums)-1, 0, -1):
             nums[i], nums[0] = nums[0], nums[i]
-            max_heapify(i, 0)
+            min_heapify(i, 0)
         
-        return nums[len(nums) - k]
+        return nums[k-1]
     
