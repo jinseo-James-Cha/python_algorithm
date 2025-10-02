7d56837e3a5391a71545694d5714a7914066d175
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-02 15:12:28 +0100

    Time: 343 ms (69.81%), Space: 18.9 MB (9.06%) - LeetHub

--- 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.py
+++ 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.py
@@ -2,9 +2,47 @@ class Solution:
     def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
         # equal length, only digits str
         # queries = [k, trim] rightmost trim digit
+
+        # version2 - radix sort
+        n = len(nums)
+        m = len(nums[0])
+        
+        def countingSortByDigit(nums, digit):
+            buckets = [[] for _ in range(10)]
+            for num, idx in nums:
+                val = int(num[digit])
+                buckets[val].append([num, idx])
+            return [x for bucket in buckets for x in bucket]
+
+        base_nums = [[num, i] for i, num in enumerate(nums)]
+
+        sorted_by_trim = {0: base_nums} 
+
+        for trim in range(1, m+1):
+            arr = sorted_by_trim[trim-1][:]
+            arr = countingSortByDigit(arr, m - trim)
+            sorted_by_trim[trim] = arr
+
+        res = []
+        for k, trim in queries:
+            res.append(sorted_by_trim[trim][k-1][1])
+        
+        return res
+
+
+
+
+
+
+
+
+
+
+        # version1 
         answer = []
         for k, trim in queries:
             trimmed = [(num[-trim:], i) for i, num in enumerate(nums)]
+            # trimmed.sort(key=lambda x: (x[0], x[1]))
             trimmed.sort()
             answer.append(trimmed[k-1][1])
         return answer
