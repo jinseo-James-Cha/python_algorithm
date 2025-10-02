2ab082ff8174f392c41211e627d266ff2948473b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-02 16:12:52 +0100

    Time: 344 ms (69.05%), Space: 18.8 MB (10.19%) - LeetHub

--- 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.py
+++ 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.py
@@ -1,50 +1,28 @@
 class Solution:
     def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
-        # equal length, only digits str
-        # queries = [k, trim] rightmost trim digit
-
-        # version2 - radix sort
+        # radix sort -> counting sort -> bucket
+        
         n = len(nums)
         m = len(nums[0])
         
         def countingSortByDigit(nums, digit):
             buckets = [[] for _ in range(10)]
-            for num, idx in nums:
-                val = int(num[digit])
-                buckets[val].append([num, idx])
+            for num, i in nums:
+                bucket_idx = int(num[digit])
+                buckets[bucket_idx].append([num, i])
+            
             return [x for bucket in buckets for x in bucket]
-
+        
         base_nums = [[num, i] for i, num in enumerate(nums)]
-
-        sorted_by_trim = {0: base_nums} 
-
+        trimmed_sorted_list = {0: base_nums}
+        
+        trimmed_list = base_nums
         for trim in range(1, m+1):
-            arr = sorted_by_trim[trim-1][:]
-            arr = countingSortByDigit(arr, m - trim)
-            sorted_by_trim[trim] = arr
-
+            trimmed_list = countingSortByDigit(trimmed_list, m-trim)
+            trimmed_sorted_list[trim] = trimmed_list
+            
+        
         res = []
         for k, trim in queries:
-            res.append(sorted_by_trim[trim][k-1][1])
-        
+            res.append(trimmed_sorted_list[trim][k-1][1])
         return res
-
-
-
-
-
-
-
-
-
-
-        # version1 
-        answer = []
-        for k, trim in queries:
-            trimmed = [(num[-trim:], i) for i, num in enumerate(nums)]
-            # trimmed.sort(key=lambda x: (x[0], x[1]))
-            trimmed.sort()
-            answer.append(trimmed[k-1][1])
-        return answer
-
-
