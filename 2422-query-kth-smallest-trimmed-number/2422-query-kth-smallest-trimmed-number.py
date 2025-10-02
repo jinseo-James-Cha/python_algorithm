5a55fdbfe5285187cdacfd327c841cae2f834cdf
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-02 14:46:00 +0100

    Time: 432 ms (46.79%), Space: 17.9 MB (98.87%) - LeetHub

---
+++ 2422-query-kth-smallest-trimmed-number/2422-query-kth-smallest-trimmed-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
+        # equal length, only digits str
+        # queries = [k, trim] rightmost trim digit
+        answer = []
+        for k, trim in queries:
+            trimmed = [(num[-trim:], i) for i, num in enumerate(nums)]
+            trimmed.sort(key=lambda x: (x[0], x[1]))
+            answer.append(trimmed[k-1][1])
+        return answer
+
+
