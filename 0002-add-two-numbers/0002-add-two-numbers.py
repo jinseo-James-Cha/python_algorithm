899af2981fc2074e3a9a024cd4dfe5a4e8d47826
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-05 23:08:49 +0100

    Time: 0 ms (100%), Space: 17.8 MB (84.81%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,40 +1,18 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        is_next = False
-        res = ListNode()
-        dummy = res
-        while l1 or l2 or is_next:
-            l1_num = 0
-            l2_num = 0
-
-            if l1:
-                l1_num = l1.val
-                l1 = l1.next
-
-            if l2:
-                l2_num = l2.val
-                l2 = l2.next
-
-            prev = 0
-            if is_next:
-                is_next = False
-                prev = 1
-
-            curr = l1_num + l2_num + prev
-
-            if curr >= 10:
-                is_next = True
-                curr %= 10
-            
-            new_node = ListNode(curr)
-            dummy.next = new_node
-            dummy = dummy.next
-        
-        return res.next
-
-        
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        curr = dummyHead
+        carry = 0
+        while l1 != None or l2 != None or carry != 0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            columnSum = l1Val + l2Val + carry
+            carry = columnSum // 10
+            newNode = ListNode(columnSum % 10)
+            curr.next = newNode
+            curr = newNode
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummyHead.next
