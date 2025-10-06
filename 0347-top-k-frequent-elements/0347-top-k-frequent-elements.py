b082447343d49f931087b090915e050dd7abd5d4
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-06 11:35:51 +0100

    Time: 9 ms (28.49%), Space: 24.3 MB (13.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,26 +1,32 @@
-from collections import defaultdict
-import heapq
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # bucket sort
+        if len(nums) <= k:
+            return nums
+        
+        counts = Counter(nums)
         buckets = [[] for _ in range(len(nums) + 1)]
+        for num, freq in counts.items():
+            buckets[freq].append(num)
+        
+        # flatten
+        res = [x for bucket in buckets for x in bucket]
+        return res[::-1][:k]
+
+
+
+
+
+
+
+
+        if len(nums) == k:
+            return nums
+        
+        buckets = [[] for _ in range(len(nums) + 1)] # 만약 다 같은 숫자여도 저장가능해야해서 +1
         counts = Counter(nums)
         for num, freq in counts.items():
             buckets[freq].append(num)
-
-        flat_list = [item for sublist in buckets for item in sublist]
+        
+        flat_list = [x for bucket in buckets for x in bucket]
         return flat_list[::-1][:k]
-
-
-
-
-        # if k == len(nums):
-        #     return nums
-        
-        # counts = defaultdict(int)
-        # for num in nums:
-        #     counts[num] += 1
-        
-        # return heapq.nlargest(k, counts.keys(), key=counts.get)
-        
-        
