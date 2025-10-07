f3ca39707dbb80d5876055a00756a286e1bbd402
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-07 15:31:57 +0100

    Time: 6209 ms (5.01%), Space: 22.8 MB (41.36%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -2,7 +2,43 @@ import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         # as k much, we need the answers
-
+        
+        def quick_select(left, right, k):
+            if left <= right:
+                pivot_idx = partition(left, right)
+                if pivot_idx == k:
+                    return
+                elif pivot_idx < k:
+                    quick_select(pivot_idx+1, right, k)
+                else:
+                    quick_select(left, pivot_idx-1, k)
+        
+        def partition(left, right):
+            pivot = dist(points[right])
+            i = left
+            for j in range(left, right):
+                if dist(points[j]) <= pivot:
+                    points[i], points[j] = points[j], points[i]
+                    i += 1
+            points[i], points[right] = points[right], points[i]
+            return i
+        
+        
+        def dist(p):
+            return p[0]**2 + p[1]**2
+        
+        quick_select(0, len(points)-1, k)
+        return points[:k]        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         # quickselect
         def dist(p):
             return p[0] ** 2 + p[1] ** 2
