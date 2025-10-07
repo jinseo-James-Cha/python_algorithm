5b1eb3347e4b4c3675f517b852d39a9509dab7a7
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-07 14:39:29 +0100

    Time: 10 ms (13.18%), Space: 22.7 MB (5.05%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,15 +1,19 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         # intervals -> sweep line algorithm
+        # define events
         events = []
         for start, end in intervals:
             events.append((start, 1))
             events.append((end, -1))
+
+        # sort events
+        events.sort(key= lambda x: (x[0], x[1]))
         
-        events.sort()
-        
-        max_rooms, curr = 0, 0
+
+
+        max_rooms, curr_rooms = 0, 0
         for k, v in events:
-            curr += v
-            max_rooms = max(max_rooms, curr)
+            curr_rooms += v
+            max_rooms = max(max_rooms, curr_rooms)
         return max_rooms
