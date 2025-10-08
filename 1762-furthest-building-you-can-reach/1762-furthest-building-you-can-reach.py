f8c6e66336e2289d855592da14d57602466a7d4b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-08 20:29:14 +0100

    Time: 59 ms (26.79%), Space: 29.2 MB (20.11%) - LeetHub

--- 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.py
+++ 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.py
@@ -1,56 +1,22 @@
-import heapq
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        # start 0
-        # move to next building by using brick or ladder
-
-        # if building 0 height > = building 1 height -> no need brick or ladder
-        # if building 0 height < building 1 height -> one ladders or building 1 height - building 0 height
-        
-        # min heap - greedy
-        # min diff -> using brick / big diff -> using ladder
-        n = len(heights)
+        # heapq
+        res = 0
         ladder_allocations = []
-        for i in range(n - 1):
-            diff = heights[i+1] - heights[i]
+        for h1, h2 in zip(heights[:len(heights)], heights[1:]):
+            diff = h2 - h1
             if diff <= 0:
+                res += 1
                 continue
-            
+
             heapq.heappush(ladder_allocations, diff)
 
-            if len(ladder_allocations) <= ladders:
-                continue
+            if len(ladder_allocations) > ladders:
+                bricks -= heapq.heappop(ladder_allocations)
             
-            bricks -= heapq.heappop(ladder_allocations)
             if bricks < 0:
-                return i
-        
-        return n - 1
-
+                break
             
-
-        
-
-
-        # dp?! -> MLE
-        # choose ladder or bricks
-        # len = 7
-        # i = 0 ~ 6
-        @cache
-        def dp(i, bricks, ladders):
-            if i == len(heights)-1:
-                return i
+            res += 1
+        return res
             
-            diff = heights[i+1] - heights[i]
-            if diff <= 0:
-                return dp(i+1, bricks, ladders)
-
-            res = i
-            if bricks >= diff:
-                res = max(res, dp(i+1, bricks - diff, ladders))
-
-            if ladders > 0:
-                res = max(res, dp(i+1, bricks, ladders - 1))
-            
-            return res
-        return dp(0, bricks, ladders)
