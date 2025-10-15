af81697f8b2db596a26640c0960cc545520cca0f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-15 17:36:12 +0100

    Time: 3 ms (71.2%), Space: 18.6 MB (17.56%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -6,47 +6,67 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        rows = set()
+        cols = set()
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+        # rows
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
+        
+
+
+
+
+
+
+
         # using markers into first row and column
         # check there is already first row and column has zero
 
-        m, n = len(matrix), len(matrix[0])
+        # m, n = len(matrix), len(matrix[0])
 
-        # check first row has zero
-        first_row_has_zero = False
-        for c in range(n):
-            if matrix[0][c] == 0:
-                first_row_has_zero = True
-                break
+        # # check first row has zero
+        # first_row_has_zero = False
+        # for c in range(n):
+        #     if matrix[0][c] == 0:
+        #         first_row_has_zero = True
+        #         break
         
-        # check first col has zero
-        first_col_has_zero = False
-        for r in range(m):
-            if matrix[r][0] == 0:
-                first_col_has_zero = True
-                break
+        # # check first col has zero
+        # first_col_has_zero = False
+        # for r in range(m):
+        #     if matrix[r][0] == 0:
+        #         first_col_has_zero = True
+        #         break
         
-        # mark first row or col if there is 0 in it
-        for r in range(1, m):
-            for c in range(1, n):
-                if matrix[r][c] == 0:
-                    matrix[0][c] = 0
-                    matrix[r][0] = 0
+        # # mark first row or col if there is 0 in it
+        # for r in range(1, m):
+        #     for c in range(1, n):
+        #         if matrix[r][c] == 0:
+        #             matrix[0][c] = 0
+        #             matrix[r][0] = 0
 
-        # update other row and col if there is marker
-        for r in range(1, m):
-            for c in range(1, n):
-                if matrix[0][c] == 0 or matrix[r][0] == 0:
-                    matrix[r][c] = 0
+        # # update other row and col if there is marker
+        # for r in range(1, m):
+        #     for c in range(1, n):
+        #         if matrix[0][c] == 0 or matrix[r][0] == 0:
+        #             matrix[r][c] = 0
 
-        # if first row has zero already without marker, update too
-        if first_row_has_zero:
-            for c in range(n):
-                matrix[0][c] = 0
+        # # if first row has zero already without marker, update too
+        # if first_row_has_zero:
+        #     for c in range(n):
+        #         matrix[0][c] = 0
 
-        # col too
-        if first_col_has_zero:
-            for r in range(m):
-                matrix[r][0] = 0
+        # # col too
+        # if first_col_has_zero:
+        #     for r in range(m):
+        #         matrix[r][0] = 0
 
 
 
