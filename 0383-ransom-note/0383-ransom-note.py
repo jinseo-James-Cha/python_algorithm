4aec3ed92a64c954434aea305810b2b5a412884b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-16 16:22:03 +0100

    Time: 14 ms (79%), Space: 18.1 MB (34.57%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,5 +1,28 @@
+from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counts = Counter(magazine)
+        for c in ransomNote:
+            if c not in counts:
+                return False
+            
+            counts[c] -= 1
+            if counts[c] < 0:
+                return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
         dict_ransomNote = {}
         for r in ransomNote:
             dict_ransomNote[r] = 1 + dict_ransomNote.get(r, 0)
