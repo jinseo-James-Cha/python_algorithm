a693db0249ecc5d640f898acccec812041536b0a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-21 15:42:11 +0100

    Time: 59 ms (95.1%), Space: 51.4 MB (35.27%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -8,7 +8,6 @@ class Solution:
         # x shot burst -> xstart <= x <= xend
         
         points.sort(key=lambda x : x[1])
-        print(points)
         arrows = 1
         first_end = points[0][1]
         for x_start, x_end in points:
