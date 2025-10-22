4792630bae1daf4054a641410264155e9e100d11
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-22 10:28:59 +0100

    Time: 48 ms (56.73%), Space: 19.7 MB (86.22%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -3,62 +3,19 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if head is None:
+        # slow, fast algorithm
+        if not head or not head.next:
             return False
 
-        # Floyd's Cycle Finding Algorithm
         slow = head
         fast = head.next
         while slow != fast:
             if fast is None or fast.next is None:
                 return False
-            fast = fast.next.next
+            
             slow = slow.next
+            fast = fast.next.next
         return True
-
-
-
-
-
-#         slow = fast = head
-
-#         while fast and fast.next:
-#             slow = slow.next
-#             fast = fast.next.next
-#             if fast == slow:
-#                 return True
-#         return False
-
-
-# # v2: two pointers: fast and slow pointers
-# class Solution:
-#     def hasCycle(self, head: Optional[ListNode]) -> bool:
-#         left = head
-#         right = head.next
-#         while left.next and right.next:
-#             if left == right:
-#                 return True
-#             left = left.next
-#             right = right.next
-#         return False
-
-
-# class Solution:
-#     def hasCycle(self, head: Optional[ListNode]) -> bool:
-#         # how do I define it is cycle linked list or just long linked list?
-#         # 3 -> 2 next == -4 -> 2 next will be the name
-#         # can we compare them ? yes we can compare linked lists
-#         # let's use two pointer
-#         slow = head
-#         fast = head
-#         while fast and fast.next:
-#             slow = slow.next
-#             fast = fast.next.next
-
-#             if fast == slow:
-#                 return True
-        
-#         return False
-        
