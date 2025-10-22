8cb6aece44aac895e9dc8ec72f55a2a4d389d9da
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-22 10:52:13 +0100

    Time: 3 ms (12.09%), Space: 17.7 MB (88.55%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -27,14 +27,15 @@ class Solution:
             
             dummy = dummy.next
         
-        while list1:
-            dummy.next = list1
-            list1 = list1.next
-            dummy = dummy.next
+        # while list1:
+        #     dummy.next = list1
+        #     list1 = list1.next
+        #     dummy = dummy.next
 
-        while list2:
-            dummy.next = list2
-            list2 = list2.next
-            dummy = dummy.next
+        # while list2:
+        #     dummy.next = list2
+        #     list2 = list2.next
+        #     dummy = dummy.next
+        dummy.next = list1 if list1 is not None else list2
         return head.next
         
