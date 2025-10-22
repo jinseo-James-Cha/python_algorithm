a3fae22e6e432bdf8f2706a48ce6fde20046c054
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-22 10:50:21 +0100

    Time: 0 ms (100%), Space: 17.8 MB (61.48%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,49 +5,36 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None:
-            return list2
-        
-        if list2 is None:
-            return list1
-        
-        if list1.val >= list2.val:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
-        else:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-
-
-
-
-
-        # recursion
-        if list1 is None:
+        if list1 is None and list2 is None:
+            return None
+        elif list1 is None:
             return list2
         elif list2 is None:
             return list1
-        elif list1.val < list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
-
-
-        # iteration
-        # res = ListNode()
-        # dummy = res
-        # while list1 and list2:
-        #     if list1.val > list2.val:
-        #         dummy.next = list2
-        #         list2 = list2.next
-        #     else:
-        #         dummy.next = list1
-        #         list1 = list1.next
-        #     dummy = dummy.next
         
-        # dummy.next = list1 if list1 else list2
+        head = ListNode()
+        dummy = head
+        while list1 and list2:
+            l1_val = list1.val
+            l2_val = list2.val
 
-        # return res.next
+            if l1_val >= l2_val:
+                dummy.next = list2
+                list2 = list2.next
+            else:
+                dummy.next = list1
+                list1 = list1.next
+            
+            dummy = dummy.next
+        
+        while list1:
+            dummy.next = list1
+            list1 = list1.next
+            dummy = dummy.next
+
+        while list2:
+            dummy.next = list2
+            list2 = list2.next
+            dummy = dummy.next
+        return head.next
         
