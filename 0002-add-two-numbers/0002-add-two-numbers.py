ddaaa702b299ca233e1f033a360c3b745ef095a5
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-22 10:38:24 +0100

    Time: 7 ms (31.8%), Space: 17.9 MB (56.79%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,18 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(
-        self, l1: Optional[ListNode], l2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        dummyHead = ListNode(0)
-        curr = dummyHead
-        carry = 0
-        while l1 != None or l2 != None or carry != 0:
-            l1Val = l1.val if l1 else 0
-            l2Val = l2.val if l2 else 0
-            columnSum = l1Val + l2Val + carry
-            carry = columnSum // 10
-            newNode = ListNode(columnSum % 10)
-            curr.next = newNode
-            curr = newNode
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return dummyHead.next
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        res = ListNode()
+        dummy = res
+        is_carry = False
+
+        while l1 or l2:
+            l1_val = 0 
+            if l1 is not None:
+                l1_val = l1.val
+                l1 = l1.next
+            
+            l2_val= 0 
+            if l2 is not None:
+                l2_val = l2.val
+                l2 = l2.next
+
+            is_carry, remainder = divmod(l1_val+l2_val+is_carry, 10)
+            curr_node = ListNode(remainder)
+            dummy.next = curr_node
+            dummy = dummy.next
+        
+        if is_carry:
+            dummy.next = ListNode(is_carry)
+        return res.next
+        
