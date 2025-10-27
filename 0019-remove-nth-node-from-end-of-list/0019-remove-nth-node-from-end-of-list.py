769b551a96c6f0abdd091cf2a5592d455c82118c
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-27 23:28:37 +0000

    Time: 15 ms (0.56%), Space: 17.7 MB (54.75%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -37,14 +37,15 @@ class Solution:
         :type n: int
         :rtype: ListNode
         """
+
         dummy = ListNode(0)
         dummy.next = head
         first = dummy
         second = dummy
-        # Advances first pointer so that the gap between first and second is n nodes apart
         for i in range(n + 1):
             first = first.next
-        # Move first to the end, maintaining the gap
+            print(first)
+
         while first is not None:
             first = first.next
             second = second.next
