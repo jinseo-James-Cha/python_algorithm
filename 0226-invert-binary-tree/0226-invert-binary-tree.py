24c7c21d20262839fa73115dc850831c39835b9f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-30 16:51:48 +0000

    Time: 3 ms (2.62%), Space: 17.7 MB (81.18%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -6,16 +6,20 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # # DFS recursive
-        # if not root:
-        #     return None
+        if not root:
+            return None
         
-        # root.left, root.right = root.right, root.left
-        # self.invertTree(root.left)
-        # self.invertTree(root.right)
-        # return root
+        stack = [root]
+        while stack:
+            curr = stack.pop()
+            curr.left, curr.right = curr.right, curr.left
+            if curr.left:
+                stack.append(curr.left)
+            if curr.right:
+                stack.append(curr.right)
+        return root
 
-        # DFS stack
+        # DFS stack preferred
         if not root:
             return None
         
@@ -29,4 +33,15 @@ class Solution:
             if node.right:
                 stack.append(node.right)
         return root
+
+        # # DFS recursive
+        # if not root:
+        #     return None
+        
+        # root.left, root.right = root.right, root.left
+        # self.invertTree(root.left)
+        # self.invertTree(root.right)
+        # return root
+
+        
         
