2a8b1142e7fe0612993b6363f8228d31b06215bb
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-10-30 16:44:54 +0000

    Time: 0 ms (100%), Space: 17.6 MB (90.82%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,6 +7,26 @@
 from collections import deque
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # recursive
+        if not p and not q:
+            return True
+        
+        if not p and q:
+            return False
+        
+        if p and not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+
+
+
+
         # BFS
 
         def check(pp:TreeNode, qq:TreeNode):
