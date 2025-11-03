686910719044eb25c64afbcd90badec1133f6643
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-03 17:29:36 +0000

    Time: 0 ms (100%), Space: 17.8 MB (59.12%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,13 +6,47 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        # Morris preorder
+        root_to_leaf = curr_number = 0
+
+        curr = root
+        while curr:
+            if curr.left is None:
+                curr_number = curr_number * 10 + curr.val
+                if curr.right is None:
+                    root_to_leaf += curr_number
+                curr = curr.right
+            else:
+                predecessor = curr.left
+                steps = 1
+                while predecessor.right and predecessor.right != curr:
+                    predecessor = predecessor.right
+                    steps += 1
+                
+                if predecessor.right is None:
+                    curr_number = curr_number * 10 + curr.val
+                    predecessor.right = curr
+                    curr = curr.left
+                else:
+                    if predecessor.left is None:
+                        root_to_leaf += curr_number
+
+                    for _ in range(steps):
+                        curr_number //= 10
+                    predecessor.right = None
+                    curr = curr.right
+            
+        return root_to_leaf
+
+
+
         # sum root to leaf
         # parent * 10 -> child..
         
         def dfs(node, totalSum):
             if not node:
                 return 0
-                
+
             if not node.left and not node.right:
                 return totalSum*10 + node.val
 
