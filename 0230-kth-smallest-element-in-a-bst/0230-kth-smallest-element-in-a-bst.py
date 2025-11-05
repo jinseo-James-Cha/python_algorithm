6c35919ddbacd1887d455bd0c34d39ac6cdacf7c
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-05 14:40:04 +0000

    Time: 5 ms (7.48%), Space: 21.1 MB (27.39%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,28 +6,19 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # using morris with inorder -> ascending order
-        # res[k-1]
+        # need to check all nodes? even tho it is BST?
+        # left is smaller, right is greater ?
 
-        res = []
-        current = root
-        while current:
-            if current.left is None:
-                # visit
-                res.append(current.val)
-                current = current.right
-            else:
-                predecessor = current.left
-                while predecessor.right and predecessor.right != current:
-                    predecessor = predecessor.right
-                
-                # first visit to right? -> link to current
-                if predecessor.right is None:
-                    predecessor.right = current
-                    current = current.left
-                else:
-                    # already have link? -> remove link and move right
-                    predecessor.right = None # reset
-                    res.append(current.val)
-                    current = current.right
-        return res[k-1]
+        # bruth force -> get all vals and sort and k-1
+        vals = []
+        def dfs(node):
+            if not node:
+                return
+            
+            vals.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+        vals.sort()
+        return vals[k-1]
