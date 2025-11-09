a43f0688dee5a7c8d3c9f4be78678784c3bba3c5
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-09 21:57:03 +0000

    Time: 0 ms (100%), Space: 17.7 MB (65.76%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,43 +1,29 @@
 from collections import deque
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
-        # 8 characters -> A C G T
-        # startGene -> endGene
-        # AACCGGTT -> AACCGGTA is one mutation
-        # bank -> all valid gene mutations
-
-        # AACCGGTT start
-        # AAACGGTA end
-        # AACCGGTA bank1
-        # AACCGCTA bank2
-        # AAACGGTA bank3
-        
-        # AACCGGTT -> AACCGGTAbank1 -> AAACGGTAbank3 ==  end => 2
-
-        # Minimum number from startGene to endGene
-        # shortest path -> BFS dijkstra algorithm
+        # shortest path from startGene to endGene
+        # bank saves all edges
         if startGene == endGene:
             return 0
-                
-        # O(1) search for bank
+        
         bank_set = set(bank)
         if endGene not in bank_set:
             return -1
 
-        # dijkstra
-        visited = set([startGene])
-        queue = deque([(startGene, 0)]) # 
+        visited = set()
+        queue = deque([(startGene, 0)])
         while queue:
-            curr_string, curr_weight  = queue.popleft()
-            for i, ch in enumerate(curr_string):
+            curr_str, curr_d = queue.popleft()
+            
+            for i, c in enumerate(curr_str):
                 for choice in ['A', 'C', 'G', 'T']:
-                    if ch == choice:
+                    if c == choice:
                         continue
-                    else:
-                        temp_string = curr_string[:i] + choice + curr_string[i+1:]
-                        if temp_string in bank_set and temp_string not in visited:
-                            visited.add(temp_string)
-                            if temp_string == endGene:
-                                return curr_weight + 1
-                            queue.append((temp_string, curr_weight+1))
+                    
+                    temp_str = curr_str[:i] + choice + curr_str[i+1:]
+                    if temp_str in bank_set and temp_str not in visited:
+                        if temp_str == endGene:
+                            return curr_d + 1
+                        visited.add(temp_str)
+                        queue.append((temp_str, curr_d+1))
         return -1
