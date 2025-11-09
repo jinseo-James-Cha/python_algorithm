abb7555da294b55516998470799c5800157fbaab
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-09 22:14:35 +0000

    Time: 0 ms (100%), Space: 17.8 MB (40.92%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,6 +1,45 @@
 from collections import deque
+import heapq
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        # dijkstra algorithm
+        if startGene == endGene:
+            return 0
+        
+        dist = {}
+        for b in bank:
+            dist[b] = float('inf')
+        dist[startGene] = 0
+
+        if endGene not in dist:
+            return -1
+
+        q = [(0, startGene)]
+        while q:
+            d, curr_str = heapq.heappop(q)
+            if d > dist.get(curr_str, float('inf')):
+                continue
+            
+            if curr_str == endGene:
+                return d
+            
+            for i in range(len(curr_str)):
+                for ch in 'ACGT':
+                    if curr_str[i] == ch:
+                        continue
+                    
+                    neighbor_str = curr_str[:i] + ch + curr_str[i+1:]
+                    if neighbor_str in dist and dist.get(neighbor_str, float('inf')) > dist[curr_str] + 1:
+                        dist[neighbor_str] = dist[curr_str] + 1
+                        heapq.heappush(q, (dist[neighbor_str], neighbor_str))
+        return -1
+
+
+
+
+
+
+        # BFS
         # shortest path from startGene to endGene
         # bank saves all edges
         if startGene == endGene:
