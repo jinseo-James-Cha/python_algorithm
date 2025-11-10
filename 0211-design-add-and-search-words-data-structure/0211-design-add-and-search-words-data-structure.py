bc3b1ab57a29aba82debe081540424b5aa51644d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-10 20:21:34 +0000

    Time: 1438 ms (26.19%), Space: 65.6 MB (48.19%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -2,39 +2,35 @@ class TrieNode:
     def __init__(self):
         self.children = {}
         self.is_word = False
-
 class WordDictionary:
 
     def __init__(self):
         self.root = TrieNode()
+        
 
     def addWord(self, word: str) -> None:
-        node = self.root
-        for c in word:
-            if c not in node.children:
-                node.children[c] = TrieNode()
-            node = node.children[c]
-        node.is_word = True
+        curr = self.root
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = TrieNode()
+            curr = curr.children[w]
+        curr.is_word = True
+        
 
     def search(self, word: str) -> bool:
-        return self.search_helper(0, word, self.root)
-    
-    def search_helper(self, word_index: int, word: str, node: TrieNode) -> bool:
-        for i in range(word_index, len(word)):
-            c = word[i]
-            # wildcard
-            if c == '.':
-                # search in children values
-                for child in node.children.values():
-                    if self.search_helper(i + 1, word, child):
-                        return True
-                return False
-            elif c in node.children:
-                node = node.children[c]
-            else:
-                return False
-        return node.is_word
-        
+        def search_in_node(word, node):
+            for i, ch in enumerate(word):
+                if ch not in node.children:
+                    if ch == ".":
+                        for x in node.children:
+                            if x != "$" and search_in_node(word[i+1:], node.children[x]):
+                                return True
+                    return False
+                else:
+                    node = node.children[ch]
+            return node.is_word
+
+        return search_in_node(word, self.root)
 
 
 # Your WordDictionary object will be instantiated and called as such:
