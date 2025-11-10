6d1d1134f5fa1d5ff5cd1c0a905821cdd6efb690
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-10 21:07:07 +0000

    Time: 142 ms (37.23%), Space: 59.7 MB (56.02%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,64 +1,17 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def backtrack(start, current):
-            if len(current) == k:
-                res.append(current[:])
+        # combination questions -> backtrack ?
+        # 1 ~ n and k 
+        def backtrack(start, curr):
+            if len(curr) == k:
+                res.append(curr[:])
                 return
             
             for i in range(start, n+1):
-                current.append(i)
-                backtrack(i+1, current)
-                current.pop()
-            
-        res = []
-        backtrack(1, [])
-        return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # 1. intuition
-        # I don't know the loop's depths -> backtracking?
-        # no duplications -> [1,2] == [2, 1]
-        # using for loop in range(1, n+1)
-        # 1 -> 2 and pop and call backtrack 1 -> 3 ~~~
-        # len == k -> put res and pop
-
-        # 2. complexity
-        # Backtrack: no duplication -> N^N
-        # 20^20 -> is it possible to use backtrack?
-
-        # 3. data structure
-        # res: [][] no duplication
-        # cur: []
-
-        def backtrack(start: int, cur: List[int]):
-            if len(cur) == k:
-                res.append(cur[:])
-                return
-            for i in range(start, n + 1):
-                cur.append(i)
-                backtrack(i + 1, cur)
-                cur.pop()
+                curr.append(i)
+                backtrack(i+1, curr)
+                curr.pop()
         
         res = []
         backtrack(1, [])
         return res
-    
-    
