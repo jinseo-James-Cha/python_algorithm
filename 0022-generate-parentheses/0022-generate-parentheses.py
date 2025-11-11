3ecd2c4d03df606984e12feca7e9d010981bd6f4
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-11 16:39:44 +0000

    Time: 1 ms (42.8%), Space: 18 MB (44.56%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,21 +1,24 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # Backtrack
-        def backtrack(curr, left_count, right_count):
-            if len(curr) == n*2:
+        # all combinations -> backtrack ?
+        def backtrack(curr, left, right):
+            # base case
+            if left + right == n*2:
                 res.append("".join(curr))
                 return
             
-            if left_count < n:
-                curr.append("(")
-                backtrack(curr, left_count+1, right_count)
+            if left < n:
+                curr.append('(')
+                backtrack(curr, left+1, right)
                 curr.pop()
             
-            if right_count < left_count:
-                curr.append(")")
-                backtrack(curr, left_count, right_count+1)
+            if right < n and right < left:
+                curr.append(')')
+                backtrack(curr, left, right + 1)
                 curr.pop()
-        
+
         res = []
         backtrack([], 0, 0)
         return res
+            
+
