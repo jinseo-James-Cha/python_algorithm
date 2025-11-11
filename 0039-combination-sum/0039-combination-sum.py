3f29fd70926de95cf20191472a5cd9fdba1db03e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-11 15:55:20 +0000

    Time: 9 ms (64.32%), Space: 17.8 MB (60.51%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,19 @@
 class Solution:
-    def combinationSum(
-        self, candidates: List[int], target: int
-    ) -> List[List[int]]:
-
-        res = []
-
-
-        def backtrack(start, remainder, combination):
-            if remainder == 0:
-                res.append(combination[:])
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # return a list of all unique combination?! -> backtrack
+        def backtrack(start, curr, remainder):
+            if 0 > remainder:
                 return
-            elif remainder < 0:
+            
+            if remainder == 0:
+                res.append(curr[:])
                 return
             
             for i in range(start, len(candidates)):
-                combination.append(candidates[i])
-                backtrack(i, remainder - candidates[i], combination)
-                combination.pop()
-
-        backtrack(0, target, [])
+                curr.append(candidates[i])
+                backtrack(i, curr, remainder - candidates[i])
+                curr.pop()
+            
+        res = []
+        backtrack(0, [], target)
         return res
