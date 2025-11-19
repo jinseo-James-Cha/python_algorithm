804b679132f9050d7fd037cced07eda466d5f2ac
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-19 21:13:49 +0000

    Time: 0 ms (100%), Space: 18.3 MB (72.67%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,17 +1,16 @@
-# Binaray search time -> O(log(n))
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        # first write how to find target
         left = 0
         right = len(nums) - 1
-        while left <= right:
+        if nums[right] < target:
+            return right + 1
+        elif nums[left] > target:
+            return left
+
+        while left < right:
             mid = (left + right) // 2
-            if nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 left = mid + 1
             else:
-                return mid
-        # left and right meets at the same spot
+                right = mid
         return left
-        
