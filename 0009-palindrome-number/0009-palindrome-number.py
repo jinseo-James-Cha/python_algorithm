e55ec236d4e0901ac549fc05b48d2906fdfa08ed
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-24 17:06:49 +0000

    Time: 6 ms (66.61%), Space: 18 MB (19.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,34 +1,20 @@
-# Follow up: Could you solve it without converting the integer to a string?
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # all negative cannot be True
-        if x < 0:
+        if x < 0 :
             return False
+        
+        if x == 0:
+            return True
 
-        # copy original
-        original = x
-
-        # for loop from the back
-        back_num = 0
+        stack = []
         while x > 0:
-            back_num *= 10
-            back_num += x % 10
-            x = x // 10
+            stack.append(x % 10)
+            x //= 10
         
-        return original == back_num
-            
-
-
-# class Solution:
-#     def isPalindrome(self, x: int) -> bool:
-#         # all negative cannot be True
-#         if x < 0:
-#             return False
-        
-#         x_str = str(x)
-#         x_str_rev = x_str[::-1]
-
-#         return x_str == x_str_rev
-
-
-        
+        left, right = 0, len(stack) - 1
+        while left < right:
+            if stack[left] != stack[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
