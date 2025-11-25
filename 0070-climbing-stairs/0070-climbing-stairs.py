64cba22d8e18901474021ab2c46fa322c41f7e86
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-25 17:02:37 +0000

    Time: 0 ms (100%), Space: 17.6 MB (95.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,33 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # bottom up optimized
-        if n <= 2:
-            return n
-        prev, prev_prev = 2, 1
-        for i in range(3, n+1):
-            curr = prev + prev_prev
-
-            prev_prev = prev
-            prev = curr
-        return prev
-
-        # bottom up
-        if n <= 2:
-            return n
-        dp = [0] * (n+1)
-        dp[1], dp[2] = 1, 2
-        
-        for i in range(3, n+1):
-            dp[i] = dp[i-2] + dp[i-1]
-        return dp[n]
-        
-        
         # top down
-        @cache
+        
+        
         def dp(i):
             if i <= 2:
                 return i
             
-            return dp(i-2) + dp(i-1)
+            if i not in memo:
+                memo[i] = dp(i-1) + dp(i-2)
+            return memo[i]
         
+        memo = {}
         return dp(n)
