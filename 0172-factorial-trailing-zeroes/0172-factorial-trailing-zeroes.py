177ed43c90dfd77bdc24c718d67b20f41db2f870
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-25 16:45:25 +0000

    Time: 7645 ms (10.61%), Space: 18.1 MB (6.23%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,12 +1,17 @@
 from collections import Counter
 class Solution:
     def trailingZeroes(self, n: int) -> int:
+        
         def fatorial(n: int):
             if n <= 2:
                 return n
             
-            return fatorial(n-1) + factorial(n-2)
+            if n not in memo:
+                memo[n] = fatorial(n-1) + factorial(n-2)
+            
+            return memo[n]
         
+        memo = {}
         num = factorial(n)
         
         res = 0
