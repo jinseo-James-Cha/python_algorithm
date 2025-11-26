5c29d449f08c28f57aabad2893b3a821ca9e4c9d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-26 22:54:50 +0000

    Time: 0 ms (100%), Space: 18 MB (12.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
-            return max(nums)
-            
         def dp(i):
             if i == 0:
                 return nums[0]
