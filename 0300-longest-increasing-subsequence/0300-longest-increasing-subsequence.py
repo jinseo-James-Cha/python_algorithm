75baaa90fc24f4e6bbd1528cee9d70dee6577528
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-11-30 22:13:30 +0000

    Time: 3 ms (95.88%), Space: 18 MB (53.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,15 +1,21 @@
 from bisect import bisect_left
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1] * len(nums)
+        # 10 9 2 5 3 7
+        # 10
+        # 9
+        # 2
+        # 2 5
+
+        lis = [nums[0]]
         for i in range(1, len(nums)):
-            current = 1
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    current = max(current, dp[j] + 1)
-            dp[i] = current
-        print(dp)
-        return max(dp)
+            if lis[-1] >= nums[i]:
+                lis_idx = bisect_left(lis, nums[i])
+                lis[lis_idx] = nums[i]
+            else:
+                lis.append(nums[i])
+        print(lis)
+        return len(lis)
 
 
 
@@ -22,15 +28,26 @@ class Solution:
 
 
 
+        # dp bottom up
+        # dp = [1] * len(nums)
+        # for i in range(1, len(nums)):
+        #     current = 1
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             current = max(current, dp[j] + 1)
+        #     dp[i] = current
+        # return max(dp)
 
-        # subs = [nums[0]]
-        # for num in nums:
-        #     if num > subs[-1]:
-        #         subs.append(num)
-        #     else:
-        #         i = bisect_left(subs, num)
-        #         subs[i] = num
-        # return len(subs)
+        # o(nlogn)
+        subs = [nums[0]]
+        for num in nums:
+            if num > subs[-1]:
+                subs.append(num)
+            else:
+                i = bisect_left(subs, num)
+                subs[i] = num
+        print(subs)
+        return len(subs)
 
 
 
