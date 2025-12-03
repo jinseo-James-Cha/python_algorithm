233e5ef00e44b8b4a7086bbf7d00b5b52e505220
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-03 22:27:21 +0000

    Time: 3 ms (11.57%), Space: 18.1 MB (18.18%) - LeetHub

--- 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.py
+++ 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.py
@@ -1,5 +1,24 @@
 class Solution:
     def movesToMakeZigzag(self, nums: List[int]) -> int:
+
+        moves_even_peaks = 0
+        moves_odd_peaks = 0
+        n = len(nums)
+
+        for i in range(n):
+            left = nums[i - 1] if i - 1 >= 0 else float('inf')
+            right = nums[i + 1] if i + 1 < n else float('inf')
+            need = max(0, nums[i] - min(left, right) + 1)
+            if i % 2 == 0:
+                moves_even_peaks += need
+            else:
+                moves_odd_peaks += need
+
+        return min(moves_even_peaks, moves_odd_peaks)
+        
+
+
+        
         nums = [float('inf')] + nums + [float('inf')]
 
         n = len(nums)
@@ -15,4 +34,4 @@ class Solution:
                 minimum_odd_greater += nums[i] - min(nums[i-1], nums[i+1]) + 1
         
         return min(minimum_even_greater, minimum_odd_greater)
-        
+
