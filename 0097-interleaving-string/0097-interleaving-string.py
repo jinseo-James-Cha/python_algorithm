054b784b1e8bd5b30a58a65ca851c3a35e15e809
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-05 18:08:36 +0000

    Time: 41 ms (75.61%), Space: 18.5 MB (29.65%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,5 +1,28 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        # add index when they matching?
+        if len(s3) != len(s1) + len(s2):
+            return False
+
+        def dp(s1_idx, s2_idx, s3_idx):
+            if s3_idx == len(s3):
+                return True
+            
+            if (s1_idx, s2_idx, s3_idx) not in memo:
+                possible = False
+                if s1_idx < len(s1) and s1[s1_idx] == s3[s3_idx]:
+                    possible = dp(s1_idx+1, s2_idx, s3_idx+1)
+                
+                if s2_idx < len(s2) and s2[s2_idx] == s3[s3_idx]:
+                    possible = possible or dp(s1_idx, s2_idx+1, s3_idx+1)
+                
+                memo[(s1_idx, s2_idx, s3_idx)] = possible
+            
+            return memo[(s1_idx, s2_idx, s3_idx)]
+            
+        memo = {}
+        return dp(0, 0, 0)
+
         # s3 formed by interleaving s1 and s2
         
         # state
