cc13f80361b9698ff5dc9200c6e61d06dfae6150
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-05 18:20:10 +0000

    Time: 48 ms (37.41%), Space: 18 MB (56.79%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -12,7 +12,7 @@ class Solution:
                     dp[i][j] = True
                 
                 elif i == 0:
-                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[j -1]
+                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[j-1]
                 
                 elif j == 0:
                     dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i-1]
