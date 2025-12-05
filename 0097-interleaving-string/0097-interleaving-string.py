d9674dab605aff6b49c63929abc3adeae2b9bbb1
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-05 18:17:39 +0000

    Time: 44 ms (60.84%), Space: 18.3 MB (37.04%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,5 +1,34 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        # dp - bottom up
+        if len(s3) != len(s1) + len(s2):
+            return False
+
+        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+        for i in range(len(s1)+1):
+            for j in range(len(s2) + 1):
+                if i == 0 and j == 0 :
+                    dp[i][j] = True
+                
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[j -1]
+                
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i-1]
+                
+                else:
+                    s1_match = dp[i-1][j] and s1[i-1] == s3[i + j - 1]
+                    s2_match = dp[i][j-1] and s2[j-1] == s3[i + j - 1]
+                    dp[i][j] = s1_match or s2_match
+        return dp[len(s1)][len(s2)]
+                
+
+
+
+
+        
+        # dp - top down
         # add index when they matching?
         if len(s3) != len(s1) + len(s2):
             return False
