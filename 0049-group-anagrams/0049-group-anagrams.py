bdba2a6b315f7a83caed23fe5d9af27be9e323ab
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-09 17:31:38 +0000

    Time: 11 ms (83.05%), Space: 21.3 MB (38.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,13 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if len(strs) <= 1:
-            return [strs]
-
         d = defaultdict(list)
         for s in strs:
-            # how to make a str as unique key into dict's key
-            temp_list = sorted(s)
-            temp_str = "".join(temp_list)
-            d[temp_str].append(s)
-
+            sorted_s = sorted(s)
+            d[tuple(sorted_s)].append(s)
+        
         res = []
         for v in d.values():
             res.append(v)
         return res
-
-        
+            
