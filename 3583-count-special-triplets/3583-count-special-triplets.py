496465422f3387ff0819a27519212decd9c068a2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-09 13:25:48 +0000

    Time: 871 ms (21.17%), Space: 47.5 MB (15.31%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.py
+++ 3583-count-special-triplets/3583-count-special-triplets.py
@@ -2,58 +2,58 @@ from collections import defaultdict
 class Solution:
     def specialTriplets(self, nums: List[int]) -> int:
         # o(n)
-        MOD = 10**9 + 7
-        num_cnt = defaultdict(int)
-        num_partial_cnt = defaultdict(int)
+        # MOD = 10**9 + 7
+        # num_cnt = defaultdict(int)
+        # num_partial_cnt = defaultdict(int)
 
-        for num in nums:
-            num_cnt[num] += 1
+        # for num in nums:
+        #     num_cnt[num] += 1
         
-        res = 0
-        for num in nums:
-            target = num * 2
-            left_cnt = num_partial_cnt.get(target, 0)
+        # res = 0
+        # for num in nums:
+        #     target = num * 2
+        #     left_cnt = num_partial_cnt.get(target, 0)
             
-            num_partial_cnt[num] += 1
+        #     num_partial_cnt[num] += 1
 
-            right_cnt = num_cnt.get(target, 0) - num_partial_cnt.get(target, 0)
+        #     right_cnt = num_cnt.get(target, 0) - num_partial_cnt.get(target, 0)
 
-            res = (res + left_cnt * right_cnt) % MOD
-        return res
-
-
+        #     res = (res + left_cnt * right_cnt) % MOD
+        # return res
 
 
 
-        # O(n**2)
-        MOD = 10**9 + 7
-        res = 0
-        for j in range(1, len(nums)-1): # j range 
-            i = j - 1
-            k = j + 1
 
-            # count left
-            count_left = 0
-            while i >= 0:
-                if nums[i] == nums[j] * 2:
-                    count_left += 1
-                i -= 1
+
+        # # O(n**2)
+        # MOD = 10**9 + 7
+        # res = 0
+        # for j in range(1, len(nums)-1): # j range 
+        #     i = j - 1
+        #     k = j + 1
+
+        #     # count left
+        #     count_left = 0
+        #     while i >= 0:
+        #         if nums[i] == nums[j] * 2:
+        #             count_left += 1
+        #         i -= 1
             
-            if count_left == 0:
-                continue
+        #     if count_left == 0:
+        #         continue
 
-            # count right
-            count_right = 0
-            while k < len(nums):
-                if nums[k] == nums[j] * 2:
-                    count_right += 1
-                k += 1
+        #     # count right
+        #     count_right = 0
+        #     while k < len(nums):
+        #         if nums[k] == nums[j] * 2:
+        #             count_right += 1
+        #         k += 1
             
-            if count_right == 0:
-                continue
+        #     if count_right == 0:
+        #         continue
                         
-            res = (res + count_left * count_right) % MOD
-        return res
+        #     res = (res + count_left * count_right) % MOD
+        # return res
 
 
         # O(n log n)
