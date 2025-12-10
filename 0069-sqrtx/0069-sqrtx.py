7bf9230af5a85b921c2ceb4208e5f03eae7d48e8
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-10 23:06:55 +0000

    Time: 0 ms (100%), Space: 17.7 MB (50.47%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,20 +1,8 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        # spare root x -> ?
-        # or nearest integer round down -> 2.82.. -> 2 = int(...)
-        # 0 -> 0 #1
-        # 1 -> 1, 2, 3  #3
-        # 2 -> 4, 5, 6, 7, 8 #5
-        # 3 -> 9, 10, 11, 12, 13, 14, 15 # 7
-        # 4 -> 16, 17, 18, 19, 20, 21, 22, 23, 24 #9
-
-        # a**2 <= x < a+1**2 ->a
-        a = 0
-        while a**2 <= 2**31 - 1 :
-            if a**2 <= x < (a+1)**2:
-                return a
-            a += 1
-        return 0
-
+        if x < 2:
+            return x
         
-        
+        left = int(e ** (0.5 * log(x)))
+        right = left + 1
+        return left if right * right > x else right
