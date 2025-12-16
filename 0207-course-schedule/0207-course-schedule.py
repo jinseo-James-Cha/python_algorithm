447103a62c348c4470f4eeebe4922bf98ae3f6fa
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-16 21:57:39 +0000

    Time: 7 ms (46.18%), Space: 18.8 MB (98.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,42 @@
 from collections import defaultdict, deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list)
-        indegree = [0] * numCourses
+        # topological sort
+        # 1. define hashmap with list
+        # 1-1 count indegree of dest
 
-        # b -> a
-        for a, b in prerequisites:
-            graph[b].append(a)
-            indegree[a] += 1
+        # 2. find and insert root node which has 0 indegree into double ended queue
+        
+        # 3. loop the queue
+        # 3-1 popleft the first one which is 0 indegree dest node
+        # 3-2 tookCourses += 1
+        # 3-3 loop hashmap[poppedNode]'s neighbors and minus its indegree
+        # 3-4 if indegree ==0 then insert into the queue
 
+        # 4. check tookCourses == numCourses
+        
+        # 1
+        hashmap = defaultdict(list)
+        indegree = defaultdict(int)
+        for dest, src in prerequisites:
+            hashmap[src].append(dest)
+            indegree[dest] += 1
+        
+        # 2
         queue = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
-
-        res = []
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+        
+        # 3
+        tookCourse = 0
         while queue:
-            node = queue.popleft()
-            res.append(node)
+            currCourse = queue.popleft()
+            tookCourse += 1
 
-            for neighbor in graph[node]:
+            for neighbor in hashmap[currCourse]:
                 indegree[neighbor] -= 1
                 if indegree[neighbor] == 0:
                     queue.append(neighbor)
         
-        return len(res) == numCourses
-        
-        
+        return tookCourse == numCourses
