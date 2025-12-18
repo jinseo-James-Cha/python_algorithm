d6f64041b95240d5e0857297cedb58622d7855ba
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-18 17:51:30 +0000

    Time: 5 ms (81.16%), Space: 17.6 MB (99.98%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,63 +1,42 @@
-# v2
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        count = 0
-        for i in range(len(flowerbed)):
-            # Check if the current plot is empty.
-            if flowerbed[i] == 0:
-                # first two
-                empty_left_plot = (i == 0) or (flowerbed[i - 1] == 0)
+        newbed = [0] + flowerbed[:] + [0]
+        for i in range(1, len(flowerbed)+1):
+            if newbed[i]:
+                continue
 
-                # last two
-                empty_right_lot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
-                
-                # If both plots are empty, we can plant a flower here.
-                if empty_left_plot and empty_right_lot:
-                    flowerbed[i] = 1
-                    count += 1
-                    if count >= n:
-                        return True
-                    
-        return count >= n
+            if not newbed[i-1] and not newbed[i+1]:
+                newbed[i] = 1
+                n -= 1
 
-class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        # flowers cannot be planted in adjacent plots.
-        # 0 or 1
-        # 0 -> empty
-        # 1 -> not empty
-        # n >= count is true
-        # 1 0 0 0 1 -> 1 0 1 0 1  -> 1 is true but 2 is false
+        return n <= 0
 
-        # lets get the maximum num and check n <= count
-        # fixed sliding window ?!
-        # size 3 and move all to right and check 000 or not. 000 is count +=1
-        count = 0
-        l = len(flowerbed)
-        if l < 3:
-            if not 1 in flowerbed:
-                count = 1
-            return n <= count
 
-        # check first 2
-        if True not in flowerbed[0:2]:
-            flowerbed[0] = 1
-            count += 1
+
+        # 1 0 0 0 1
+        # 1 pass
+        # 0 check -> prev and next are 0 then n - 1
+        # -> way one
+        # <- way one
+
+        # -> way 1 "1" "1" 0 1 candidates 1, 2
+        # <- way 1  0   1  1 1 only index 3 a
+        # candidates = set()
+        # len_flowerbed = len(flowerbed)
+        # for i in range(len_flowerbed-1):
+        #     if flowerbed[i]:
+        #         continue
+            
+        #     if not flowerbed[i+1]:
+        #         candidates.add(i)
         
-        # check last 2
-        if True not in flowerbed[len(flowerbed)-2:]:
-            flowerbed[len(flowerbed)-1] = 1
-            count += 1
-
-        # check middle
-        left = 0
-        right = 3
-        while right < len(flowerbed):                
-            if True not in flowerbed[left:right]:
-                flowerbed[left + 1] = 1
-                count += 1
-            left += 1
-            right += 1
+        # for j in range(len_flowerbed-1, 0, -1):
+        #     if flowerbed[j]:
+        #         if j in candidates:
+        #             candidates.remove(j)
+        #         continue
+            
+        #     if flowerbed[j-1] and j in candidates:
+        #         candidates.remove(j)
         
-        
-        return n <= count
+        # return len(candidates) == n
