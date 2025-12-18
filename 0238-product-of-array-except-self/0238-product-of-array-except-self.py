afeb1e25bf91ca662e115c42ef13da8891d62e96
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-18 18:33:22 +0000

    Time: 27 ms (48.19%), Space: 23.6 MB (40.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,47 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        res = [1] * n
+        # 1 2 3 4
+        # 1 = right 2 * 3 * 4
+        # 2 = left1 *  right3 * 4 
+        # 3 = left1*2 * right4
+        # 4 = left 1*2*3
 
-        # idea is all multiple by i-1 * all multiple by (i+1)
-        # im the middle and multiple left and right
-        
-        # 1. left first
-        # 1 2 3 4 -> 1 
-        for i in range(1, n):
-            res[i] = res[i - 1] * nums[i - 1]
-        # 2. right 
-        right_product = 1
-        for i in range(n - 1 , -1, -1):
-            res[i] *= right_product
-            right_product *= nums[i]
+        # -> way
+        res = [1] * len(nums)
+        curr = nums[0]
+        for i in range(1, len(nums)):
+            res[i] = curr
+            curr *= nums[i]
+        print(res, curr) # [0, 1, 2, 6] 24
+
+        curr = nums[-1]
+        for j in range(len(nums)-2,-1, -1): # 2
+            res[j] *= curr
+            curr *= nums[j]
         
         return res
 
-# class Solution:
-#     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # answer[i] == sum(nums) - nums[i]
-        # O(n)
-
-        # 1 2 3 4
-        # 24 12 8 6
-        # 2 * 3 * 4 = 24 % 1 = 24
-        # 1 * 3 * 4 = 24 % 2 = 12
-        # 1 * 2 * 4 = 24 % 3 = 8
-        # 1 * 2 * 3 = 24 % 4 = 6
-        
-        # 1 .get all multiple num
-        # loop one by one and divide by index num
-        # total = 1
-        # for n in nums:
-        #     total *= n
-
-        # answer = []
-        # for i, n in enumerate(nums):
-        #     # cannot resolve by this case..
-        #     if n == 0:
-        #         temp = 
-        #     else:
-        #         temp = int(total / n)
-        #     answer.append(temp)
-        # return answer
