79dedfaaa01acedf56d82989726c06e080a6c1fc
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-21 21:36:02 +0000

    Time: 99 ms (68.07%), Space: 27.9 MB (96.88%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,44 +1,14 @@
-# Two pointers...
-# [1,8,6,2,5,4,8,3,7]
-# use inward ? and check left or right to move to make greater output?
-# uh? min(height[left], height[right]) * (right - left) = 49
-
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # two pointers
-        left = 0
-        right = len(height) - 1
-        max_amount = 0
-        while left < right:
-            curr_amount = min(height[left], height[right]) * (right-left)
-            max_amount = max(max_amount, curr_amount)
-            if height[left] > height[right]:
-                right -= 1
-            elif height[left] < height[right]:
-                left += 1
-            else:
-                right -= 1
-                left += 1
-        return max_amount
-
-
-
-
-
-
-        # try 2
-        maximum_amount = 0
-        
+        # 190
+        max_water = 0
         left = 0
         right = len(height) - 1
         while left < right:
             w = right - left
             h = min(height[left], height[right])
-            current_amount = h * w
-            
-            maximum_amount = max(current_amount, maximum_amount)
+            max_water = max(max_water, w*h)
 
-            # move smaller line to the next
             if height[left] > height[right]:
                 right -= 1
             elif height[left] < height[right]:
@@ -46,31 +16,4 @@ class Solution:
             else:
                 left += 1
                 right -= 1
-        return maximum_amount
-
-
-
-
-
-
-
-
-
-
-
-
-        # left = 0
-        # right = len(height) - 1
-        # m = 0
-        # while left < right:
-        #     h = min(height[left], height[right]) * (right - left) 
-        #     m = max(m, h)
-        #     if height[left] < height[right]:
-        #         left += 1
-        #     elif height[left] = height[right]:
-        #         left += 1
-        #         right -= 1
-        #     else:
-        #         right -= 1
-        # return m
-        
+        return max_water
