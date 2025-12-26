37a10fd39017b7d3b8cf3c085ec7b905a4a7d9a0
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-26 17:42:34 +0000

    Time: 75 ms (57.9%), Space: 17.7 MB (99.31%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -0,0 +1,43 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        # visit log indexed 0, 'N' or 'Y'
+        # if ith is 'Y', customers come at ith hour
+        # if 'N', no customer at ith
+        # shop closes 'j'th hour
+        
+        # Panalty calculation
+        # 1. shop is open, no customer -> +1
+        # 2. shop is close, customer -> +1
+
+        # return earliest hour to have minimize panalty.
+        # YYNY
+        # 0123
+        
+        # 0close -> YY Y -> 3 -> how many Y
+        # 1close ->  Y Y -> 2
+        # 2close ->    Y -> 1
+        # 3close ->   NY -> 2
+        # 4close ->   N  -> 1
+        # 2, 4 is the minimum and 2 is the earliest
+
+        #  YYNY
+        # 32121
+        n = len(customers)
+        
+        curr_panalty = 0
+        for hour in range(n):
+            if customers[hour] == 'Y':
+                curr_panalty += 1
+
+        minimum_panalty = curr_panalty
+        minimum_hour = 0
+        for hour in range(n): # YYNY , 3
+            if customers[hour] == 'Y':
+                curr_panalty -= 1
+            else:
+                curr_panalty += 1
+            
+            if curr_panalty < minimum_panalty:
+                minimum_panalty = curr_panalty
+                minimum_hour = hour+1
+        return minimum_hour
