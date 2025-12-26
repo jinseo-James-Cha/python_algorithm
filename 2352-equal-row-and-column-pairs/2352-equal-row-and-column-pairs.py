0da3315a5ef9601fc0dafbab4029bdd074c47955
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-26 18:08:28 +0000

    Time: 91 ms (29.93%), Space: 31 MB (5.32%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,5 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.count = 0
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.trie = TrieNode()
+    
+    def insert(self, array):
+        curr_trie = self.trie
+        for a in array:
+            if a not in curr_trie.children:
+                curr_trie.children[a] = TrieNode()
+            curr_trie = curr_trie.children[a]
+        curr_trie.count += 1
+        
+    def search(self, array):
+        curr_trie = self.trie
+        for a in array:
+            if a not in curr_trie.children:
+                return 0
+            
+            curr_trie = curr_trie.children[a]
+        return curr_trie.count
+
+
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
+        # Trie
+        my_trie = Trie()
+        count = 0
+        for row in grid:
+            my_trie.insert(row)
+        
+        n = len(grid)
+        for col in range(n):
+            col_array = [grid[i][col] for i in range(n)]
+            count += my_trie.search(col_array)
+        return count
+
+
+
+
+        # hashmap
         n = len(grid)
         row_hashmap = {}
         for row in grid:
