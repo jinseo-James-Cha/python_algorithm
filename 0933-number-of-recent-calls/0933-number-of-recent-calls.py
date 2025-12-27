cd3d2d7c5830bf8cda8e1d70be9d78996ba5475f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-27 23:39:47 +0000

    Time: 46 ms (39.5%), Space: 23.3 MB (10.55%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -0,0 +1,19 @@
+from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.sliding_window = deque()
+
+    def ping(self, t: int) -> int:
+        self.sliding_window.append(t)
+        while self.sliding_window[0] < t - 3000:
+            self.sliding_window.popleft()
+        
+        return len(self.sliding_window)
+
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
