169b3970dff283e6a4a3e2c23a338a56282aa9e9
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-29 17:38:35 +0000

    Time: 127 ms (91.01%), Space: 31.4 MB (98.36%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         # good node == root -> x there are no nodes with a value greater than X
@@ -11,6 +12,22 @@ class Solution:
         #       1      4
         # here 3<=     here 4<=
         # keep updating maximum.. and curr val >= maximum count += 1 and reset maximum
+
+        # bfs
+        res = 0
+        queue = deque([(root, float('-inf'))])
+        while queue:
+            node, maximum = queue.popleft()
+            if node.val >= maximum:
+                res += 1
+                maximum = node.val
+            if node.left:
+                queue.append((node.left, maximum))
+            if node.right:
+                queue.append((node.right, maximum))
+        return res
+
+
         def dfs(node, maximum):
             nonlocal count
             if not node:
