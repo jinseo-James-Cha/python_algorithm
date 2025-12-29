47335cd82128a5c076c5389aa7b1f687acdf6273
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-29 18:37:10 +0000

    Time: 9 ms (45.48%), Space: 17.8 MB (94.86%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -7,6 +7,27 @@
 from collections import defaultdict
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        """
+        this is quite difficult for me, so I want to understand properly.
+
+        I am using DFS and Prefix-Sum algorithms
+
+        if not node return 
+
+        curr_sum is the prefix sum
+        curr_sum + curr_node.val ==> curr_prefix_sum == targetSum -> found one path -> count += 1
+        
+        if curr_prefix_sum - targetSum is in hashmap means there is sub path is matching..
+        like prefix[j] - prefix[i] => i~j sum,,
+        ts = prefix[j] - prefix[i]
+        prefix[i] = prefix[j] - ts
+
+        and then
+        register its curr_prefix_sum into hashmap
+        
+        and traversal as preorder, parent -> left -> right
+        after children traversal, remove the path => h[curr_sum] -= 1
+        """
         def preorder(node, curr_sum):
             nonlocal count
             if not node:
