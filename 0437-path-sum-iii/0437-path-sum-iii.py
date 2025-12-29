bfb46779e07a78c2f8b7987b5ccea9090535d4ed
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2025-12-29 22:48:57 +0000

    Time: 3 ms (87.71%), Space: 17.8 MB (94.34%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -7,49 +7,28 @@
 from collections import defaultdict
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        """
-        this is quite difficult for me, so I want to understand properly.
-
-        I am using DFS and Prefix-Sum algorithms
-
-        if not node return 
-
-        curr_sum is the prefix sum
-        curr_sum + curr_node.val ==> curr_prefix_sum == targetSum -> found one path -> count += 1
-        
-        if curr_prefix_sum - targetSum is in hashmap means there is sub path is matching..
-        like prefix[j] - prefix[i] => i~j sum,,
-        ts = prefix[j] - prefix[i]
-        prefix[i] = prefix[j] - ts
-
-        and then
-        register its curr_prefix_sum into hashmap
-        
-        and traversal as preorder, parent -> left -> right
-        after children traversal, remove the path => h[curr_sum] -= 1
-        """
-        def preorder(node, curr_sum):
-            nonlocal count
+        # dfs + prefix sum with the path sum in hashmap + count
+        def dfs(node, prefix_sum):
+            nonlocal path_count
             if not node:
                 return
             
-            curr_sum += node.val
-            if curr_sum == ts:
-                count += 1
+            total_sum = prefix_sum + node.val
+            if total_sum == ts:
+                path_count += 1
+            path_count += hashmap[total_sum - ts]
             
-            count += h[curr_sum - ts]
+            hashmap[total_sum] += 1
+            dfs(node.left, total_sum)
+            dfs(node.right, total_sum)
 
-            h[curr_sum] += 1
-            preorder(node.left, curr_sum)
-            preorder(node.right, curr_sum)
+            # take back the node.val when it is done
+            hashmap[total_sum] -= 1
 
-            h[curr_sum] -= 1
 
-        count = 0
         ts = targetSum
-        h = defaultdict(int)
-        preorder(root, 0)
-        return count
+        hashmap = defaultdict(int)
+        path_count = 0
+        dfs(root, 0)
+        return path_count
 
-
-        
