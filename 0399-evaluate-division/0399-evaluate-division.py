734c6a4b3d5d5ac439933e143515f701cb38a2c8
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-01 22:27:12 +0000

    Time: 3 ms (7.78%), Space: 17.6 MB (82.71%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,30 +1,47 @@
 from collections import defaultdict
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        # equations[i] = Ai, Bi
+        # values[i] = VALi
+        # A / B = val
+
+        # queries[j] = Cj, Dj
+        # Cj / Dj  = ?
+        # or -1
+
+        # a  b = 2.0 => b a => 1 / 2.0
+        # b  c = 3.0 => c b => 1/ 3.0
+
+
+        # a c => a -> b -> c = 2.0 * 3.0
+        # b a => b -> a = 1/ 2.0
+        def dfs(src, dest, curr, visited):
+            visited.add(src)
+            ret = -1.0
+            neighbors = graph[src]
+            if dest in neighbors:
+                ret = curr * neighbors[dest]
+            else:
+                for neighbor, value in neighbors.items():
+                    if neighbor in visited:
+                        continue
+                    ret = dfs(neighbor, dest, curr * value, visited)
+                    if ret != -1.0:
+                        break
+            visited.remove(src)
+            return ret
+
         graph = defaultdict(dict)
-        for i in range(len(equations)):
-            a, b = equations[i]
-            val = values[i]
-
-            graph[a][b] = val
-            graph[b][a] = 1.0 / val
-
-        def dfs(source, target ,visited):
-            if source not in graph or target not in graph:
-                return -1.0
-            
-            if source == target:
-                return 1.0
-            
-            visited.add(source)
-            for neighbor, weight in graph[source].items():
-                if neighbor not in visited:
-                    result = dfs(neighbor, target, visited)
-                    if result != -1.0:
-                        return weight * result
-            return -1.0
+        for i, (a, b) in enumerate(equations):
+            graph[a][b] = values[i]
+            graph[b][a] = 1 / values[i]
         
         res = []
-        for a, b in queries:
-            res.append(dfs(a, b, set()))
+        for c, d in queries:
+            if c not in graph or d not in graph:
+                res.append(-1.0)
+            elif c == d:
+                res.append(1.0)
+            else:
+                res.append(dfs(c, d, 1.0, set()))
         return res
