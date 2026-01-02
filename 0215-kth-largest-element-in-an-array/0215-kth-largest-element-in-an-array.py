f337edf0a42bbc31a0a9833808e78e61246dfdef
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-02 14:14:44 +0000

    Time: 73 ms (66.85%), Space: 28.8 MB (28.13%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,97 +1,25 @@
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # count sort
-        min_val = min(nums)
-        max_val = max(nums)
-        count = [0] * (max_val - min_val + 1)
+        """
+        kth largest -> max heap and take out 0?
 
+        1 2 3 4 5
+        1 2 3
+          2 3 4
+            3 4 5 
+            pop()
+        3 7 8 1 2
+        3 7 8
+          3 7 8
+        """
+        pq = []
         for num in nums:
-            count[num - min_val] += 1
-        
-        remain = k
-        for num in range(len(count) - 1, -1, -1):
-            remain -= count[num]
-            if remain <= 0:
-                return num + min_val
-        return -1
+            if len(pq) < k:
+                heapq.heappush(pq, num)
+            elif pq[0] < num:
+                heapq.heappop(pq)
+                heapq.heappush(pq, num)
+        return heapq.heappop(pq)
+        # [3,2,1,5,6,4]
 
-
-        n = len(nums)
-        heapq.heapify(nums)
-
-        for _ in range(n - k):
-            heapq.heappop(nums)
-        
-        return heapq.heappop(nums)
-
-
-
-
-
-
-
-
-        # counting sort
-        min_num = min(nums)
-        max_num = max(nums)
-        counts = [0] * (max_num - min_num + 1)
-
-        for num in nums:
-            count_idx = num - min_num
-            counts[count_idx] += 1
-        
-        remain = k
-        for num in range(len(counts) -1, -1, -1):
-            remain -= counts[num]
-            if remain <= 0:
-                return num + min_num
-        
-        return -1
-
-
-
-
-        heap = []
-        for num in nums:
-            heapq.heappush(heap, num)
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return heap[0]
-
-        # using max heap
-        heapq.heapify(nums)
-        i = len(nums)
-        while i > k:
-            heapq.heappop(nums)
-            i -= 1
-        
-        return nums[0]
-        
-        # min heap
-        def min_heapify(heap_size, index):
-            left = index * 2 + 1
-            right = index * 2 + 2
-            largest = index
-
-            if left < heap_size and nums[left] < nums[largest]:
-                largest = left
-            
-            if right < heap_size and nums[right] < nums[largest]:
-                largest = right
-            
-            if largest != index:
-                nums[largest], nums[index] = nums[index], nums[largest]
-                min_heapify(heap_size, largest)
-            
-        # initial heapify
-        for i in range(len(nums)//2 -1, -1, -1):
-            min_heapify(len(nums), i)
-        
-        # swap first and last and heapify
-        for i in range(len(nums)-1, 0, -1):
-            nums[i], nums[0] = nums[0], nums[i]
-            min_heapify(i, 0)
-        
-        return nums[k-1]
-    
