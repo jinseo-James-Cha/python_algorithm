f9c04134259057e8ae0f2b7d2abdeb2434c6d708
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-03 18:12:02 +0000

    Time: 0 ms (100%), Space: 17.1 MB (99.89%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,30 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # all possible ? -> backtrack
+        # all combinations .. and range smaller... -> backtracking
+        letter_combinations = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
 
-        telephone_buttons = {}
-        telephone_buttons['1'] = []
-        telephone_buttons['2'] = ['a', 'b', 'c']
-        telephone_buttons['3'] = ['d', 'e', 'f']
-        telephone_buttons['4'] = ['g', 'h', 'i']
-        telephone_buttons['5'] = ['j', 'k', 'l']
-        telephone_buttons['6'] = ['m', 'n', 'o']
-        telephone_buttons['7'] = ['p', 'q', 'r', 's']
-        telephone_buttons['8'] = ['t', 'u', 'v']
-        telephone_buttons['9'] = ['w', 'x', 'y', 'z']
-
-        def backtrack(i, curr):
-            if len(curr) == len(digits):
-                res.append("".join(curr))
+        n = len(digits)
+        def backtrack(curr_combi, start):
+            if len(curr_combi) == n:
+                res.append("".join(curr_combi))
                 return
             
-            for letter in telephone_buttons[digits[i]]:
-                curr.append(letter)
-                backtrack(i+1, curr)
-                curr.pop()
+            for letter in letter_combinations[digits[start]]:
+                curr_combi.append(letter)
+                backtrack(curr_combi, start + 1)
+                curr_combi.pop()
         
         res = []
-        backtrack(0, [])
+        backtrack([], 0)
         return res
-            
-            
