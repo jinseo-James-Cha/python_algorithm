2ee51b25f56982c0f60654617e795ffc8759e446
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-03 00:38:51 +0000

    Time: 0 ms (100%), Space: 17.4 MB (84.88%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,29 +1,13 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        # binary search
         left = 0
-        right = len(nums) - 1 # should be one of index
-        while left < right: # left == right is the answer
-            mid = (left + right) // 2
-            if nums[mid] < nums[mid+1]:
+        right = len(nums) - 1
+        while left < right:
+            mid = (right + left) // 2
+            if nums[mid] <= nums[mid+1]:
                 left = mid + 1
             else:
                 right = mid
+
         return left
-
-
-
-        # recursive
-        # peek => greater than neighbors
-        # find a peek and return its "index"
-        # if multiple, return any of them
-        # O(log n) -> binary search
-        return self.binarySearch(nums, 0, len(nums) - 1)
-    
-    def binarySearch(self, nums, left, right):
-        if left == right:
-            return left
-        mid = (left + right) // 2
-        if nums[mid] > nums[mid+1]:
-            return self.binarySearch(nums, left, mid)
-        return self.binarySearch(nums, mid+1, right)
+            
