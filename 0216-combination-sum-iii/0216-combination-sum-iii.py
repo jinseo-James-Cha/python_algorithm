9558a584f575df3820f0edc57ec8754349572a70
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-03 18:18:08 +0000

    Time: 0 ms (100%), Space: 17.3 MB (91.84%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,18 +1,19 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        # backtrack
-        res = []
+        # 1 ~ 9
+        # only once in a combination
 
-        def backtrack(combination, start):
-            if len(combination) == k:
-                if sum(combination) == n:
-                    res.append(combination[:])
+        def backtrack(curr_combi, curr_num):
+            if sum(curr_combi) == n and len(curr_combi) == k:
+                combinations.append(curr_combi[:])
+            elif sum(curr_combi) > n or len(curr_combi) > k:
                 return
             
-            for i in range(start, 10):
-                combination.append(i)
-                backtrack(combination, i + 1)
-                combination.pop()
-        
+            for num in range(curr_num, 10):
+                curr_combi.append(num)
+                backtrack(curr_combi, num+1)
+                curr_combi.pop()
+
+        combinations = []
         backtrack([], 1)
-        return res
+        return combinations
