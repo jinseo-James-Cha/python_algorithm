2ca7004acf7884f850466691d5b96e2f5d70420f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-05 16:32:09 +0000

    Time: 107 ms (5.03%), Space: 18.1 MB (83.6%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,21 +1,12 @@
-# return len(ans) = n + 1
-# i 0 <= i <= n
-# ans[i] is the number of 1's in the binary representation of i.
-
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def to_binary(n):
-            result = ''
-            while 0 < n:
-                result = str(n % 2) + result
-                n //= 2
-            return result or '0'
-
-        return [to_binary(a).count('1') for a in range(n+1)]
-    
-
-    
-
-    
-
-        
+        ans = [0] * (n+1)
+        for i in range(1, n+1):
+            # convert i -> binary and count 1 and save in ans[i]
+            curr = i
+            num_of_one = 0
+            while curr >= 1:
+                num_of_one += int(curr % 2 != 0)
+                curr = curr // 2
+            ans[i] = num_of_one
+        return ans
