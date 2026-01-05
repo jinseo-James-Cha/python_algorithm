87a989ea7a86ba34d41ddf89bb7b8fe1930130e9
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-05 12:55:11 +0000

    Time: 722 ms (5.05%), Space: 200.1 MB (16.11%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,54 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        # maximum profit
-        # transaction fee = -2
-        
-        # state
-        # days -> i
-        # action -> buy or skip / sell or skip
+        """
+        return Maximum profit
+        unlimited transaction with fee.
+        state = which day, holding stock?
 
-        # bottom up optimize
-        hold = -prices[0]
-        free = 0
-        for i in range(1, len(prices)):
-            tmp = hold
-            hold = max(hold, free - prices[i])
-            free = max(free, tmp + prices[i] - fee)
-        
-        return free
-
-
-
-
-
-        # bottom up
-        dp_hold = [0] * len(prices)
-        dp_free = [0] * len(prices)
-
-        dp_hold[0] = -prices[0]
-        for i in range(1, len(prices)):
-            dp_hold[i] = max(dp_hold[i-1], dp_free[i-1] - prices[i]) # 어제꺼 홀딩 or 지금 매수
-            dp_free[i] = max(dp_free[i-1], dp_hold[i-1] + prices[i] - fee) # 어제꺼 그대로, 매도
-        return dp_free[-1]
-
-
-
-        # top down
-        def dp(i, holding):
+        t1: sell price - buy price - fee + t2: ...
+        """
+        def dp(i, is_holding):
             if i == len(prices):
                 return 0
-    
-            if (i, holding) not in memo:
-                doNothing = dp(i+1, holding)
-                doSomething = 0
-                # sell already hold, 
-                if holding:
-                    doSomething = (prices[i] - fee) + dp(i+1, 0)
-                else: # buying stock
-                    doSomething = -prices[i] + dp(i+1, 1)
+            
+            if (i, is_holding) not in memo:
+                # skip - do nothing
+                do_nothing = dp(i+1, is_holding)
+                do_something = 0
+                
+                # time to sell
+                if is_holding:
+                    do_something = prices[i] + dp(i+1, False) - fee
+                else:# buy
+                    do_something = -prices[i] + dp(i+1, True)
+                
+                memo[(i, is_holding)] = max(do_nothing, do_something)
 
-                memo[(i, holding)] = max(doNothing, doSomething)
-            return memo[(i, holding)]
+            return memo[(i, is_holding)]
         
         memo = {}
-        return dp(0, 0)
+        return dp(0, False)
