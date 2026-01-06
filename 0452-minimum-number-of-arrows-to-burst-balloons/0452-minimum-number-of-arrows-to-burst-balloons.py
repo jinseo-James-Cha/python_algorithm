da704e1b6d7a4169fc0c3951a33795a582b0c5be
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-06 18:02:18 +0000

    Time: 55 ms (98.45%), Space: 53.4 MB (6.67%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,17 +1,54 @@
-from collections import defaultdict
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        if not points:
-            return 0
+        #  B    B  B
+        #  B  B    B 
+        #  A  A A  A => return 4?
 
-        # start, end 
-        # x shot burst -> xstart <= x <= xend
-        
-        points.sort(key=lambda x : x[1])
+        points.sort(key=lambda x:x[1])
+
         arrows = 1
-        first_end = points[0][1]
-        for x_start, x_end in points:
-            if first_end < x_start:
+        curr_end = points[0][1]
+        for start, end in points:
+            if start > curr_end:
                 arrows += 1
-                first_end = x_end
+                curr_end = end
         return arrows
+
+
+
+        """
+        [[10,16],[2,8],[1,6],[7,12]]
+
+                            - - - - - - - -
+                    - - - - - - - -
+          - - - - - - -
+        - - - - - - 
+        1 2       6 7 8    10    12       16
+    curr1 2       0 1      2
+    res           1               2
+                   - -
+                 - -
+          -      -
+        - -
+        1 *2     3 *4 5
+    curr-1-2->0 -1
+    res       1
+        """
+        # events = []
+        # for start, end points:
+        #     events.append((start, -1))
+        #     events.append((end, 1))
+        
+        # events.sort()
+
+        # minimum_number_of_arrows = 0
+        # curr_score = 0
+        # idx = 0
+        # while idx < len(events)
+        #     x, score = events[i]
+        #     if score == 1 and curr_score < 0: # which is closing, and need to shot
+        #         minimum_number_of_arrows += 1
+        #         curr_score = 0
+        #     else:
+        #         curr_score += score
+        # return minimum_number_of_arrows
