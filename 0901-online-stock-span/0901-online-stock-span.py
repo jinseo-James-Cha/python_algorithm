c38bfdf143ee30afed5c4622d6dca474ad830039
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-07 16:22:16 +0000

    Time: 9478 ms (5.01%), Space: 24.4 MB (7.09%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -0,0 +1,42 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.prev_prices = []
+
+    def next(self, price: int) -> int:
+        span = 1
+        for prev_price in reversed(self.prev_prices):
+            if price >= prev_price:
+                span += 1
+            else:
+                break
+        self.prev_prices.append(price)
+        return span
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
+"""
+return the span of that stock's price for the current day.
+Span => stock's price in one day is the maximum number of consecutive days
+starting from that day and going Backward for the stock price was less than or equal to
+
+7 2    1 2
+  here +1+1
+0 1    2 3 -> or 3 - 1 ?
+the price of the stock today is 2 -> the span of today is 4
+
+7 34 1 2
+if price is 8 today
+the span -> 3
+
+[[100], [80], [60], [70], [60], [75], [85]]
+   1      1    1     2      1    4     6
+
+stack= [100]
+span = 1
+stack[-1] 100 > curr 80 return span
+100 8
+"""
