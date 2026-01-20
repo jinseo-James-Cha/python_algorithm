83707b39fce83b841e559905b47f98952dedd238
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-20 15:54:20 +0000

    Time: 0 ms (100%), Space: 19.3 MB (15.03%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,10 +1,21 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
+        # abab -> abab + abab 
+        # s -> s + s
+        # s+s has all rotations of s
+        # like baba abab baba abab
+        # if s+s[1:-1] has s -> it is answer
+        # first and last removed for excepting the first start of s and the second end of s
+        # a bababa b
+        # s + s but removed first and last one
+        double_s = s + s
+        if s in double_s[1:-1]:
+            return True
+        return False
 
 
 
 
-        
         # the length should be a divisor
         n = len(s)
         for i in range(1, n // 2 + 1):
