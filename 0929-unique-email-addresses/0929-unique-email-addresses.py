7d1e5af4b43872f368b12c44786ec9902ff8d6c1
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-21 18:06:09 +0000

    Time: 0 ms (100%), Space: 19.3 MB (20.53%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -0,0 +1,39 @@
+# local name and a domain name
+# localname@domain.name
+# . or +
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        def getForwardEmail(email):
+            email_list = email.split('@')
+            local_name, domain_name = email_list[0], email_list[1]
+            
+            # we do think only local name
+            # 1. remove after @
+            local_name_list = local_name.split('+')
+            local_name = local_name_list[0]
+            local_name = local_name.replace('.', '')
+            
+            return local_name + '@' +domain_name
+        
+        receive_emails = set()
+        for email in emails:
+            forward_email = getForwardEmail(email)
+            receive_emails.add(forward_email)
+        
+        return len(receive_emails)
+        # split by @
+        # remove after +
+        # replace . to ""
+        # save localname + @ + domain name into set -> no duplication
+        s = set()
+        for email in emails:
+            localName, domainName = email.split('@')
+            localName = localName.replace('.', "")
+            if '+' in localName:
+                localName = localName[:localName.index('+')]
+
+            s.add(localName + '@' + domainName)
+        return len(s)
+        
+        
