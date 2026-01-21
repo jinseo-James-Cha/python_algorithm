2564b6737992189c082b6fe1334d87457ff805f3
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-21 16:32:56 +0000

    Time: 122 ms (7.06%), Space: 31.7 MB (6.01%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        seen = set()
+
+        def backtrack(combination, used_index):
+            if combination:
+                if tuple(combination) not in seen:
+                    seen.add(tuple(combination))
+
+            for i, num in enumerate(tiles):
+                if i not in used_index:
+                    used_index.add(i)
+                    combination.append(num)
+
+                    backtrack(combination, used_index)                    
+
+                    used_index.remove(i)
+                    combination.pop()
+    
+        backtrack([], set())
+        return len(seen)
