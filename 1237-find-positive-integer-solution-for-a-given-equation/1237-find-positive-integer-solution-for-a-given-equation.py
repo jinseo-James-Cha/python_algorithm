a92ab797108ddd76b147db75d712f2247332e2e1
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-22 16:26:32 +0000

    Time: 40 ms (82.27%), Space: 19.3 MB (12.5%) - LeetHub

--- 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.py
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.py
@@ -11,6 +11,26 @@
 
 class Solution:
     def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+        # optimize - two pointers
+        x, y = 1, z
+        pairs = []
+        
+        while x<=z and y>0:
+            cf = customfunction.f(x,y)
+            if cf==z:
+                pairs.append([x,y])
+                x, y = x+1, y-1
+            elif cf > z:
+                y -= 1
+            else:
+                x += 1
+        return pairs
+
+
+
+
+        
+        # brute force
         res = []
         for x in range(1, 1001):
             for y in range(1, 1001):
