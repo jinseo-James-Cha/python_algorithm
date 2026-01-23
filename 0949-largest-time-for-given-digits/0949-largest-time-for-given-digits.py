f9cb32522289ad3e7bf5bc2d1dabcc2826bff752
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-23 17:11:37 +0000

    Time: 3 ms (41.87%), Space: 19.7 MB (6.06%) - LeetHub

---
+++ 0949-largest-time-for-given-digits/0949-largest-time-for-given-digits.py
@@ -0,0 +1,28 @@
+class Solution:
+    def largestTimeFromDigits(self, arr: List[int]) -> str:
+        def backtrack(curr, used_idx):
+            if len(curr) == 4:
+                # validate time
+                hh, mm = curr[0]+curr[1], curr[2]+curr[3]
+                if 0 <= int(hh) <= 23 and 0 <= int(mm) <= 59:
+                    combinations.append("".join(curr[:]))
+            
+            for i in range(len(arr)):
+                if i not in used_idx:
+                    curr.append(str(arr[i]))
+                    used_idx.add(i)
+                    
+                    backtrack(curr, used_idx)
+                    
+                    curr.pop()
+                    used_idx.remove(i)
+                    
+        
+        combinations = []
+        backtrack([], set())
+        if not combinations:
+            return ""
+        
+        combinations.sort(reverse=True)
+        hh,mm = combinations[0][:2], combinations[0][2:]
+        return hh + ":" + mm
