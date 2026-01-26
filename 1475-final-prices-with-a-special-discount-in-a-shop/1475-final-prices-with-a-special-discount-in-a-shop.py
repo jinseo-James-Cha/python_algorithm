a12de976f74abf2c11f2c0693a780d061ec0b093
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-26 21:17:56 +0000

    Time: 3 ms (50.34%), Space: 19.5 MB (12.13%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,63 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        # special discount
+        # if you buy "i"th item, discount prices[j] ==> minimum index j > i and prices[j] <= prices[i]
+
+
+
+        """
+        8 4 6 2 3
+        4 2 4 2 3
+
+
+        stack [8]
+        8 > 4
+        pop
+        answer[4]
+        
+        stack [4]
+        4 < 6
+        push
+        
+        stack [4, 6]
+        6 > 2
+        pop
+        [4]
+        4 > 2
+        pop
+        [4, 2]
+        stack[4, 2, 4]
+        push 2
+        
+        stack[2]
+        stack[2,3]
+
+        [4,2,4] + [2,3]
+        [1, 2, 3, 4, 5]
+
+        8 4 6 5
+        [4]
+
+        [4, 6]
+        5
+
+        [1 4] 
+        monotonic stack ? no..?
+
+
+
+        8 4 6 2 3
+        4 2 4 2 3
+
+        """
+        res = [-1] * len(prices)
+        for i in range(len(prices)):
+            flag = False
+            for j in range(i+1, len(prices)):
+                if prices[i] >= prices[j]:
+                    res[i] = prices[i]-prices[j]
+                    flag = True
+                    break
+            if not flag:
+                res[i] = prices[i]
+        return res
