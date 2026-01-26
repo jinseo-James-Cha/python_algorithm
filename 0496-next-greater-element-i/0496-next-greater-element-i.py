7c5dd08e27b80bdb6f1b1fe7d38a82e7aceb0b4a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-26 23:27:40 +0000

    Time: 0 ms (100%), Space: 19.4 MB (27.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,42 +1,13 @@
-# v2 : can I optimize to O(nums1.length + nums2.length) 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # nums2 make hashmap with all possible sets by stack
         hashmap = {}
         stack = []
         for n2 in nums2:
             while stack and n2 > stack[-1]:
                 hashmap[stack.pop()] = n2
-            stack.append(n2)       
+            stack.append(n2)
         
-        return [hashmap.get(i, -1) for i in nums1]
-                
-        
-
-
-# class Solution:
-#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-#         # all unique values
-#         # all nums1 -> in nums2
-#         # what is the next greater value..
-
-#         # 4 -> 1, 3, 4 and 2 -> -1
-
-#         # 2 4 3 1
-        
-#         # brute force O(N*M)
-#         ans = []
-#         for i in range(len(nums1)):
-#             found = False
-#             for j in range(len(nums2)):
-#                 if found and nums1[i] < nums2[j]:
-#                     ans.append(nums2[j])
-#                     break
-#                 if nums1[i] == nums2[j]:
-#                     found = True
-            
-#             # if it didn't add
-#             if len(ans) == i:
-#                 ans.append(-1)
-#         return ans
-            
+        res = []
+        for n1 in nums1:
+            res.append(hashmap.get(n1, -1))
+        return res
