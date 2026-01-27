e8627f9fcc3041278d6dd2c951b49e53a4ccbd57
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-27 16:17:46 +0000

    Time: 3 ms (12.03%), Space: 19.6 MB (9.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,6 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
+        red, white, blue = 0, 0, 0
+        for num in nums:
+            if num == 0:
+                red += 1
+            elif num == 1:
+                white += 1
+            else:
+                blue += 1
+        
+        nums[0:red] = [0] * red
+        nums[red:red+white] = [1] * white
+        nums[red+white:] = [2] * blue
+
+
         # counting sort
         count = [0, 0, 0]
         for num in nums:
@@ -15,37 +30,33 @@ class Solution:
                 curr_index += 1
 
 
-
-
-
         # order by red while blue
         #           0   1     2
         
         # one pass - two(three pointers) pointers
-        # left = curr = 0
-        # right = len(nums) - 1
-        # while curr <= right:
-        #     if nums[curr] == 2:
-        #         nums[curr], nums[right] = nums[right], nums[curr]
-        #         right -= 1
-        #     elif nums[curr] == 0:
-        #         nums[left], nums[curr] = nums[curr], nums[left]
-        #         left += 1
-        #         curr += 1
-        #     else:
-        #         curr += 1    
+        left, curr, right = 0, 0, len(nums) - 1
+        while curr <= right:
+            if nums[curr] == 2:
+                nums[curr], nums[right] = nums[right], nums[curr]
+                right -= 1
+            elif nums[curr] == 0:
+                nums[left], nums[curr] = nums[curr], nums[left]
+                left += 1
+                curr += 1
+            else:
+                curr += 1    
 
 
 
         
         
-        # # just sort by ascending order
-        # # selection sort
-        # for i in range(len(nums)):
-        #     min_index = i
-        #     for j in range(i+1, len(nums)):
-        #         if nums[j] < nums[min_index]:
-        #             min_index = j
-        #     nums[i], nums[min_index] = nums[min_index], nums[i]
+        # just sort by ascending order
+        # selection sort
+        for i in range(len(nums)):
+            min_index = i
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            nums[i], nums[min_index] = nums[min_index], nums[i]
             
         
