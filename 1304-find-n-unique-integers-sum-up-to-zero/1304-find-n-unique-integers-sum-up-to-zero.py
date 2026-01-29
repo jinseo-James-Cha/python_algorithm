7d585713a22f632fc3a1f16860e11ef1bd504099
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-29 14:40:30 +0000

    Time: 7 ms (0.95%), Space: 23.8 MB (17.11%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
@@ -7,7 +7,19 @@ class Solution:
         # 1  2   3       4          5
         # 0 -1,1 -1 0 1  -2 -1 1 2  -2 -1 0 1 2
 
+        # bottom up
+        if n == 1:
+            return [0]
+
+        dp = [[] for _ in range(n+1)]
+        dp[1] = [0]
+        for i in range(2, n+1):
+            dp[i] = dp[i-2] + [i//2] + [-(i//2)]
+        return dp[n]
+
         
+
+        # top down
         def dp(i):
             if i == 0:
                 return []
