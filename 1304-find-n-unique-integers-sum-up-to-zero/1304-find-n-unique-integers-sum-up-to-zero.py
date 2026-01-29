efea56ade787cae9b24330120e1f22554546fe39
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-01-29 14:35:33 +0000

    Time: 3 ms (6.07%), Space: 26.2 MB (17.11%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        # len(array) == n with unique integer? sum(arr) == 0
+        
+
+        # n 
+        # 1  2   3       4          5
+        # 0 -1,1 -1 0 1  -2 -1 1 2  -2 -1 0 1 2
+
+        
+        def dp(i):
+            if i == 0:
+                return []
+            elif i == 1:
+                return [0]
+            
+            if i not in memo:
+                curr_val = i // 2
+                memo[i] = dp(i-2) +  [-curr_val, curr_val]
+            
+            return memo[i]
+
+        memo = {}
+        return dp(n)
+        
+
+
