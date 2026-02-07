e04e59ecbf8c0a0bce679c5a062760d5865f31c6
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-07 00:20:55 +0000

    Time: 31 ms (68.49%), Space: 22 MB (23.78%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,34 +1,18 @@
-from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = 0
+        # subarray sum = start - end
+        # end = start - subarray sum
+        # curr_total_sum = prev sum - k
         
-        prefix_sum_map = defaultdict(int)
-        prefix_sum_map[0] = 1
-        curr_prefix_sum = 0
+        total = 0
+
+        subarraySum_hashmap = { 0:1 }
+        curr_total_sum = 0
+
         for num in nums:
-            curr_prefix_sum += num
+            curr_total_sum += num
+            if curr_total_sum - k in subarraySum_hashmap:
+                total += subarraySum_hashmap[curr_total_sum - k]
             
-            if curr_prefix_sum - k in prefix_sum_map:
-                count += prefix_sum_map[curr_prefix_sum - k]
-
-            prefix_sum_map[curr_prefix_sum] += 1
-        return count
-
-
-# using prefix_sum algorithm 
-# O(n^2) 
-# 1 <= nums.length <= 2 * 10^4 -> TLE
-# class Solution:
-#     def subarraySum(self, nums: List[int], k: int) -> int:
-#         res = 0
-        
-#         prefix_sum = [0] # for just prefix index itself
-#         for i in range(len(nums)):
-#             prefix_sum.append(prefix_sum[-1] + nums[i])
-        
-#         for j in range(1, len(nums) + 1):
-#             for i in range(1, j + 1):
-#                 if prefix_sum[j] - prefix_sum[i - 1] == k:
-#                     res += 1
-#         return res            
+            subarraySum_hashmap[curr_total_sum] = subarraySum_hashmap.get(curr_total_sum, 0) + 1
+        return total
