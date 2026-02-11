b5e78eb3af7ebc503052fc74a5297dd9dd88fe0f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-11 11:11:27 +0000

    Time: 3 ms (85.52%), Space: 22.2 MB (9.53%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,12 +1,6 @@
-import heapq
-
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # minimum number of conference room ..
-        # -> how many meetings are intersection
-        # -> sweep line algorithm
-
-
+        # sweep line
         events = []
         for start, end in intervals:
             events.append((start, 1))
@@ -14,60 +8,28 @@ class Solution:
         
         events.sort()
 
-        max_room_used = 0
+        res = 0
         curr_room = 0
-        for time, score in events:
+        for point, score in events:
             curr_room += score
-            max_room_used = max(max_room_used, curr_room)
-        return max_room_used
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # priority queue
-        intervals.sort(key=lambda x: x[0])
-
-        rooms = []
-        heapq.heappush(rooms, intervals[0][1]) # add first's end time
+            res = max(res, curr_room)
         
-        for start, end in intervals[1:]:
-            if start >= rooms[0]:
-                heapq.heappop(rooms)
-            
-            heapq.heappush(rooms, end)
-        return len(rooms)
-        
-        
-        
-        
-        
-        # intervals -> sweep line algorithm
-        # define events
-        events = []
-        for start, end in intervals:
-            events.append((start, 1))
-            events.append((end, -1))
-
-        # sort events
-        events.sort(key= lambda x: (x[0], x[1]))
-        
+        return res
 
 
-        max_rooms, curr_rooms = 0, 0
-        for k, v in events:
-            curr_rooms += v
-            max_rooms = max(max_rooms, curr_rooms)
-        return max_rooms
+
+
+        # intervals.sort()
+        # res = 1
+        # curr_room = 1
+        # prev_end = intervals[0][1]
+
+        # for i in range(1, len(intervals)):
+        #     if intervals[i][0] < prev_end:
+        #         curr_room += 1
+        #         res = max(res, curr_room)
+                
+        #     else:
+        #         curr_room -= 1
+        #     prev_end = min(prev_end, intervals[i][1])
+        # return res
