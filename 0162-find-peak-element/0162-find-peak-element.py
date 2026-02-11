44442323ccf5914f98f30090da26cda6d612abc8
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-11 10:56:15 +0000

    Time: 0 ms (100%), Space: 19.4 MB (27.77%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,6 +1,32 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         """
+        log n solution
+        binary search
+
+        1 2 1 3 5 6 4
+        L
+                    R
+              M
+        """
+        n = len(nums)
+        left = 0
+        right = n - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[mid+1]:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+        
+
+
+
+
+        """
         peak element
 
         1 2 1 3 5 6 4
