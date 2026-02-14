eddb79a8bbb3796c42ad4f54816c999cbc5a8493
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-14 17:35:06 +0000

    Time: 7 ms (82.2%), Space: 19.7 MB (58.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,49 @@
 # retry with two pointers inward traversal strategy
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        """
+        A man, a plan.... Panama
+        L
+                               R
+        if it is the same in case insensitive-> left += 1 right +=1
+        if left is not alpha-> left +=1
+        if right is not alpha->right -=1
+        and then compare
+                
+        """
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            
+            while 0 < right and not s[right].isalnum():
+                right -= 1
+            
+
+            if left < right and s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         left = 0
         right = len(s) - 1
         while left < right:
