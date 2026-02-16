ffadd3d7d86b5f507d58aca2dcdd6978f30a8f44
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-16 11:37:09 +0000

    Time: 7 ms (59.92%), Space: 20.4 MB (26.39%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,5 +1,62 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        """
+        + = right
+        - = left
+
+        return asteroids after collisions.
+        two asterids meet smaller one explode
+        a1 > a2 -> remove a2
+        a1 == a2 -> remove a1, a2 
+        
+        -> -> <- inward values are collision
+        5 10 -5
+           10 and -5 meet and -5 gone because absolute value 10 > 5
+        
+        -> <-
+        8 -8
+        removed the both
+
+        -> -> <-
+        10 2 -5
+           abs(2) < abs(5)  2 gone and 10 > 5 5 gone
+
+        10 2 
+            -5
+        10 and -5
+        """
+
+        # 3 5 -6 2 -1 4
+        # stack [3, 5]
+        # 5 < 6
+        # pop()
+        # 3 < 6
+        # pop()
+        # stack = []
+        # stack = [-6]
+
+        # 6 < 2
+        stack = []
+
+        for a in asteroids:
+            alive = True
+            
+            # collision
+            while alive and stack and stack[-1] > 0 and a < 0:
+                if stack[-1] > -a:
+                    alive = False
+                elif stack[-1] == -a:
+                    stack.pop()
+                    alive = False
+                else:
+                    stack.pop()
+            
+            if alive:
+                stack.append(a)
+
+        return stack
+                
+
         # relative positing in space..
         # abs value -> size, sign(+right -left )
 
