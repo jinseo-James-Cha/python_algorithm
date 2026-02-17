bdf57df49488e9ae0c090c077d2d5c766ce0de51
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-17 10:30:22 +0000

    Time: 0 ms (100%), Space: 19.5 MB (13.49%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -6,8 +6,20 @@ class Solution:
         # 6 leds on bottom, minutes, 0 ~ 59(not 1 - 60)
         # 32 16 8 4 2 1
 
-        res = []
+        # question
+        # top light on + bottom light on == turnedOn
+        # calculate with binary number of time and convert to time format
+        
+        # key points
+        # 1. int -> binary
+        # bin(integer) -> "0b"+binary number for the integer
+        # bin(1) -> 0b1
+        # bin(-5) -> -0b101
+        # 2. int -> time str formating
+        # f"{h}:{m:02d}"
+        # {m:02d} m formating with 2digits, if not fill with 0
 
+        res = []
         for h in range(12):
             for m in range(60):
                 if bin(h).count('1') + bin(m).count('1') == turnedOn:
