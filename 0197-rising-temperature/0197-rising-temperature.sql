a876084224dbda9872c35f7774b397a288c079cc
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-21 21:20:06 +0000

    Time: 378 ms (80.27%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,29 @@
 # Write your MySQL query statement below
-SELECT w1.id
-  FROM Weather w1
-  JOIN Weather w2 on DATEDIFF(w1.recordDate, w2.recordDate) = 1
- WHERE w1.temperature > w2.temperature
+# select id which has warmer temp than yesterday
+# date should compare itself with yesterday temp
+# how can we compare with date-1
+# if cannot have yesterday? -> ignore the dates
+
+WITH PreviousWeatherData AS
+(
+    SELECT 
+        id,
+        recordDate,
+        temperature, 
+        LAG(temperature, 1) OVER (ORDER BY recordDate) AS PreviousTemperature,
+        LAG(recordDate, 1) OVER (ORDER BY recordDate) AS PreviousRecordDate
+    FROM 
+        Weather
+)
+SELECT id
+  FROM PreviousWeatherData
+ WHERE temperature > PreviousTemperature
+   AND DATEDIFF(recordDate, PreviousRecordDate) = 1
 ;
+
+
+-- SELECT w1.id
+--   FROM weather w1
+--   JOIN weather w2 on(DATEDIFF(w1.recordDate, w2.recordDate) = 1)
+--  WHERE w1.temperature > w2.temperature
+-- ;
