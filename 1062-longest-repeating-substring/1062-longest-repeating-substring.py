0f47454dd97f773cb6320a57676144c7307d812a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-23 10:10:10 +0000

    Time: 195 ms (52.45%), Space: 35.6 MB (33.82%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -1,5 +1,20 @@
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
+        # dp
+        length = len(s)
+        dp = [[0] * (length + 1) for _ in range(length + 1)]
+        max_length = 0
+
+        for i in range(1, length + 1):
+            for j in range(i + 1, length + 1):
+                if s[i - 1] == s[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    max_length = max(max_length, dp[i][j])
+        return max_length
+
+
+        # brute force
+        # o(n**3) o of cubed 
         curr_size = len(s) - 1
 
         for size in range(len(s) - 1, 0, -1):
