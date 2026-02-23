e7e4fb61f0f2fea3eb867209de965b1dc61790fb
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-23 12:01:12 +0000

    Time: 262 ms (28.89%), Space: 25 MB (28.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,54 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # '1' => land or '0' => water
+        # return number of islands
+        
+        # count 1 and mark its connected cells -> 1 island
+        # out of bounds => water
+
+        # can I color(change) the value in grid? -> YES
+        # put island id and then dfs to color all land cells with id
+        # id starts from 2 and return id - 1
+
+        def is_within_bounds(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+
+        DIRS = [(1,0), (-1,0), (0,-1), (0,1)]
+        def dfs(row, col, island_id):
+            grid[row][col] = str(island_id)
+
+            for dr, dc, in DIRS:
+                next_row, next_col = row + dr, col + dc
+                if is_within_bounds(next_row, next_col) and grid[next_row][next_col] == '1':
+                    dfs(next_row, next_col, island_id)
+
+        island_id = 2
+        m, n = len(grid), len(grid[0])
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] == '1':
+                    dfs(row, col, island_id)
+                    island_id += 1
+
+        return island_id - 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # char '1' or '0' -> '0' if visited
         m, n = len(grid), len(grid[0])
         res = 0
