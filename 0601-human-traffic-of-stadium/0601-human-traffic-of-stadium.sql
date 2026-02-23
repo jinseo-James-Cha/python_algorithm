6a3551ba863de8a4612105347749773ae545f341
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-02-23 17:05:50 +0000

    Time: 363 ms (54.93%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,29 @@
+# Write your MySQL query statement below
+# visit_date is UNIQUE
+# 3 <= consecutive ids 1,2,3 for id -> people >= 100 for each
+
+SELECT 
+    DISTINCT a.*
+FROM 
+    stadium AS a, stadium AS b, stadium AS c
+WHERE
+     a.people >= 100 AND b.people >= 100 AND c.people >= 100
+AND 
+    (
+       (a.id - b.id = 1 AND b.id - c.id = 1)
+    OR (c.id - b.id = 1 AND b.id - a.id = 1)
+    OR (b.id - a.id = 1 AND a.id - c.id = 1)
+    )
+ORDER BY visit_date
+
+
+# CTE for 100 people
+
+
+
+-- WITH hundred_people AS (
+--     SELECT s.*
+--       FROM Stadium s
+--      WHERE s.people >= 100
+-- )
+
