80fac146a0d09821b9b0ce2d1121d75f44dff4bf
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-02 15:11:59 +0000

    Time: 71 ms (8.64%), Space: 23.6 MB (88.44%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,36 +7,38 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        stack = [root]
+        # iterative version
+        # save parent[child] = parent
+        # save ancestor of all p
+        # loop until q not in ancestor
         parent = {root: None}
+        stack = [root]
         while p not in parent or q not in parent:
-            node = stack.pop() # root
-            if node.left:
-                parent[node.left] = node
-                stack.append(node.left)
-            if node.right:
-                parent[node.right] = node
-                stack.append(node.right)
-
+            curr = stack.pop()
+            if curr.left:
+                parent[curr.left] = curr
+                stack.append(curr.left)
+            
+            if curr.right:
+                parent[curr.right] = curr
+                stack.append(curr.right)
+        
         ancestors = set()
         while p:
             ancestors.add(p)
             p = parent[p]
-            
+        
         while q not in ancestors:
             q = parent[q]
+        
         return q
 
+        """
+        left true
+        right true 
+        i am the LCA
 
-
-
-        # Lowest Common Ancester
-        # me? -> 1
-        # left? -> 1
-        # right? -> 1
-        # if 2 == this is the answer
-
-        # this is not Binary Search Tree, so I don't care the range.
+        """
         def dfs(node, p, q):
             nonlocal res
             if not node:
@@ -51,7 +53,7 @@ class Solution:
             
             if dfs(node.right, p, q):
                 score += 1
-            
+
             if score == 2:
                 res = node
             
@@ -61,31 +63,4 @@ class Solution:
         dfs(root, p, q)
         return res
 
-        # LCA
-        # between p and q as the lowest 
-        # who is the common ancestor for the both p and q.
-        def dfs(root):
-            nonlocal lca
-            if not root:
-                return False
-            
-            # if p or q reaches curr_node .. return its.
-            left = dfs(root.left)
-            right = dfs(root.right)
-            myself = root == p or root == q
 
-            if left + myself + right >= 2:
-                lca = root
-            
-            return left or myself or right
-            
-
-            # how to stop when we meet one of p or q
-            # and keep moving lower ?
-            # p and q will be root left or right.. how to check them?
-            # 4 ways... and if 2 >= return root
-        lca = None
-        dfs(root)
-        return lca
-
-            
