db6e56a3b6580f30e710c8251ed1710ed408fd11
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-03 11:20:47 +0000

    Time: 155 ms (42.7%), Space: 19.8 MB (50.69%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -7,6 +7,31 @@ class Solution:
         every time I can choose + or -
         if at the end sum == target return 1
         """
+
+        # bottom up
+        total = sum(nums)
+
+        if abs(target) > total:
+            return 0
+
+        offset = total
+        n = len(nums)
+
+        dp = [[0] * (2 * total + 1) for _ in range(n + 1)]
+        dp[0][offset] = 1  # sum = 0
+
+        for i in range(1, n + 1):
+            for s in range(2 * total + 1):
+                if dp[i-1][s] != 0:
+                    dp[i][s + nums[i-1]] += dp[i-1][s]
+                    dp[i][s - nums[i-1]] += dp[i-1][s]
+
+        return dp[n][target + offset]
+
+
+
+
+        # top down
         def dp(i,curr_sum):
             if i == len(nums):
                 if curr_sum == target:
