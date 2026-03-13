9cb55b4a49c30586215b83ab7fe86f867a2ee6ed
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-13 23:45:07 +0000

    Time: 7 ms (23.83%), Space: 22.3 MB (9.78%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,6 +1,22 @@
 import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # sweep line
+        events = []
+        for start, end in intervals:
+            events.append((start, 1))
+            events.append((end, -1))
+
+        events.sort()
+
+        max_room = 0
+        total_score = 0
+        for position, score in events:
+            total_score += score
+            max_room = max(max_room, total_score)
+        return max_room
+
+
         """
         add close position in heap
         and check if heap[0] > start -> add end in the heap and renew max room
