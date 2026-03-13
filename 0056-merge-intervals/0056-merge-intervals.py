21ef1dcd52e37a7e6a0c470b8d0bc3c3b5d0543d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-13 23:21:08 +0000

    Time: 11 ms (25.12%), Space: 24 MB (7.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,11 +1,33 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        """
-        1 2 3 4 5 6 7 8
-        - - -
-          - - - - -
-        1~~~~~~~~~6
-        """
+        # v2 sweep line
+        # o(n log n)
+        events = []
+        for start, end in intervals:
+            events.append((start, -1))
+            events.append((end, 1))
+        
+        events.sort()
+        merged = []
+        curr = [float('inf'), float('-inf')]
+        curr_score = 0
+
+        for position, score in events:
+            # open
+            if score < 0 and curr[0] > position:
+                curr[0] = position
+            # close
+            elif score > 0 and curr[1] < position:
+                curr[1] = position
+            
+            curr_score += score
+            if curr_score == 0:
+                merged.append(curr)
+                curr = [float('inf'), float('-inf')]
+        return merged
+
+
+        # o(n log n)
         intervals.sort()
         merged = [intervals[0]]
 
@@ -18,4 +40,6 @@ class Solution:
             else:
                 merged.append(curr)
         return merged
+
+    
         
