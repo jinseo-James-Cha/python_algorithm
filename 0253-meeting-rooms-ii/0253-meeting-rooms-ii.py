e25a9b9c101d879df94db990e9367d173b799280
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-13 23:32:51 +0000

    Time: 5 ms (33.1%), Space: 21.4 MB (70.41%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,83 +1,27 @@
+import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        #  0 5 10 15 20 25 30
-        #A - -  -  -  -  -  -
-        #B   -  -
-        #C         - -
-
-        # check intervals by start order
+        """
+        add close position in heap
+        and check if heap[0] > start -> add end in the heap and renew max room
+        """
+        minimum_room = 1
         
-        # case 1: non-overlapping
-        # if prev.end < curr.start
-        #   update prev = curr
-
-        # case 2: overlapping
-        # if prev.end >= curr.start
-        #   append curr
-
-        # every time get lengh of the queue
-        intervals.sort(key=lambda x:x[0])
-
+        intervals.sort()
+        
         pq = []
         heapq.heappush(pq, intervals[0][1])
-        max_len = 1
 
-        for curr_start, curr_end in intervals[1:]:
-            minimum_end = pq[0]
-
-            if minimum_end <= curr_start:
+        for i in range(1, len(intervals)):
+            curr = intervals[i]
+            # if pq[0] > curr[0]:
+            #     heapq.heappush(pq, curr[1])
+            #     minimum_room = max(minimum_room, len(pq))
+            # else:
+            while pq and curr[0] >= pq[0]:
                 heapq.heappop(pq)
-                
-            heapq.heappush(pq, curr_end)
-        return len(pq)
+            heapq.heappush(pq, curr[1])
+            minimum_room = max(minimum_room, len(pq))
 
-        # priority queue
-        # 0 ------- 30 start 31 ..
-        # 
-        # 30
-        intervals.sort()
-
-        rooms = []
-        heapq.heappush(rooms, intervals[0][1]) # add first's end time
+        return minimum_room
         
-        for start, end in intervals[1:]:
-            if start >= rooms[0]:
-                heapq.heappop(rooms)
-            
-            heapq.heappush(rooms, end)
-        return len(rooms)
-
-
-        # sweep line
-        events = []
-        for start, end in intervals:
-            events.append((start, 1))
-            events.append((end, -1))
-        
-        events.sort()
-
-        res = 0
-        curr_room = 0
-        for point, score in events:
-            curr_room += score
-            res = max(res, curr_room)
-        
-        return res
-
-
-
-
-        # intervals.sort()
-        # res = 1
-        # curr_room = 1
-        # prev_end = intervals[0][1]
-
-        # for i in range(1, len(intervals)):
-        #     if intervals[i][0] < prev_end:
-        #         curr_room += 1
-        #         res = max(res, curr_room)
-                
-        #     else:
-        #         curr_room -= 1
-        #     prev_end = min(prev_end, intervals[i][1])
-        # return res
