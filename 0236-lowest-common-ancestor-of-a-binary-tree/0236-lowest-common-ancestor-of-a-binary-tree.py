bcb5083b65d296ab1704bfc9d6fbf90e07d9a871
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-15 23:33:28 +0000

    Time: 55 ms (82.74%), Space: 23.7 MB (95.23%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -4,14 +4,33 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+from collections import deque
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         # bfs
+        parents = {root: None}
+        pq = deque([root])
+        while pq:
+            node = pq.popleft()
+            if node.left:
+                parents[node.left] = node
+                pq.append(node.left)
+            if node.right:
+                parents[node.right] = node
+                pq.append(node.right)
+        
+        p_ancestors = set()
+        while p:
+            p_ancestors.add(p)
+            p = parents[p]
 
-        # dfs
+        while q not in p_ancestors:
+            q = parents[q]
+        
+        return q
+
+        # dfs post order traversal
         # score
-
         def dfs(node):
             nonlocal lca
             if not node:
