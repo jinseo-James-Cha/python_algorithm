463f6260f435331b989d8b4e1a8e344e08386a7e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-16 12:14:19 +0000

    Time: 13 ms (12%), Space: 25.1 MB (5.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,57 +1,19 @@
-from collections import Counter
-import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # priority queue
-        # O(n log n)
-        counts = Counter(nums)
+        frequency = {}
+        for num in nums:
+            frequency[num] = frequency.get(num, 0) + 1
 
-        pq = []
-        for num, freq in counts.items():
-            heapq.heappush(pq, (-freq, num))
-        
+        bucket = [[] for _ in range(len(nums) + 1)]
+        for num, freq in frequency.items():
+            bucket[freq].append(num)
+        print(bucket)
         res = []
-        for i in range(k):
-            freq, num = heapq.heappop(pq)
-            res.append(num)
-        
+        for i in range(len(bucket)-1, -1, -1):
+            if bucket[i]:
+                for num in bucket[i]:
+                    res.append(num)
+                    if len(res) == k:
+                        return res
         return res
-
-        # bucket sort
-        # O(n)
-        if len(nums) <= k:
-            return nums
-        
-        counts = Counter(nums)
-        buckets = [[] for _ in range(len(nums) + 1)]
-        for num, freq in counts.items():
-            buckets[freq].append(num)
-        
-        # flatten
-        res = []
-        for i in range(len(buckets)-1, 0, -1):
-            for num in buckets[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
-        return res
-        # res = [x for bucket in buckets for x in bucket]
-        # return res[::-1][:k]
-
-
-
-
-
-
-
-
-        if len(nums) == k:
-            return nums
-        
-        buckets = [[] for _ in range(len(nums) + 1)] # 만약 다 같은 숫자여도 저장가능해야해서 +1
-        counts = Counter(nums)
-        for num, freq in counts.items():
-            buckets[freq].append(num)
-        
-        flat_list = [x for bucket in buckets for x in bucket]
-        return flat_list[::-1][:k]
