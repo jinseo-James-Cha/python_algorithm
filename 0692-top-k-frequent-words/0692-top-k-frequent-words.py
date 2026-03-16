0349f1d436ddec58532ad32c03de4f231cb4263e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-16 11:42:46 +0000

    Time: 0 ms (100%), Space: 19.6 MB (43.82%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,34 +1,21 @@
-# min heap not working
-# from collections import Counter
-# import heapq
-# class Solution:
-#     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-#         freqs = Counter(words)
-#         min_heap = []
-#         for word, freq in freqs.items():
-#             heapq.heappush(min_heap, (freq, word))
-
-#             # remove if it is more than k
-#             if len(min_heap) > k:
-#                 heapq.heappop(min_heap)
-
-#         res = [heapq.heappop(min_heap)[1] for _ in range(k)]
-#         print(res)
-#         res.reverse()
-#         return res
-
-# max heap
-from collections import Counter
+from collections import defaultdict
 import heapq
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        freqs = Counter(words)
+        # get frequence by word
+        frequency = {}
+        for word in words:
+            frequency[word] = frequency.get(word, 0) + 1
         
-        # using tuple (1priority to compare, 2priority if 1st is the same value)
-        # using -freq to take "minimum" first but actually maximum
-        max_heap = [(-freq, word) for word, freq in freqs.items()]
+        bucket = [[] for _ in range(len(words) + 1)]
+        for word, freq in frequency.items():
+            heapq.heappush(bucket[freq], word)
 
-        # transform list -> heapq by tuple elements
-        heapq.heapify(max_heap)
-        return [heapq.heappop(max_heap)[1] for _ in range(k)]
-    
+        res = []
+        for i in range(len(bucket) - 1, -1, -1):
+            while bucket[i]:
+                res.append(heapq.heappop(bucket[i]))
+
+                if len(res) == k:
+                    return res
+        return res
