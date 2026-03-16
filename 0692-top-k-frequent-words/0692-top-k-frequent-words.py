e6425026ebeae57267bc8e2c8151935215e26d46
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-16 11:48:25 +0000

    Time: 3 ms (54.19%), Space: 19.5 MB (70.52%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -2,6 +2,23 @@ from collections import defaultdict
 import heapq
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        # min heap version 2
+        frequency = {}
+        for word in words:
+            frequency[word] = frequency.get(word, 0) + 1
+
+        bucket = []
+        for word, freq in frequency.items():
+            heapq.heappush(bucket, (-freq, word))
+        
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(bucket)[1])
+        return res
+
+        
+
+        # min heap version 1
         # get frequence by word
         frequency = {}
         for word in words:
