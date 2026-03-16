25263ebec7f5f450ce275072f7bd96638a18424b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-16 12:17:46 +0000

    Time: 7 ms (51.15%), Space: 24.7 MB (26.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -8,12 +8,25 @@ class Solution:
         bucket = [[] for _ in range(len(nums) + 1)]
         for num, freq in frequency.items():
             bucket[freq].append(num)
-        print(bucket)
+
         res = []
         for i in range(len(bucket)-1, -1, -1):
-            if bucket[i]:
-                for num in bucket[i]:
-                    res.append(num)
-                    if len(res) == k:
-                        return res
+            for num in bucket[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+        return res
+
+        # priority queue
+        # O(n log n)
+        counts = Counter(nums)
+
+        pq = []
+        for num, freq in counts.items():
+            heapq.heappush(pq, (-freq, num))
+        
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(pq)[1])
+        
         return res
