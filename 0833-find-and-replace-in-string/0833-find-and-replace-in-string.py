613b4becf9b8c3dc22337d593f907e3b794ebcc6
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-18 16:47:20 +0000

    Time: 0 ms (100%), Space: 19.4 MB (46.12%) - LeetHub

--- 0833-find-and-replace-in-string/0833-find-and-replace-in-string.py
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.py
@@ -1,5 +1,8 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
+        # time complexity = O(N + K * L) L: length of a source 
+        # =>s[idx: idx + len(src)] → O(len(src)) -> O(L)
+        
         changes = {}
         for idx, src, tgt in zip(indices, sources, targets):
             if s[idx: idx + len(src)] == src:
