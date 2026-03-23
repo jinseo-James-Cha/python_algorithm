614d3d6e335a0a8ad7b9f6162e6ecf4e1c4bf787
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-23 22:46:42 +0000

    Time: 5234 ms (10.14%), Space: 29 MB (90.73%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -2,6 +2,78 @@ from collections import deque
 import heapq
 class Solution:
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        # binary search + bfs
+        def is_within_bounds(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+        
+        def is_valid_safeness(grid, minSafeness):
+            n = len(grid)
+            if grid[0][0] < minSafeness or grid[n-1][n-1] < minSafeness:
+                return False
+            
+            queue = deque([(0,0)])
+            visited = [[False] * n for _ in range(n)]
+            visited[0][0] = True
+
+            while queue:
+                curr_r, curr_c = queue.popleft()
+                if curr_r == n-1 and curr_c == n-1:
+                    return True
+                
+                for dr, dc in DIRS:
+                    next_r, next_c = curr_r + dr, curr_c + dc
+                    if is_within_bounds(next_r, next_c) and not visited[next_r][next_c] and grid[next_r][next_c] >= minSafeness:
+                        visited[next_r][next_c] = True
+                        queue.append((next_r, next_c))
+
+            return False
+
+        
+        # update 1 -> 0 and 0 -> -1
+        n = len(grid)
+        queue = deque()
+        for row in range(n):
+            for col in range(n):
+                if grid[row][col] == 1:
+                    queue.append((row, col))
+                    grid[row][col] = 0
+                else:
+                    grid[row][col] = -1
+        
+        # bfs
+        # update with safeness factor
+        safeness = 1
+        DIRS = [(1,0), (-1,0), (0,1), (0,-1)]
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                r,c = queue.popleft()
+                for dr, dc in DIRS:
+                    next_r, next_c = r+dr, c+dc
+                    if is_within_bounds(next_r, next_c) and grid[next_r][next_c] == -1:
+                        grid[next_r][next_c] = safeness
+                        queue.append((next_r, next_c))
+            safeness += 1
+
+        # binary search for possible answers 0~ max safeness
+        left = 0
+        right = safeness
+        res = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if is_valid_safeness(grid, mid):
+                res = mid
+                left = mid + 1
+            else:
+                right = mid -1
+        return res
+        
+
+
+
+
+
+
         """
         a path from (0,0) to (n-1, n-1)
         a cell in a path has minimum mahattan distance == safeness factor
@@ -20,21 +92,8 @@ class Solution:
         2 -> 1 -> 2 -> 1 -> 2 => this path is 1
         # we need to choose the maximum safeness factor every time to keep the minimum high
         # dijstra algorithm with max heap
-        -2 -1  0
-        -1 -2 -1
-         0 -1 -2
         
-        get all thieves and bfs to assign safeness factor
-        starting from (0,0) -> put all options and pop from heapq and return
 
-
-        0 0 1
-        0 0 0
-        1 0 0
-
-        -3 -2 -1
-        -2 -3 -2
-        -1 -2 -3
         """
         # edge case
         n = len(grid)
@@ -64,7 +123,7 @@ class Solution:
                     queue.append((next_r, next_c))
 
         # dijkstra algorithm
-        pq = [(-dist[0][0], 0, 0)] # -3 0 0
+        pq = [(-dist[0][0], 0, 0)]
         visited = [[False] * n for _ in range(n)]
         while pq:
             curr_safeness, r, c = heapq.heappop(pq)
