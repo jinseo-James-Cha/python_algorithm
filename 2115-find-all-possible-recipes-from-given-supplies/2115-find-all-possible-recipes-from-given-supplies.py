74fa7efdc5aeffd12c9f37c7c8af54a280bcfe7e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-23 11:51:28 +0000

    Time: 24 ms (97.53%), Space: 21.5 MB (84.92%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,72 +1,58 @@
+from collections import deque
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         """
-        I have n recipes
-        recipe[i] -> ingredients[i] are all in supplies? -> then return recipe[i]
-        """
-        # kahn's topological sort
-        set_sup = set(supplies)
-        recipe_to_index = {recipe: idx for idx, recipe in enumerate(recipes)}
+        recipes ingredients
 
-        graph = defaultdict(list)
-        indegree = [0] * len(recipes)
-
-        for recipe_idx, ingredient_list in enumerate(ingredients):
-            for ingredient in ingredient_list:
-                if ingredient not in set_sup:
-                    graph[ingredient].append(recipes[recipe_idx])
-                    indegree[recipe_idx] += 1
+        recipes + ingredients -> new recipe -> can be a ingredient for another recipe
         
-        queue = deque()
-        for idx, count in enumerate(indegree):
-            if count == 0:
-                queue.append(idx)
+        yeast, flour -> bread
+        bread, meat -> sandwitch
 
-        created_recipes = []
-        while queue:
-            recipe_idx = queue.popleft()
-            recipe = recipes[recipe_idx]
-            created_recipes.append(recipe)
+        directed acyclic graph -> kahn's topological sort
+        
+        check which recipe needs ingredients from recipes
+        check which recipe can make by initiated supplies -> starting points
 
-            for neighbor in graph[recipe]:
-                indegree[recipe_to_index[neighbor]] -= 1
-                if indegree[recipe_to_index[neighbor]] == 0:
-                    queue.append(recipe_to_index[neighbor])
+        we start from bread because it needs only initial ingredients
 
-        return created_recipes
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # brute force
-        # check each each ingredients in supplies
-        # it is not working cuz of order matters in this solution..
+        """
         n = len(recipes)
         set_supplies = set(supplies)
-        res = []
-
+        
+        graph = defaultdict(list)
+        indegree = [0] * n
         for i in range(n):
             curr_recipe = recipes[i]
-            curr_ingredients = ingredients[i]
+            for ingredient in ingredients[i]:
+                if ingredient not in set_supplies:
+                    indegree[i] += 1
+                    graph[ingredient].append(recipes[i])
 
-            all_pass = True
-            for ing in curr_ingredients:
-                if ing not in set_supplies:
-                    all_pass = False
-                    break
+        # indegree [0, 1]
+        # initial recipe if indegree 0
+        pq = deque()
+        for i, count in enumerate(indegree):
+            if count == 0:
+                pq.append(i)
+
+        recipe_to_idx = {}
+        for i, recipe in enumerate(recipes):
+            recipe_to_idx[recipe] = i
+
+        # pq = [0]
+        res = []
+        while pq:
+            recipe_idx = pq.popleft()
+            complete_recipe = recipes[recipe_idx]
             
-            if all_pass:
-                set_supplies.add(curr_recipe)
-                res.append(curr_recipe)
+            # register
+            res.append(complete_recipe)
+
+            # check other ingredients to use this new recipe
+            for next_recipe in graph[complete_recipe]:
+                # i need the idx from recipe name
+                indegree[recipe_to_idx[next_recipe]] -= 1
+                if indegree[recipe_to_idx[next_recipe]] == 0:
+                    pq.append(recipe_to_idx[next_recipe])
         return res
