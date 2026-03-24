3ed1e0afa53b303a843dad12009985391b43c51e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-24 16:52:10 +0000

    Time: 266 ms (23.15%), Space: 21.4 MB (95.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -8,24 +8,54 @@ class Solution:
         find a "1" and dfs and check visited -> if it is done count += 1
         I can put label starting from "2" and return label - 2
         """
+
+        # DFS
+        # Time complexity = O(M*N)
+        # Space complexity = O(M*N) => recursive for all land cells
+        # def is_within_bounds(row, col):
+        #     return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+        # DIRS = [(1,0), (-1,0), (0,1), (0,-1)]
+        # def dfs(row, col, label):
+        #     grid[row][col] = str(label)
+
+        #     for dr, dc in DIRS:
+        #         next_r, next_c = row+dr, col+dc
+        #         if is_within_bounds(next_r, next_c) and grid[next_r][next_c] == "1":
+        #             dfs(next_r, next_c, label)
+
+        # label = 2
+        # m,n = len(grid), len(grid[0])
+        # for row in range(m):
+        #     for col in range(n):
+        #         if grid[row][col] == "1":
+        #             dfs(row, col, label)
+        #             label += 1
+        
+        # return label - 2
+
+
+        # BFS
+        # Time complexity = O(M*N)
+        # Space complexity = O(min(M, N)) => queue can grow up to min(m,n)
         def is_within_bounds(row, col):
             return 0 <= row < len(grid) and 0 <= col < len(grid[0])
 
+        m, n = len(grid), len(grid[0])
+        num_of_islands = 0
         DIRS = [(1,0), (-1,0), (0,1), (0,-1)]
-        def dfs(row, col, label):
-            grid[row][col] = str(label)
-
-            for dr, dc in DIRS:
-                next_r, next_c = row+dr, col+dc
-                if is_within_bounds(next_r, next_c) and grid[next_r][next_c] == "1":
-                    dfs(next_r, next_c, label)
-
-        label = 2
-        m,n = len(grid), len(grid[0])
         for row in range(m):
             for col in range(n):
                 if grid[row][col] == "1":
-                    dfs(row, col, label)
-                    label += 1
-        
-        return label - 2
+                    num_of_islands += 1
+                    queue = [(row, col)]
+                    
+                    while queue:
+                        curr_r, curr_c = queue.pop()
+                        grid[curr_r][curr_c] = "0"
+                        for dr, dc in DIRS:
+                            next_r, next_c = curr_r + dr, curr_c + dc
+                            if is_within_bounds(next_r, next_c) and grid[next_r][next_c] == "1":
+                                queue.append((next_r, next_c))
+
+        return num_of_islands
