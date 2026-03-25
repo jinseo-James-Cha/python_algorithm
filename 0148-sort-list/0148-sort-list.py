c91f3c2b99e83df958912c1ea5399127b61f3368
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-25 11:16:07 +0000

    Time: 230 ms (13.8%), Space: 40.6 MB (94.97%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,41 +5,55 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # divide and conquer
-        # top down merge sort
-        # O(n logn) -> merge sort?!
+        """
+        sort ?
+        what can I choose to sort linkedlist?
+        - cannot have random access through indexing -> no QuickSort
+
+        divide     4->2->1->3      
+        divide   4->2     1->3
+        divide 4     2 1     3
+        merge   2->4     1->3
+        merge     1->2->3->4 
+
+        # divide half in linked list -> using slow&fast pointers
+        # merge -> compare first and second list and move next list until one is done
+        """
+
+        def find_second_half(head):
+            slow = fast = head
+            while fast.next and fast.next.next:
+                slow = slow.next
+                fast = fast.next.next
+            
+            second_half = slow.next
+            slow.next = None
+            return second_half
+        
+        def merge_lists(first_list, second_list):
+            merged_list = ListNode()
+            dummy = merged_list
+            
+            while first_list and second_list:
+                if first_list.val < second_list.val:
+                    dummy.next = first_list
+                    first_list = first_list.next
+                else:
+                    dummy.next = second_list
+                    second_list = second_list.next
+                dummy = dummy.next
+            
+            dummy.next = first_list if first_list else second_list
+            return merged_list.next
 
         if not head or not head.next:
-            return head
+                return head
+            
+        second_half_head = find_second_half(head)
         
-        mid = self.getPartition(head)
-        left = self.sortList(head)
-        right = self.sortList(mid)
-        return self.mergeSortList(left, right)
+        first_half_sorted = self.sortList(head)
+        second_half_sorted = self.sortList(second_half_head)
+        
+        return merge_lists(first_half_sorted, second_half_sorted)
 
-    def mergeSortList(self, list1, list2):
-        dummyHead = ListNode(0)
-        tail = dummyHead
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next
-        tail.next = list1 if list1 else list2
-        return dummyHead.next
     
-    def getPartition(self, head):
-        slow = head
-        fast = head
-        prev = None # to save slow's prev 
-
-        while fast and fast.next:
-            prev = slow
-            slow = slow.next
-            fast = fast.next.next
-
-        prev.next = None
-        return slow  
