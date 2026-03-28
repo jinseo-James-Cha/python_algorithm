df9d2ee9f3c8a20e0324dc4a0f78e5213054c714
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-29 00:49:22 +0100

    Time: 0 ms (100%), Space: 19.3 MB (76.12%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,8 +2,21 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         """
         either take this house or skip this for the next house?
-        state: house index, is_took
+        state: house index
         """
+        # bottom up
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[n-1]
+
         # top down
         def dp(i):
             if i == 0:
