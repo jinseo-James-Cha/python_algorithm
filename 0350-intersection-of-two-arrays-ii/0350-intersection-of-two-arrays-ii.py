a6d1fdf238e4f642556794071106aebd559e6131
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-28 22:42:35 +0000

    Time: 0 ms (100%), Space: 19.2 MB (93.52%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,21 +1,12 @@
-from bisect import bisect_left, bisect_right
-
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort()
-        nums2.sort()
+        num_count = {}
+        for num in nums1:
+            num_count[num] = num_count.get(num, 0) + 1
         
-        is_num1_small = len(nums1) < len(nums2)
-        if not is_num1_small:
-            nums1, nums2 = nums2, nums1
-            
-        answer = []
-        for i in range(len(nums1)):
-            if nums1[i] in answer:
-                continue
-            elif nums1[i] in nums2:
-                n1_count = bisect_right(nums1, nums1[i]) - bisect_left(nums1, nums1[i])
-                n2_count = bisect_right(nums2, nums1[i]) - bisect_left(nums2, nums1[i])
-                answer += [nums1[i]] * min(n1_count, n2_count)
-                
-        return answer
+        res = []
+        for num in nums2:
+            if num in num_count and num_count[num] > 0:
+                res.append(num)
+                num_count[num] -= 1
+        return res
