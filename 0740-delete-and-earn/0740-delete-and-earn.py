ed8a6055c30b5daac22010366f80395e1882db56
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-31 00:40:24 +0200

    Time: 11 ms (37.63%), Space: 21.3 MB (44.29%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -6,10 +6,25 @@ class Solution:
         delete every element equal to nums[i] - 1
         and nums[i] + 1
         """
-
         # 2 2 3 3 3 4
         # 2-4 , 3-9, 4-4
-        # top down
+        
+        # dp bottom up
+        hashmap = defaultdict(int)
+        max_number = 0
+        for num in nums:
+            hashmap[num] += num
+            max_number = max(max_number, num)
+        
+        dp = [0] * (max_number + 1)
+        dp[1] = hashmap[1]
+        for i in range(2, max_number+1):
+            dp[i] = max(dp[i-1], dp[i-2] + hashmap[i])
+        return dp[-1]
+
+
+
+        # dp top down
         hashmap = defaultdict(int)
         max_number = 0
         for num in nums:
