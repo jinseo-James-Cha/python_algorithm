38cf54e34f8cd3cdbf00e8e2fac08e98ca413f55
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-31 00:22:07 +0200

    Time: 3 ms (63.24%), Space: 19.5 MB (37.21%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,5 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # dp bottom up
         prev_prev = prev = 0
         for i in range(2, len(cost) + 1):
             curr = min(prev + cost[i - 1], prev_prev + cost[i - 2])
@@ -7,7 +8,6 @@ class Solution:
             prev_prev = prev
             prev = curr
         return prev
-
 
         # dp top down
         def dp(i):
