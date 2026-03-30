3b24e96752fc0167971c6e1a5230a768b67ab257
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-03-31 00:36:40 +0200

    Time: 15 ms (20.4%), Space: 24.9 MB (11.05%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,73 +1,31 @@
-from collections import defaultdict
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        # return "maximized" point
-        # pick any nums[1] delete 
-        # and then res += nums[1]
+        """
+        maximize points
+        pick nums[i] and delete it to earn nums[i] points
+        delete every element equal to nums[i] - 1
+        and nums[i] + 1
+        """
 
-        # and then delete the both nums[1] + 1 or nums[1] - 1
-
-        # seems like I have to check all the possibilities...
-        # one remove and remove if there is one..
-        # bottom up optimized
-        if len(nums) == 1:
-            return nums[0]
-        
+        # 2 2 3 3 3 4
+        # 2-4 , 3-9, 4-4
+        # top down
         hashmap = defaultdict(int)
         max_number = 0
         for num in nums:
             hashmap[num] += num
             max_number = max(max_number, num)
         
-        prev_prev = 0
-        prev = hashmap[1]
-        for i in range(2, max_number + 1):
-            current = max(prev, prev_prev + hashmap[i])
-            prev_prev = prev
-            prev = current
-        return prev
 
-
-
-
-        # bottom up
-        # if len(nums) == 1:
-        #     return nums[0]
-
-        # hashmap = defaultdict(int)
-        # max_number = 0
-        # for num in nums:
-        #     hashmap[num] += num
-        #     max_number = max(max_number, num)
-        
-        # dp = [0] * (max_number+1)
-        # dp[1] = hashmap[1]
-        # for i in range(2, max_number+1):
-        #     dp[i] = max(dp[i-2] + hashmap[i], dp[i-1])
-        # return dp[max_number]
-        
-
-
-
-
-        # top down
-        # hashmap = defaultdict(int)
-        # max_number = 0
-        # for num in nums:
-        #     hashmap[num] += num
-        #     max_number = max(max_number, num)
-        
-
-        # def dp(num):
-        #     if num == 0:
-        #         return 0
-        #     if num == 1:
-        #         return hashmap[1]
+        def dp(num):
+            if num == 0:
+                return 0
+            if num == 1:
+                return hashmap[1]
             
-        #     if num not in memo:
-        #         memo[num] = max(dp(num-1), dp(num-2) + hashmap[num])
-        #     return memo[num]
+            if num not in memo:
+                memo[num] = max(dp(num-1), dp(num-2) + hashmap[num])
+            return memo[num]
 
-        # memo = {}
-        # return dp(max_number)
-
+        memo = {}
+        return dp(max_number)
