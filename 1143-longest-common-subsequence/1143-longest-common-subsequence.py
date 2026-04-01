51da07bedd6800797af1896ceca82f191fb00060
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-01 17:58:55 +0200

    Time: 275 ms (90.51%), Space: 19.6 MB (88.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -7,6 +7,22 @@ class Solution:
         """
 
         """
+        DP bottom up - optimized space
+        """
+        m, n = len(text1), len(text2)
+        prev = [0] * (n+1)
+        for i in range(1, m+1):
+            curr = [0] * (n+1)
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    curr[j] = prev[j-1] + 1
+                else:
+                    curr[j] = max(prev[j], curr[j-1])
+            prev = curr
+        return prev[n]
+
+
+        """
         DP bottom up
         """
         m, n = len(text1), len(text2)
