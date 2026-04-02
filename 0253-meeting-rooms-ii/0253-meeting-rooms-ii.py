437bb283192fff7dbb5dc7279892bf12e297bacc
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-02 20:01:00 +0200

    Time: 3 ms (84.44%), Space: 21.5 MB (63.17%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         """
@@ -23,6 +24,17 @@ class Solution:
         if prev.end > curr.start -> overlapping
         if prev.end <= curr.start -> no overlapping
         """
+        # pirority queue
+        intervals.sort(key=lambda x:x[0])
+        free_rooms = []
+        heapq.heappush(free_rooms, intervals[0][1])
+
+        for i in range(1, len(intervals)):
+            if free_rooms[0] <= intervals[i][0]:
+                heapq.heappop(free_rooms)
+            
+            heapq.heappush(free_rooms, intervals[i][1])
+        return len(free_rooms)
 
 
         # sweep line
