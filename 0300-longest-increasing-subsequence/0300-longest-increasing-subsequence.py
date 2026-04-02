77afd71794e7f749c71936fd676f10ccc2c22572
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-02 17:51:26 +0200

    Time: 2 ms (96.14%), Space: 19.6 MB (46.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,67 +1,29 @@
 from bisect import bisect_left
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # 10 9 2 5 3 7
-        # 10
-        # 9
-        # 2
-        # 2 5
+        """
+        10 9 2 5 3 7 101 18
+        -> longest -> 2 3 7 101
 
-        lis = [nums[0]]
-        for i in range(1, len(nums)):
-            if lis[-1] >= nums[i]:
-                lis_idx = bisect_left(lis, nums[i])
-                lis[lis_idx] = nums[i]
-            else:
-                lis.append(nums[i])
-        return len(lis)
+        monotonic stack?
 
+        10
+        9
+        2 5 and 3 is smaller than 5 -> pop() -> and append -> 2 3
+        7 is greater 3 => 2 3 7
+        101 is greater 7 => 2 3 7 101
+        18 is smaller than 101 -> but 2 3 7 18 possible
+        """
 
-
-
-
-
-
-
-
-
-
-
-        # dp bottom up
-        # dp = [1] * len(nums)
-        # for i in range(1, len(nums)):
-        #     current = 1
-        #     for j in range(i):
-        #         if nums[i] > nums[j]:
-        #             current = max(current, dp[j] + 1)
-        #     dp[i] = current
-        # return max(dp)
-
-        # o(nlogn)
-        subs = [nums[0]]
+        stack = []
+        longest = 0
         for num in nums:
-            if num > subs[-1]:
-                subs.append(num)
+            if not stack:
+                stack.append(num)
             else:
-                i = bisect_left(subs, num)
-                subs[i] = num
-        print(subs)
-        return len(subs)
-
-
-
-        # maximum ?! or minimum ?! -> is it DP ?
-
-        # 1. dp[i]
-        
-        # 2.recurrence relation -> a way to transition between states dp[5] and dp[7]
-        # -> dp[i] = max(dp[j] + 1) for all j where nums[j] < nums[i] and j < i
-
-        # 3. base case
-        
-        # dp = [1] * len(nums)
-        # for i in range(1, len(nums)):
-        #     for j in range(i):
-        #         if nums[i] > nums[j]:
-        #             dp[i] = max(dp[i], dp[j] + 1)
-        # return max(dp)
+                if stack[-1] < num:
+                    stack.append(num)
+                else:
+                    insert_idx = bisect_left(stack, num)
+                    stack[insert_idx] = num
+        return len(stack)
