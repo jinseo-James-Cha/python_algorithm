4a74ddc2331b10173a5f6de451301b4eb43a44f9
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-02 02:25:26 +0200

    Time: 223 ms (65.19%), Space: 19.4 MB (97.44%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,23 +1,16 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # state
-        # ith coins
-        # amount
-
-        # no duplication combinations !
-
         # bottom up optimization
         dp = [0] * (amount + 1)
         dp[0] = 1  # 0원을 만드는 방법은 아무것도 안 쓰는 경우 1개
 
         for coin in coins:  # 코인을 바깥 루프로
             for j in range(coin, amount + 1):  # j-c가 존재할 때만
-                dp[j] += dp[j - coin]
+                dp[j] += dp[j - coin] # 나 자신 값 + 내 이후의 경우의 수 = 나의 경우의 수에 저장
         return dp[amount]
 
         # bottom up
         n = len(coins)
-        # dp[coins][amount]
         dp = [[0] * (amount + 1) for _ in range(n+1)] 
         for i in range(n):
             dp[i][0] = 1 
@@ -30,9 +23,6 @@ class Solution:
                     dp[i][j] = dp[i + 1][j] + dp[i][j - coins[i]]
                     
         return dp[0][amount]
-
-
-
 
         # top down
         def dp(i, target_amount):
@@ -52,39 +42,16 @@ class Solution:
         memo = {}
         return dp(0, amount)
 
-
-
-
-
-
-
-
-        # top down
-        def dfs(i, amount):
-            if amount == 0:
-                return 1
-            if i == len(coins):
+        # backtracking
+        def backtrack(start, total):
+            if total > amount:
                 return 0
-            if memo[i][amount] != -1:
-                return memo[i][amount]
-            
-            if coins[i] > amount: # coin is greater than amount, skip to the next
-                memo[i][amount] = dfs(i+1, amount)
-            else: # calculatable coin !
-                memo[i][amount] = dfs(i, amount-coins[i]) + dfs(i+1, amount)
+            if total == amount:
+                return 1
+    
+            count = 0
+            for i in range(start, len(coins)):
+                count += backtrack(i, total + coins[i])
+            return count
 
-            return memo[i][amount]
-        
-        memo = [[-1] * (amount + 1) for _ in range(len(coins))]
-        dfs(0, amount)
-        print(memo)
-        return 0
-
-        # dp = [0] * (amount + 1)
-        # dp[0] = 1
-
-        # for c in coins:
-        #     for a in range(1, amount + 1):
-        #         if a - c >= 0:
-        #             dp[a] += dp[a-c]
-        # return dp[-1]
+        return backtrack(0, 0)
