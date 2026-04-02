a5e16f52e1cb3339b0df0c1f64ab7ef3a7c90e63
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-02 02:02:59 +0200

    Time: 503 ms (69.87%), Space: 19.5 MB (85.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -11,13 +11,14 @@ class Solution:
         1 - 1 => 0 => 3 coins
 
         """
+
+
         # DP - bottom up
         dp = [float('inf')] * (amount+1)
         dp[0] = 0
-        for target in range(1, amount + 1):
-            for coin in coins:
-                if coin <= target:
-                    dp[target] = min(dp[target], dp[target - coin] + 1)
+        for coin in coins:
+            for target in range(coin, amount + 1):
+                dp[target] = min(dp[target], dp[target - coin] + 1)
         return dp[amount] if dp[amount] != float('inf') else -1
                 
 
