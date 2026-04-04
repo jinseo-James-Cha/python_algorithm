59aceca026c374233b4fa57e8ce5469bff95798b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-04 18:46:27 +0200

    Time: 979 ms (81.59%), Space: 24.2 MB (74.85%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.py
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.py
@@ -1,67 +1,126 @@
 class Solution:
     def longestDupSubstring(self, s: str) -> str:
+        # Binary Search + Rabin-Karp + Rolling hash
+        # Time complexity = O(N log N)
+        # Space Complexity = O(N)
         """
-        find duplicate substring 
-        return longest length duplicate substring
+        #Hashing
+        b = 1
+        c = 2
+        d = 3
+        H = (1 * 26^2) + (2 * 26^1) + (3 * 26^0) = 676 + 52 + 3 = 731
 
-        constraint
-        longest length => len(s) - 1
-        minimum length => 1
-        if nothing duplicated => 0
+        #Rolling
+        if it is decimal system, 12345
+        123 -> 234
+        1. remove 1 -> 1 * base10^2 -> 23
+        2. times by base10-> 230
+        3. add the next number 4 -> 234
 
-        brute force -> TLE
-        check all substrings and return the longest
+        Hash(new) = (Hash(prev) - C(out)*base26) * base 26 + C(in)
 
-        - peusocode
-        loop maximum size first 
-            initialize a set to check dup
-            create a substring
-            check if it is already in the set
-              if yes, return the current size
-              if no, add in the set
-
-        time complexity = O(n^3)
-        space complexity = O(n^2)
+        #Module
+        to prevent hashing collision
+        MOD = 10**9 + 7
+        H = (H % MOD)
         """
-        # binary search
-        def get_duplicate_substring(s, size):
-            duplicates = set()
-            for i in range(len(s) - size + 1): # 6 - 3 = 3 -> 0 1 2 3
-                curr_str = s[i:i+size]
-                if curr_str in duplicates:
-                    return curr_str
-                duplicates.add(curr_str)
-            return ""
+        def search_dup(length):
+            if length == 0:
+                return -1
+            
+            h = 0
+            for i in range(length):
+                h = (h * base + nums[i]) % MOD
+            
+            seen = {h}
+            aL = pow(base, length-1, MOD) 
+            for start in range(1, len(s) - length + 1):
+                # Hash(new) = (Hash(prev) - C(out)*base26) * base 26 + C(in)
+                h = ((h - nums[start - 1] * aL) * base + nums[start + length - 1]) % MOD
 
+                if h in seen:
+                    return start
+                seen.add(h)
+            
+            return -1
+
+
+        MOD = 2**63 - 1
+        base = 26
         n = len(s)
-        left = 1 # minimum
-        right = n-1 # maximum
-        longest_dup = ""
-        longest_len = 0
+
+        nums = [ord(s[i]) - ord('a') for i in range(n)]
+
+        start_idx = -1
+        left, right = 1, n-1 # acceptable length for the answer, minimum 1 maximum n-1
         while left <= right:
-            curr_size = (left + right) // 2 # 1 + 5
-            duplicate_string = get_duplicate_substring(s, curr_size)
-            if longest_len < len(duplicate_string):
-                longest_dup = duplicate_string
-                longest_len = len(duplicate_string)
-                left = curr_size + 1
+            curr_length = (left + right) // 2
+
+            found_dup = search_dup(curr_length)
+            if found_dup != -1:
+                max_len = curr_length
+                start_idx = found_dup
+                left = curr_length + 1
             else:
-                right = curr_size - 1
-
-        return longest_dup
-
-        # brute force
-        # banana
-        n = len(s) # 6
-        for curr_size in range(n-1, 0, -1): # 5
-            duplicates = set()
-            for i in range(n - curr_size + 1): # 6 - 5 +1 => 0, 1
-                curr_substring = s[i:i + curr_size] # 0:0+5, 1:1+5
-                if curr_substring in duplicates:
-                    return curr_substring
-                duplicates.add(curr_substring)
-
+                right = curr_length - 1
+                
+        if start_idx != -1:
+            return s[start_idx : start_idx + max_len]
         return ""
 
 
-        
+
+
+        # binary search for length
+        # if found substring -> move longer length
+        # if not, move shorter length        
+        # Time complexity = O(N^2 log N)
+        # Space Complexity = O(N^2)
+        left = 1
+        right = len(s) - 1
+
+        max_len = 0
+        longest_substring = ""
+        while left <= right:
+            length = (left + right) // 2
+
+            seen = set()
+            updated = False
+            for i in range(len(s) - length + 1): # 6 - 3 + 1
+                curr_str = s[i:i+length]
+                if curr_str in seen and max_len < length:
+                    max_len = length
+                    longest_substring = curr_str
+                    updated = True
+                else:
+                    seen.add(curr_str)
+            if updated:
+                left = length + 1
+            else:
+                right = length - 1
+        return longest_substring
+
+
+        # brute force -> MLE
+        # check all substrings and save in set
+        # return the longest
+        # time complexity = O(n^3)
+        # memory complexity = O(n^3)
+        max_len = 0
+        res =  ""
+        seen = set()
+        for i in range(len(s)):
+            curr = s[i]
+            if curr in seen and len(curr) > max_len:
+                max_len = len(curr)
+                res = curr
+            else:
+                seen.add(curr)
+            for j in range(i+1, len(s)):
+                curr += s[j]
+                if curr in seen and len(curr) > max_len:
+                    max_len = len(curr)
+                    res = curr
+                else:
+                    seen.add(curr)
+        return res
