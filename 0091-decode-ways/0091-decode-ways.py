a22256264bfc12f978c1939d75bf0e6ab6a26fd9
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-04 17:43:58 +0200

    Time: 0 ms (100%), Space: 19.7 MB (9.4%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,5 +1,50 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        """
+        return the number of ways to decode a string
+
+        it can decode 1 character or 2 characters
+        my possibilities = 1character decode ways + 2 characters decode ways
+
+        """
+        if len(s) == 1:
+            return 1 if s[0] != "0" else 0
+        
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == "0" else 1
+        for i in range(2, len(s)+1):
+            if s[i-1] != "0":
+                dp[i] = dp[i-1]
+            
+            two_digits = int(s[i-2: i])
+            if 10 <= two_digits <= 26:
+                dp[i] += dp[i-2]
+            
+        return dp[-1] 
+
+
+        def dp(i):
+            if i == len(s):
+                return 1
+            
+            # there is no start with 0
+            if s[i] == "0":
+                return 0
+            
+            if i not in memo:
+                possibilities = dp(i+1)
+                if i + 1 < len(s) and int(s[i:i+2]) <= 26:
+                    possibilities += dp(i+2)
+                memo[i] = possibilities
+
+            return memo[i]
+        
+        memo = {}
+        return dp(0)
+
+
+
         # state
         # string and its index s[i:]
         # decode complete? i == len(s)
@@ -65,25 +110,22 @@ class Solution:
         return dp[-1]
 
         # Top-down / memoization
-        # def dfs(i):
-        #     if i in memo:
-        #         return memo[i]
+        def dp(i):
+            if i in memo:
+                return memo[i]
             
-        #     if i == len(s):
-        #         return 1
+            if i == len(s):
+                return 1
             
-        #     if s[i] == "0":
-        #         return 0
+            if s[i] == "0":
+                return 0
             
-        #     # 한 글자 해석
-        #     res = dfs(i + 1)
-
-        #     # 두 글자 해석 가능하면 더하기
-        #     if i + 1 < len(s) and int(s[i]) * 10 + int(s[i+1]) <= 26:
-        #         res += dfs(i + 2)                    
+            res = dfs(i + 1)
+            if i + 1 < len(s) and int(s[i]) * 10 + int(s[i+1]) <= 26:
+                res += dfs(i + 2)                    
             
-        #     memo[i] = res
-        #     return memo[i]
+            memo[i] = res
+            return memo[i]
         
-        # memo = {}
-        # return dfs(0)
+        memo = {}
+        return dp(0)
