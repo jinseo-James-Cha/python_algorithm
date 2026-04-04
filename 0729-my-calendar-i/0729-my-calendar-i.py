2e2f1b99896136d401805602ebb35dc5105e5ba7
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-05 01:11:31 +0200

    Time: 47 ms (71.77%), Space: 20.3 MB (17.74%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,18 +1,41 @@
 class MyCalendar:
-
     def __init__(self):
-        self.bookings = []
+        self.calendar = []
+
+    def book(self, start: int, end: int) -> bool:
+        right = len(self.calendar)
+        if right == 0:
+            self.calendar.append((start, end))
+            return True
+
+        left = 0
+        while left < right:
+            mid = int(left + (right - left)/2)
+            if self.calendar[mid][1] <= start:
+                left = mid + 1
+            else:
+                right = mid
+
+        if left == len(self.calendar):
+            self.calendar.append((start, end))
+            return True
+                
+        if self.calendar[left][0] >= end:
+            self.calendar.insert(left, (start, end))
+            return True
+            
+        return False
+
+    # def __init__(self):
+    #     self.bookings = []
+
+    # def book(self, startTime: int, endTime: int) -> bool:
+    #     for start, end in self.bookings:
+    #         if start < endTime and end > startTime:
+    #             return False
         
-    #         startTime5 ----- endTime 10
-    #              3---------------9
-    #       1---------5         7----------11
-    def book(self, startTime: int, endTime: int) -> bool:
-        for start, end in self.bookings:
-            if start < endTime and end > startTime:
-                return False
-        
-        self.bookings.append((startTime, endTime))
-        return True
+    #     self.bookings.append((startTime, endTime))
+    #     return True
         
 
 
