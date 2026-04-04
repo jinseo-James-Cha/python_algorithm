4be58fedab61c5da21a43f491db8a12e3808802c
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-04 16:29:51 +0200

    Time: 0 ms (100%), Space: 19.5 MB (20.93%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.py
+++ 0276-paint-fence/0276-paint-fence.py
@@ -1,16 +1,16 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
         # bottom up constant space
-        if n <= 2:
-            return k**n
+        # if n <= 2:
+        #     return k**n
         
-        one_back = k * k
-        two_back = k
-        for i in range(3, n+1):
-            current = (k-1) * one_back + (k-1) * two_back
-            two_back = one_back
-            one_back = current
-        return one_back
+        # one_back = k * k
+        # two_back = k
+        # for i in range(3, n+1):
+        #     current = (k-1) * one_back + (k-1) * two_back
+        #     two_back = one_back
+        #     one_back = current
+        # return one_back
 
 
 
@@ -18,18 +18,22 @@ class Solution:
         # bottom up
         if n == 1:
             return k
-
+        
         dp = [0] * (n+1)
         dp[1] = k
         dp[2] = k * k
-
         for i in range(3, n+1):
-            dp[i] = (k-1) * dp[i-1] + (k-1) * dp[i-2]
-        
+            dp[i] = dp[i-1] * (k-1) + dp[i-2] * (k-1)
+
         return dp[n]
 
 
-        # Top down
+
+        # Counting DP - Top down
+        # possibilities + possibilities => my possibilities
+        # what is my current possibilities?
+        # 1. any different colors with right prev color => dp(i-1) * (k-1)
+        # 2. the same color with prev color=> different colors with 2 prev post => dp(i-2) * (k-1)
         def dp(i):
             if i <= 2:
                 return k**i
