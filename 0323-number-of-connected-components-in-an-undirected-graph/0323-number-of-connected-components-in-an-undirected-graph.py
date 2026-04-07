10abd6cf3c6f27c18281d083c2f70bb0e3e301c2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-07 14:10:28 +0100

    Time: 0 ms (100%), Space: 20.5 MB (66.59%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,5 +1,42 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.ranl = [0] * size
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        xset, yset = self.find(x), self.find(y)
+        if xset == yset:
+            return False
+
+        if self.parent[xset] < self.parent[yset]:
+            self.parent[xset] = yset
+        elif self.parent[xset] > self.parent[yset]:
+            self.parent[yset] = xset
+        else:
+            self.parent[yset] = xset
+            self.rank[xset] += 1
+        return True
+
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        uf = UnionFind(n)
+        connected = 0
+        for a, b in edges:
+            if uf.union(a, b):
+                connected += 1
+        
+        return n - connected
+
+
+
+
+
+
         def dfs(node):
             visited[node] = True
         
