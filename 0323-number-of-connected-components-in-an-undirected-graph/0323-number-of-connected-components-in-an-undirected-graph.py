c34171b69f40a481cc111c6e552e8c5bde7f7109
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-07 14:05:38 +0100

    Time: 3 ms (89.46%), Space: 21.2 MB (34.17%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,36 +1,22 @@
-class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.rank = [0] * size
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        xset, yset = self.find(x), self.find(y)
-
-        if xset == yset:
-            return False
-        
-        if self.rank[xset] > self.rank[yset]:
-            self.parent[yset] = xset
-        elif self.rank[xset] < self.rank[yset]:
-            self.parent[xset] = yset
-        else:
-            self.parent[xset] = yset
-            self.rank[yset] += 1
-        
-        return True
-
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        res = n
-        uf = UnionFind(n)
-
-        for a,b in edges:
-            if uf.union(a,b):
-                res -= 1
+        def dfs(node):
+            visited[node] = True
         
-        return res
+            for neighbor in graph[node]:
+                if not visited[neighbor]:
+                    dfs(neighbor)
+
+        graph = defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        num_of_connected = 0
+        visited = [False] * n
+        for node in range(n):
+            if not visited[node]:
+                num_of_connected += 1
+                dfs(node)
+
+        return num_of_connected
