e838d6bfc85a65c63eb19cee6b535dd987a89047
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-10 16:39:43 +0100

    Time: 1729 ms (31.85%), Space: 60.8 MB (23.29%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,32 +1,34 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
+        # Sieve of Eratosthenes
         if n <= 2:
             return 0
+
+        arr = [True] * n
+        arr[0], arr[1] = False, False
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if not arr[i]:
+                continue
+            
+            for j in range(i*i, n, i):
+                arr[j] = False
+        return sum([arr[i] for i in range(2, n) if arr[i]])
+
+
+        # optimize brute force: O(n * sqrt(n))) : O(n times square root of n)
+        def is_prime_number(i):
+            for j in range(2, int(math.sqrt(i)) + 1):
+                if i % j == 0:
+                    return False
+            return True
+
+        return sum([is_prime_number(x) for x in range(2, n)])
         
-        primes = [True] * n
-        primes[0] = primes[1] = False
-        for i in range(2, n):
-            for j in range(i * i, n, i):
-                primes[j] = False
+        # brute force : O(n^2) -> TLE
+        def is_prime_number(i):
+            for j in range(2, i):
+                if i % j == 0:
+                    return False
+            return True
 
-        return primes.count(True)
-
-# TLE
-# class Solution:
-#     def countPrimes(self, n: int) -> int:
-#         if n <= 1:
-#             return 0
-        
-#         primes = []
-#         for num in range(2, n):
-#             flag = True
-#             for p in primes:
-#                 if num % p == 0:
-#                     flag = False
-#                     break
-#             if flag:
-#                 primes.append(num)
-
-#         return len(primes)
-
-        
+        return sum([is_prime_number(x) for x in range(2, n)])
