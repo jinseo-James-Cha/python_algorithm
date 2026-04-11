7430d785ed52be3c2706e68222e7eca07be9c74f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-11 17:55:37 +0100

    Time: 60 ms (66.89%), Space: 23.6 MB (98.89%) - LeetHub

---
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -0,0 +1,45 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        xset, yset = self.find(x), self.find(y)
+        if xset == yset:
+            return False
+        
+        if xset < yset:
+            self.parent[xset] = yset
+        else:
+            self.parent[yset] = xset
+        return True
+
+
+class Solution:
+    def minimumCost(self, n: int, connections: List[List[int]]) -> int:
+        """
+        n and cities 1 ~ n
+        
+        connections[i] = x <-cost-> y
+
+        all connections with minimum cost -> kruskal
+        - MST
+        - Union Find
+        """
+
+        # sort by cost in ascending order
+        connections.sort(key=lambda x:x[2])
+        total_cost = 0
+
+        uf = UnionFind(n+1)
+        connected = 0
+        for u,v,w in connections:
+            if uf.union(u,v):
+                total_cost += w
+                connected += 1
+        
+        return total_cost if connected == n-1 else -1
