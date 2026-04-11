960b7990a7f0174f2aa7d7c5b90c5afa4f4133bc
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-11 18:13:21 +0100

    Time: 1682 ms (33.23%), Space: 102.7 MB (34.18%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,159 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [0] * size
+    
+    def find(self, x):
+        # path compression
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        xset, yset = self.find(x), self.find(y)
+        if xset == yset:
+            return False
+        
+        if self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = yset
+        elif self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+        else:
+            self.parent[yset] = xset
+            self.rank[xset] += 1
+        return True 
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        # manhattan distance ?
+        # abs(x1, x2) + abs(y1, y2)
+        # kruskal algorithm
+
+        # get all paths ..
+        n = len(points)
+        edges = []
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i+1, n):
+                x2, y2 = points[j]
+                manhattan_distance = abs(x1-x2) + abs(y1-y2)
+                edges.append((i, j, manhattan_distance))
+
+        edges.sort(key=lambda x:x[2])
+        total_distance = 0
+
+        uf = UnionFind(n)
+        for u,v,w in edges:
+            if uf.union(u,v):
+                total_distance += w
+        return total_distance
+
+
+
+
+
+
+
+
+
+
+
+
+        # Prim's algorithm
+        n = len(points)
+        mst_cost = 0
+        edges_used = 0
+        
+        # Track nodes which are visited.
+        visited = [False] * n
+        
+        min_dist = [math.inf] * n 
+        min_dist[0] = 0
+        
+        while edges_used < n:
+            # default setting 
+            curr_min_edge = math.inf
+            curr_node = -1
+            
+            # Pick least weight node which is not in MST.
+            # update the defaults with the least candidate
+            for node in range(n):
+                if not visited[node] and curr_min_edge > min_dist[node]:
+                    curr_min_edge = min_dist[node]
+                    curr_node = node
+            
+            mst_cost += curr_min_edge
+            edges_used += 1
+            visited[curr_node] = True
+            
+            # Update adjacent nodes of current node.
+            for next_node in range(n):
+                weight = abs(points[curr_node][0] - points[next_node][0]) +\
+                         abs(points[curr_node][1] - points[next_node][1])
+                
+                if not visited[next_node] and min_dist[next_node] > weight:
+                    min_dist[next_node] = weight
+        
+        return mst_cost
+        
+        # Prim's algorithm - by adding vertices
+        n = len(points)
+        visited = [False] * n
+        heap = [(0,0)]
+
+        mst_cost = 0
+        edges_used = 0
+        while edges_used < n:
+            weight, curr_node = heapq.heappop(heap)
+
+            if visited[curr_node]:
+                continue
+            
+            visited[curr_node] = True
+            mst_cost += weight
+            edges_used += 1
+
+            for next_node in range(n):
+                if not visited[next_node]:
+                    next_weight = abs(points[curr_node][0] - points[next_node][0]) + abs(points[curr_node][1] - points[next_node][1])
+                    heapq.heappush(heap, (next_weight, next_node))
+            
+        return mst_cost
+
+
+
+
+
+
+
+
+        # Kruskal's algorithm - by adding edges
+        n = len(points)
+        all_edges = []
+
+        # save all edges between nodes with its weight
+        for curr_node in range(n):
+            for next_node in range(curr_node+1, n):
+                weight = abs(points[curr_node][0] - points[next_node][0]) + abs(points[curr_node][1] - points[next_node][1])
+                all_edges.append((weight, curr_node, next_node))
+        
+        # sort by weight
+        all_edges.sort()
+
+        # UnionFind 
+        # connect all nodes with one
+        uf = UnionFind(n)
+        mst_weight = 0
+        edges_used = 0 # to count n-1 is done.
+
+        for weight, curr_node, next_node in all_edges:
+            if uf.union(curr_node, next_node):
+                mst_weight += weight
+                edges_used += 1
+            
+            if edges_used == n-1:
+                break
+        return mst_weight
+        
+        
