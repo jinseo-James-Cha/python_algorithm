723f167af19fc8f551ed08cf0133dd718a235bbe
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-13 23:19:24 +0100

    Time: 7 ms (51.29%), Space: 20.6 MB (59.28%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -18,23 +18,6 @@ class Solution:
 
 
         """
-        nums.sort()
-        left = 0
-        right = 1
-        result = 0
-        while left < len(nums) and right < len(nums):
-            if left == right or nums[right] - nums[left] < k:
-                right += 1
-            elif nums[right] - nums[left] > k:
-                left += 1
-            else:
-                left += 1
-                result += 1
-                while left < len(nums) and nums[left] == nums[left-1]:
-                    left += 1
-        return result
-
-
         counter = defaultdict(int)
         for i, num in enumerate(nums):
             counter[num] += 1
@@ -63,4 +46,8 @@ class Solution:
                 while left < len(nums) and nums[left] == nums[left-1]:
                     left += 1
         return result
+
+
+        
+
         
