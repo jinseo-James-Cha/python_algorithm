0181b8e4e07d2a705597f61ad237a61c0c4b227b
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-14 18:08:46 +0100

    Time: 165 ms (13.93%), Space: 77 MB (63.41%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,58 +1,72 @@
-class DoublyLinkedNode:
-    def __init__(self, key, value):
+"""
+capacity > 0
+
+O(1)
+search -> map, set, index in list
+insert, delete -> map, set, linkedlist
+
+hashmap[key] = linkedlist node
+head <-> node2 <-> node1 <-> tail
+
+"""
+
+class DoublyLinkedList:
+    def __init__(self, key, val):
         self.key = key
-        self.value = value
+        self.val = val
         self.next = None
         self.prev = None
-
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.head = DoublyLinkedNode(-1,-1)
-        self.tail = DoublyLinkedNode(-1,-1)
+        self.head = DoublyLinkedList(-1,-1)
+        self.tail = DoublyLinkedList(-1,-1)
+
         self.head.next = self.tail
         self.tail.prev = self.head
-
-        self.storage = {}
-
+        self.hashmap = {}
+        
+    # O(1)
     def get(self, key: int) -> int:
-        if key not in self.storage:
+        if key not in self.hashmap:
             return -1
         
-        curr = self.storage[key]
-        self.remove(curr)
-        self.add(curr)
-        return curr.value
-        
+        # if found
+        # take out the node and put into tail
+        found_node = self.hashmap[key]
+        self.remove(found_node)
+        self.insert(found_node)
+        return found_node.val
+
     def remove(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
-        
-    def add(self, node):
-        temp = self.tail.prev
-        temp.next = node
+    
+    def insert(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+
+        node.next = self.tail
         self.tail.prev = node
 
-        node.prev = temp
-        node.next = self.tail
-
+    # O(1)
     def put(self, key: int, value: int) -> None:
-        if key in self.storage:
-            curr = self.storage[key]
-            self.remove(curr)
-            del self.storage[key]
-        
-        newNode = DoublyLinkedNode(key, value)
-        self.storage[key] = newNode
-        self.add(newNode)
+        if key in self.hashmap:
+            old_node = self.hashmap[key]
+            self.remove(old_node)
 
-        if len(self.storage) > self.capacity:
-            removingNode = self.head.next
-            self.remove(self.head.next)
-            del self.storage[removingNode.key]
-        
+        new_node = DoublyLinkedList(key, value)
+        self.hashmap[key] = new_node
+        self.insert(new_node)
+
+        # if it is more than the capacity -> remove the first one
+        if len(self.hashmap) > self.capacity:
+            first_node = self.head.next
+            self.remove(first_node)
+            del self.hashmap[first_node.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
