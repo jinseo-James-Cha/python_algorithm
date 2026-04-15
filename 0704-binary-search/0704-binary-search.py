ee8f6f642b3b73bfe799986c9888061b41f1d480
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-15 16:23:59 +0100

    Time: 0 ms (100%), Space: 20.7 MB (16.5%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,5 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # find upper bound and then check upperbound-1 == target
+        # 1 2 3 4 5 -  4
+        #     4
+
+        left = 0
+        right = len(nums)
+
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid
+        
+        if left > 0 and nums[left-1] == target:
+            return left - 1
+        else:
+            return -1
+
+
+
         # sorted nums
 
         left = 0
