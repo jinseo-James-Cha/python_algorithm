e5d88c08e1be4fca0d53b82e980d2afef2639799
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-15 15:34:13 +0100

    Time: 181 ms (5.09%), Space: 46.1 MB (45.72%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.py
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.py
@@ -1,99 +1,28 @@
 class Solution:
     def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
-        # 1. find a maximum value in col
-        # 2. 
+        """
+        brute force 
+        check every each cell and compare values on 4 directions cells within the bounds.
 
+        """
+        def is_within_bound(row, col):
+            return 0 <= row < len(mat) and 0 <= col < len(mat[0])
+
+        DIRS = [(0,-1), (0,1), (-1,0), (1,0)]
+        def is_peak_element(row, col):
+            check_all = 0
+            for dr, dc in DIRS:
+                next_r, next_c = row + dr, col + dc
+                if not is_within_bound(next_r, next_c):
+                    check_all += 1 
+                elif mat[next_r][next_c] < mat[row][col]:
+                    check_all += 1
+            return check_all == 4
+
+        count_peak_elements = 0
         m, n = len(mat), len(mat[0])
-        left, right = 0, n-1
-
-        while left <= right:
-            mid = (left + right) // 2
-
-            # 1.
-            max_row = 0
-            for r in range(m):
-                if mat[r][mid] > mat[max_row][mid]:
-                    max_row = r
-            
-            # 2.
-            val = mat[max_row][mid]
-            left_val = mat[max_row][mid-1] if mid > 0 else -1
-            right_val = mat[max_row][mid+1] if mid < n-1 else -1
-
-            # find the peack
-            if left_val < val > right_val:
-                return [max_row, mid]
-            elif left_val > val:
-                right = mid-1
-            else:
-                left = mid + 1
-        
-        return [-1, -1]
-
-
-
-
-        # Strictly greater than 4directions
-        # find a peak in every row and check row-1 and row +1 values
-        def check_other_rows(row, col, peak_val):
-            if row < 0 or row >= len(mat):
-                return True
-            
-            return mat[row][col] < peak_val
-
-        def get_peak_col(row):
-            left = 0
-            right = len(row) - 1
-            while left < right:
-                mid = (left + right) // 2
-                if row[mid] < row[mid+1]:
-                    left = mid+1
-                else:
-                    right = mid
-            return left
-        
-        for i, curr_row in enumerate(mat):
-            peak_col = get_peak_col(curr_row)
-            up_score = check_other_rows(i-1, peak_col, mat[i][peak_col])
-            down_score = check_other_rows(i+1, peak_col, mat[i][peak_col])
-            if up_score and down_score:
-                return [i, peak_col]
-        
-        return [-1,-1]
-
-        
-
-
-
-
-
-        # dfs 
-        # time = O of m times n
-        m, n = len(mat), len(mat[0])
-
-        def is_within_bound(row,col):
-            if row < 0 or row >= len(mat):
-                return False
-            
-            if col < 0 or col >= len(mat[0]):
-                return False
-            
-            return True
-
-        def dfs(row, col, val):
-            if not is_within_bound(row, col):
-                return 1
-            
-            return int(mat[row][col] < val)
-
-
         for row in range(m):
             for col in range(n):
-                up = dfs(row-1, col, mat[row][col])
-                down = dfs(row+1, col, mat[row][col])
-                left = dfs(row, col-1, mat[row][col])
-                right = dfs(row, col+1, mat[row][col])
-                if up+down+left+right == 4:
-                    return [row, col]
-
-        return [-1, -1]
+                if is_peak_element(row, col):
+                    return [row,col]
+        return []
