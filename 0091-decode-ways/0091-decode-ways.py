bb6832622d559f30163569a17fd49e3a2a48f112
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-15 14:43:43 +0100

    Time: 1 ms (33.91%), Space: 19.5 MB (16.78%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -27,8 +27,28 @@ class Solution:
         # action choose 1letter or 2letters
 
         # 1 1 1 0 6 -> 1 / 1106 + 11 / 106
-        # top down
 
+        # bottom up
+        dp = [0] *(len(s) + 1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == "0" else 1
+        for i in range(2, len(s) + 1):
+            if s[i-1] != "0":
+                dp[i] = dp[i-1]
+            
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[len(s)]
+
+
+
+
+
+
+
+
+        # top down
         # dp(0) -> dp(1) + dp(2)
         def dp(i):
             if i == len(s):
