68fef6960389706836098721f78bacc5bd5a3f89
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-16 12:20:54 +0100

    Time: 3 ms (90.7%), Space: 20 MB (29.32%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,5 +1,37 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        # time complexity - o(n * n!) if [1,2,3,..] all different, if all the same [1,1,1] => o(n)
+        #                 - O(n * k) # k = number of unique permutations
+        # space complexity - o(n * k)
+        nums.sort()
+        result = []
+        used = [False] * len(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack(path)
+
+                path.pop()
+                used[i] = False
+
+        backtrack([])
+        return result
+
+
         # using one element at each time and make a permutation -> unique
         # get all combination and save only unique -> set
 
