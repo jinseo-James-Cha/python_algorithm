e575f3a8127a5ca7d6d7f8848d18025ba1475bb4
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-21 16:44:30 +0100

    Time: 15 ms (94.29%), Space: 25.4 MB (71.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,43 +1,33 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         """
+
         1 2 3 4
 
-        1: 2 * 3 * 4 all from right
-        2: 1 * 3 * 4 1 left 2 right
-        3: 1 * 2 * 4 2 left 1 right
-        4: 1 * 2 * 3 all from left
+        0 : 1 2 3
+        1 : 0 2 3 
+        2 : 0 1 3
+        3 : 0 1 2
 
-        -> 
-        prev = nums[0]
-        
+        -> one pass
+        res[i] = total_product
+        total_product * nums[i]
+
+        <- one pass
         """
-        # two pass o(n)
-        # product -> way and then <- way
         
-        answer = [1] * len(nums)
-        
-        # ->
-        prev = nums[0]
-        for i in range(1, len(nums)):
-            answer[i] = prev
-            prev *= nums[i]
-
-        # <- 
-        prev = nums[-1]
-        for i in range(len(nums) - 2, -1, -1):
-            answer[i] *= prev
-            prev *= nums[i]
-        
-        return answer
-
-
-        # naive solution - brute force
-        # time complexity - O(n^2)
-        answer = [1] * len(nums)
+        # to right
+        curr = 1
+        res = [1] * len(nums)
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j:
-                    answer[i] *= nums[j]
-        
-        return answer
+            res[i] = curr
+            curr *= nums[i]
+
+        # 24 12 8 6
+        # 
+        # to left
+        curr = nums[-1]
+        for j in range(len(nums)-2, -1, -1):
+            res[j] = res[j] * curr # 2 * 4 -> 1 * 12 -> 1 * 24
+            curr *= nums[j] # 4 * 3 12 24 -> 
+        return res
