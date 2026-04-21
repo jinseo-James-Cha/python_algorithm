27866b0223d731603b0b8f6eba17e47d7a446d73
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-04-21 12:16:09 +0100

    Time: 16 ms (21.49%), Space: 39.3 MB (19.12%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,45 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        """
+        - arr[i] > 0
+
+        - starting from start index
+
+        - i + arr[i] or i - arr[i]
+
+        - return any index has 0
+
+        """
+        #  DP
+        # state i
+        # - is i in range of the array? 
+        # - is i == 0? 
+        # - or move other index
+        
+        n = len(arr)
+        if start >= n:
+            return False
+
+        if arr[start] == 0:
+            return True
+
+        def dp(i):
+            if i < 0 or i >= len(arr):
+                return False
+            
+            if i in visited:
+                return False
+            visited.add(i)
+
+            if i not in memo:
+                if arr[i] == 0:
+                    memo[i] = True
+                else:
+                    memo[i] = dp(i + arr[i]) or dp(i - arr[i])
+            visited.remove(i)
+            return memo[i]
+        
+        memo = {}
+        visited = set()
+        return dp(start)
+    
