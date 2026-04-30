e6b4b5f68f44270624cb053e4875c94602097ae6
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-05-01 00:10:27 +0100

    Time: 3 ms (39.33%), Space: 26.6 MB (5.13%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -4,8 +4,35 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        # BFS
+        if not root:
+            return []
+        
+        res = []
+        queue = deque([(root, [], targetSum)])
+        while queue:
+            curr_node, curr_combo, remaining = queue.popleft()
+            curr_combo.append(curr_node.val)
+            if remaining == curr_node.val and not curr_node.left and not curr_node.right:
+                res.append(curr_combo[:])
+                continue
+            
+            if curr_node.left:
+                queue.append((curr_node.left, curr_combo, remaining - curr_node.val))
+            
+            if curr_node.right:
+                queue.append((curr_node.right, curr_combo, remaining - curr_node.val))
+            
+
+
+
+
+
+
+        # DFS
         def dfs(node, curr, remaining):
             nonlocal res
             if not node:
