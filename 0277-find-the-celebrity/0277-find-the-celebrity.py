7e3af682ac59683bbda4eb83db987d83dacb80ab
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-05-17 23:22:17 +0100

    Time: 801 ms (57.55%), Space: 19.6 MB (28%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.py
+++ 0277-find-the-celebrity/0277-find-the-celebrity.py
@@ -9,6 +9,23 @@ class Solution:
         n = 3
         0, 1 ,2
         """
+        # greedy
+        def is_celebrity(i):
+            for j in range(n):
+                if i == j: continue
+                if knows(i,j) or not knows(j,i):
+                    return False
+            return True
+
+        candidate = 0
+        for i in range(1, n):
+            if knows(candidate, i):
+                candidate = i
+        if is_celebrity(candidate):
+            return candidate
+        return -1
+
+        # brute force -> o(n^2)
         for i in range(n):
             found = True
             for j in range(n):
