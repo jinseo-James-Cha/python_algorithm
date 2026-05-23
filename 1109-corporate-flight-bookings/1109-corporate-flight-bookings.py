3b4bf39043d9ee487e1676f6e08e2d05e65c9538
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-05-24 00:00:47 +0100

    Time: 20 ms (86.79%), Space: 31 MB (29.64%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,5 +1,23 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        """
+        prefix
+        starting point += seats
+        end point -= seats
+
+        bookings = [[1,2,10],[2,3,20],[2,5,25]]
+        n = 5
+
+        [10,45,-10,-20,0]
+        =>
+        [10,
+            10+45=55,
+            55-10=45,
+            45-20=25,
+        25]
+
+        """
+
         prefix = [0] * n
 
         for first, last, seats in bookings:
