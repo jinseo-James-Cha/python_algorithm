af8c454a7ee696288d7eead09eac7950c293bd57
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-05-25 11:47:46 +0100

    Time: 538 ms (81.79%), Space: 22.4 MB (18.62%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -11,6 +11,42 @@ class Solution:
         
         using sets not to have duplicates
         """
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            target = -nums[i]
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                curr = nums[left] + nums[right]
+
+                if curr == target:
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif curr < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return res
+
+
         def find_two_sums(arr, target):
             found = []
             left = 0
