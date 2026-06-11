39e245a8c624d563c6564047a83dec9adafc4331
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-11 23:41:03 +0100

    Time: 321 ms (97.41%), Space: 21.2 MB (53.37%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,38 +1,27 @@
 from collections import deque, defaultdict
-import heapq
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # network 1 ~ n
-        # u -> v with weight
-        
-        # starting from node k return minimum time for all visit
-        # return -1 if not possible to visit
-        
-        # reset all with INF
-        # and visit from starting node and neighbor, renew the weight
-        # dijkstra
-        
+        """
+        Dijkstra algorithm
+        """
         graph = defaultdict(list)
-        for src, tg, w in times:
-            graph[src].append((tg, w))
-        
-        # graph[2] = [(1,1), (3,1)]
-        # graph[3] = [(4, 1)]
-        
-        
-        network = [float('inf')] * (n+1) # node -1 in network
-        network[0] = 0 # node starts from 0 so this is not reachable
-        network[k] = 0 # starting node has no weight
-        
-        queue = [(0, k )] # weight, node in queue
+        for u,v,w in times:
+            graph[u].append((v,w))
+
+        network_time = [float('inf')] * (n + 1)
+        network_time[0] = 0
+        network_time[k] = 0
+
+        queue = deque([(0, k)])
         while queue:
-            weight, curr = heapq.heappop(queue)
-            if network[curr] < weight:
+            curr_time, curr_node = queue.popleft()
+            if curr_time > network_time[curr_node]:
                 continue
             
-            for neighbor, new_w in graph[curr]:
-                if network[neighbor] > weight + new_w:
-                    network[neighbor] = weight + new_w
-                    heapq.heappush(queue, (network[neighbor], neighbor))
-                
-        return -1 if max(network) == float('inf') else max(network)
+            for next_node, next_time in graph[curr_node]:
+                total_time = next_time + curr_time
+                if network_time[next_node] > total_time:
+                    network_time[next_node] = total_time
+                    queue.append((total_time, next_node))
+        
+        return -1 if max(network_time) == float('inf') else max(network_time)
