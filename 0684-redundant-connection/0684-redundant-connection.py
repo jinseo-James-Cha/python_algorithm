ac3e88fa36475af9f23a58f95aa6652a6e42639f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-13 00:05:11 +0100

    Time: 4 ms (34.01%), Space: 19.5 MB (79.58%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -7,37 +7,30 @@ class UnionFind:
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
-
+    
     def union(self, x, y):
         xset, yset = self.find(x), self.find(y)
         if xset == yset:
             return False
         
-        if self.rank[xset] < self.rank[yset]:
+        if self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+        elif self.rank[xset] < self.rank[yset]:
             self.parent[xset] = yset
-        elif self.rank[xset] > self.rank[yset]:
-            self.parent[yset] = xset
         else:
-            self.parent[yset] = xset
-            self.rank[xset] += 1
+            self.parent[xset] = yset
+            self.rank[yset] += 1
         return True
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        # connect nodes based on edges order
-        # and check if the nodes are already connected?
-        # then return the edge
-        # using union find
-        if not edges:
-            return []
-
-        # 3 nodes needs 2 edges only what is the last redundant edge?
-        n = len(edges) + 1
-        uf = UnionFind(n)
+        # union find
+        n = len(edges)
+        uf = UnionFind(n+1)
 
         res = []
         for a, b in edges:
             if not uf.union(a, b):
-                res = [a,b]
-                break
+                res = [a, b]
         return res
+        
