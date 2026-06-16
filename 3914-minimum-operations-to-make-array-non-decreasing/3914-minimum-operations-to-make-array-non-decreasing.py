c2f103fe654f0e8a48db453f4a7234a53dfa7e43
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-16 15:25:37 +0100

    Time: 76 ms (57.83%), Space: 32.2 MB (84.86%) - LeetHub

--- 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.py
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.py
@@ -8,17 +8,25 @@ class Solution:
 
         Make non-decreasing is >= prev
 
+        Input: nums = [3,3,2,1]
+        when 3  > 2 -> 1
+        3 <= 3 <= 2+1 <= 1+1 
+        
+        when 2+1 > 1+1
+        3 <= 3 <= 2+1 <= 1+1+1
+    
+        ----------------------
         Input: nums = [5,1,2,3]
 
-        n = 4
-
-        range(3)
+        when 5 > 1 -> 4
+        5 <= x+4 <= x+4 <= x+4, after first we add all right elements +4
+        5 <= 6 <= 2+4 <= 3+4
         """
         n = len(nums)
         operations = 0
 
         for i in range(n-1):
             if nums[i] > nums[i+1]:
-                operations += nums[i] - nums[i+1]  # Add the size of the drop
+                operations += nums[i] - nums[i+1]
 
         return operations
