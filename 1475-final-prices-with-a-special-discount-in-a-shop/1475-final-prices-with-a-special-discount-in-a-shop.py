f120e895b8bf68ceda329eb64de49d77043d9fb2
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-17 16:03:41 +0100

    Time: 3 ms (51.7%), Space: 19.2 MB (70.05%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,70 +1,7 @@
-from collections import deque
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        # special discount
-        # if you buy "i"th item, discount prices[j] ==> minimum index j > i and prices[j] <= prices[i]
-
-
-
-        """
-        8 4 6 2 3
-        4 2 4 2 3
-
-
-        stack [8]
-        8 > 4
-        pop
-        answer[4]
-        
-        stack [4]
-        4 < 6
-        push
-        
-        stack [4, 6]
-        6 > 2
-        pop
-        [4]
-        4 > 2
-        pop
-        [4, 2]
-        stack[4, 2, 4]
-        push 2
-        
-        stack[2]
-        stack[2,3]
-
-        [4,2,4] + [2,3]
-        [1, 2, 3, 4, 5]
-
-        8 4 6 5
-        [4]
-
-        [4, 6]
-        5
-
-        [1 4] 
-        monotonic stack ? no..?
-
-
-
-        8 4 6 2 3
-        4 2 4 2 3
-
-        """
-        res = prices[:]
-        idx_stack = []
-        for i, p in enumerate(prices):
-            while idx_stack and p <= prices[idx_stack[-1]]:
-                res[idx_stack.pop()] -= p
-            idx_stack.append(i)
-        return res
-
-
-
-
-
-
         # monotonic stack
+        # keep idx which has smaller than next elements
         res = prices[:]
         idx_stack = []
         for i in range(len(prices)):
@@ -72,9 +9,6 @@ class Solution:
                 res[idx_stack.pop()] -= prices[i]
             idx_stack.append(i)
         return res
-
-
-
 
         # brute force
         res = [-1] * len(prices)
