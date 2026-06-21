35e189b67bf3a4b189035b32950f4c8e5c464043
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-21 15:28:17 +0100

    Time: 60 ms (27.69%), Space: 23.3 MB (48.72%) - LeetHub

---
+++ 1063-number-of-valid-subarrays/1063-number-of-valid-subarrays.py
@@ -0,0 +1,37 @@
+class Solution:
+    def validSubarrays(self, nums: List[int]) -> int:
+        """
+        1 4 2 5 3
+
+        1 4 2 5 3 -> 5
+        4 -> 1
+        2 5 3 -> 3
+        5 -> 1
+        3 -> 1
+        """
+        # monotonic stack
+        res = 0
+        stack = []
+        for i in range(len(nums)):
+            while stack and nums[i] < nums[stack[-1]]:
+                res += i - stack[-1]
+                stack.pop()
+            stack.append(i)
+
+        while stack:
+            res += len(nums) - stack[-1]
+            stack.pop()
+        return res
+
+        # brute force 
+        # time complexity -> o(n^2)
+        res = 0
+        for i in range(len(nums)):
+            res += 1
+            for j in range(i+1, len(nums)):
+                if nums[j] >= nums[i]:
+                    res += 1
+                else:
+                    break
+
+        return res
