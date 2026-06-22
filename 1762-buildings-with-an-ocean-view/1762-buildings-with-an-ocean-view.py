33149682f6f625cdcd49839d12f11ad19c91a07e
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-22 11:49:30 +0100

    Time: 57 ms (26.45%), Space: 31.9 MB (12.77%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,6 +1,67 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         """
+        B - - ->
+          B - ->
+            B -> 
+                    OCEAN VIEW
+        """
+        # optimized solution
+        # monotonic stack -> having index that having lower than stack[-1]
+        # time: o(n)
+        n = len(heights)
+        stack = []
+        for i in range(n):
+            while stack and heights[stack[-1]] <= heights[i]:
+                stack.pop()
+            stack.append(i)
+        return stack
+
+        # Brute force
+        # 2 nested loops -> check next elements has bigger or not
+        # time o(n^2) > 10^5 -> TLE
+        n = len(heights)
+        res = []
+        for i in range(n):
+            curr_building = heights[i]
+            ocean_view = True
+            for j in range(i+1, n):
+                if curr_building <= heights[j]:
+                    ocean_view = False
+                    break
+            if ocean_view:
+                res.append(i)
+        return res
+
+
+        # optimized solution
+        # monotonic stack -> having index that having lower than stack[-1]
+        # time o(n)
+        monotonic_index_stack = [0]
+        for i in range(1, len(heights)):
+            while monotonic_index_stack and heights[monotonic_index_stack[-1]] <= heights[i]:
+                monotonic_index_stack.pop()
+
+            monotonic_index_stack.append(i)
+        
+        return monotonic_index_stack
+
+        # brute force
+        # O of n squared > 10**5 TLE
+        res = []
+        n = len(heights)
+        for i in range(n):
+            flag = True
+            for j in range(i+1, n):
+                if heights[i] <= heights[j]:
+                    flag = False
+                    break
+            
+            if flag:
+                res.append(i)
+        return res
+
+        """
         questions
         - n buildings -> n = len(heights)
         - ocean is the right side ->
@@ -20,6 +81,7 @@ class Solution:
 
         """
 
+
         # 4 2 3 1
         
         # res [3]
@@ -36,5 +98,7 @@ class Solution:
             if heights[i] > heights[res[-1]]:
                 res.append(i)
         
+        # res.reverse()
         return res[::-1]
-                
+
+        
