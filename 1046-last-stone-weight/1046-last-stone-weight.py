e3b8eb05c548dcd0d517ad1e4d0c5a4571032644
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-25 12:19:14 +0100

    Time: 0 ms (100%), Space: 19.4 MB (35.02%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -9,6 +9,33 @@ class Solution:
         1. x == y => both removed
         2. x != y => x removed and y = y - x
         """
+        # bucket sort or counting sort
+        max_weight = max(stones)
+        buckets = [0] * (max_weight + 1)
+
+        for weight in stones:
+            buckets[weight] += 1
+
+        biggest_weight = 0 
+        current_weight = max_weight
+        while current_weight > 0:
+            if buckets[current_weight] == 0:
+                current_weight -= 1
+            elif biggest_weight == 0:
+                buckets[current_weight] %= 2
+                if buckets[current_weight] == 1:
+                    biggest_weight = current_weight
+                current_weight -= 1
+            else:
+                buckets[current_weight] -= 1
+                if biggest_weight - current_weight <= current_weight:
+                    buckets[biggest_weight - current_weight] += 1
+                    biggest_weight = 0
+                else:
+                    biggest_weight -= current_weight
+        return biggest_weight
+
+
         # max heap
         if len(stones) == 1:
             return 1
