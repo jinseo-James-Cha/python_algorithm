faab56ade470aa308469baa077de6539f60174ec
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-25 12:22:22 +0100

    Time: 1 ms (21.48%), Space: 19.3 MB (69.43%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,41 +1,6 @@
 import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        """
-        i stone
-        choose the heaviest two stones and smash
-        and x <= y
-        
-        1. x == y => both removed
-        2. x != y => x removed and y = y - x
-        """
-        # bucket sort or counting sort
-        max_weight = max(stones)
-        buckets = [0] * (max_weight + 1)
-
-        for weight in stones:
-            buckets[weight] += 1
-
-        biggest_weight = 0 
-        current_weight = max_weight
-        while current_weight > 0:
-            if buckets[current_weight] == 0:
-                current_weight -= 1
-            elif biggest_weight == 0:
-                buckets[current_weight] %= 2
-                if buckets[current_weight] == 1:
-                    biggest_weight = current_weight
-                current_weight -= 1
-            else:
-                buckets[current_weight] -= 1
-                if biggest_weight - current_weight <= current_weight:
-                    buckets[biggest_weight - current_weight] += 1
-                    biggest_weight = 0
-                else:
-                    biggest_weight -= current_weight
-        return biggest_weight
-
-
         # max heap
         if len(stones) == 1:
             return 1
@@ -46,10 +11,10 @@ class Solution:
         
         # take out two max and loop
         while len(max_heap) > 1:
-            y = -heapq.heappop(max_heap)
-            x = -heapq.heappop(max_heap)
+            first_max = -heapq.heappop(max_heap)
+            second_max = -heapq.heappop(max_heap)
             
-            new_weight = y - x
+            new_weight = first_max - second_max
             if new_weight > 0:
                 heapq.heappush(max_heap, -new_weight)
         
