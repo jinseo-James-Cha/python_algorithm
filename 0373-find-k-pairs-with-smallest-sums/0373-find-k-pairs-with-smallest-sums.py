cd41a5e0424047d9ad794fc59263e38c9a550627
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-25 16:02:52 +0100

    Time: 83 ms (55.59%), Space: 40 MB (24.17%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -1,45 +1,42 @@
 import heapq
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        """
+        one from nums1
+        one from nums2
+        return k number of smallest sum combinations
+        """
         m, n = len(nums1), len(nums2)
-
         res = []
-        visited = set()
+        seen_combinations = set()
+        pq = [(nums1[0] + nums2[0], 0, 0)]
+        while k > 0 and pq:
+            curr_sum, u, v = heapq.heappop(pq)
+            res.append([nums1[u], nums2[v]])
 
-        min_heap = [(nums1[0] + nums2[0], (0,0))]
-        while k > 0 and min_heap:
-            s, (i, j) = heapq.heappop(min_heap)
-            res.append([nums1[i], nums2[j]])
-
-            if i+1 < m and (i+1, j) not in visited:
-                heapq.heappush(min_heap, (nums1[i+1]+nums2[j], (i+1, j)))
-                visited.add((i+1, j))
+            if u+1 < m and (u+1, v) not in seen_combinations:
+                heapq.heappush(pq, (nums1[u+1] + nums2[v], u+1, v))
+                seen_combinations.add((u+1, v))
             
-            if j+1 < n and (i, j+1) not in visited:
-                heapq.heappush(min_heap, (nums1[i]+nums2[j+1], (i, j+1)))
-                visited.add((i, j+1))
-            
+            if v+1 < n and (u, v+1) not in seen_combinations:
+                heapq.heappush(pq, (nums1[u] + nums2[v+1], u, v+1))
+                seen_combinations.add((u,v+1))
             k -= 1
         return res
-        
 
 
-        # ascending order
-        # [one from nums1 , second from nums2]
 
-        # 1 7 11 // 2 4 6
-        # 12
-        # 14
-        # 16
-        # get all combinations and then sort by sum and cut k
-        # Memory Limit Exceeded
-        all_combinations = []
-        for num1 in nums1[:k]:
-            for num2 in nums2[:k]:
-                heapq.heappush(all_combinations, (num1 + num2, num1, num2))
+
+
+        # brute force -> MLE
+        # save all combinations and min heap pop
+        all_combi = []
+        for u in nums1[:k]:
+            for v in nums2[:k]:
+                heapq.heappush(all_combi, (u+v, u, v))
         
         res = []
         for _ in range(k):
-            s, u, v = heapq.heappop(all_combinations)
+            s, u, v = heapq.heappop(all_combi)
             res.append([u,v])
         return res
