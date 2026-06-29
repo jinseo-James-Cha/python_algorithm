50d73c34915af183c18de2dfc1a9f358e875040a
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-06-29 14:53:10 +0100

    Time: 0 ms (100%), Space: 19.2 MB (93.95%) - LeetHub

---
+++ 0831-masking-personal-information/0831-masking-personal-information.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maskPII(self, s: str) -> str:
+        """
+        string manipulation
+
+        Email or Phone number
+
+        Email
+        - two uppercase and lowercase letters + @ + domain + . 
+        - all lowercase and first letter +  * X 5 + last letter 
+        """
+        is_email = False
+        for ch in s:
+            if ch == "@":
+                is_email = True
+        
+        if is_email:
+            res = s.split('@')
+            res[0] = res[0].lower()
+            res[1] = res[1].lower()
+
+            res[0] = res[0][0] + '*****' + res[0][-1]
+            return "@".join(res)
+        else:
+            res = [ch for ch in s if ch.isnumeric()]
+            phone_len = len(res)
+            last_four = res[phone_len-4:]
+            country_code_len = phone_len % 10
+            country_code = ""
+            if country_code_len > 0:
+                country_code = "+" + ("*" * country_code_len) + "-"
+            
+            
+            return country_code + "***-***-" + "".join(last_four)
